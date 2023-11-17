fa9aedd9f669fca31fc390143646353dd15dff2b
LeeJongsu <lik9819@naver.com>
2023-11-18 00:32:05 +0900

    Update 격자판 최대합.java

--- inflearn/배열/격자판 최대합/격자판 최대합.java
+++ inflearn/배열/격자판 최대합/격자판 최대합.java
@@ -8,6 +8,9 @@
 
 // 예시 출력 155
 
+
+//풀이 1
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -67,3 +70,50 @@ class Main {
 		System.out.println(T.solution(n, arr));
 	}
 }
+
+
+// 풀이 2
+import java.util.Scanner;
+
+class Main {
+	
+	public int solution(int n, int[][] arr){
+		
+		int answer = Integer.MIN_VALUE;
+		int sum1, sum2;
+		
+		for(int i=0; i<n; i++) {
+			sum1=sum2=0;
+			for(int j=0; j<n; j++) {
+				sum1+=arr[i][j];
+				sum2+=arr[j][i];
+			}
+			answer=Math.max(answer, sum1);
+			answer=Math.max(answer, sum2);
+		}
+		sum1=sum2=0;
+		for(int i=0; i<n; i++) {
+			sum1+=arr[i][i];
+			sum2=arr[i][n-1-i];
+		}
+		answer=Math.max(answer, sum1);
+		answer=Math.max(answer, sum2);
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		
+		int n= sc.nextInt();
+		int[][] arr = new int[n][n];
+		
+		for(int i=0; i<n; i++)
+			for(int j=0; j<n; j++)
+				arr[i][j]=sc.nextInt();
+
+		System.out.println(T.solution(n, arr));
+	}
+}
+
