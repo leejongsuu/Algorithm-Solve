9a4aeb08e3d0c7337a8e834ff7e4b56a33365760
LeeJongsu <lik9819@naver.com>
2023-11-21 18:36:20 +0900

    Update 최대 길이 연속부분수열.java

--- inflearn/Two pointers,Sliding window/최대 길이 연속부분수열/최대 길이 연속부분수열.java
+++ inflearn/Two pointers,Sliding window/최대 길이 연속부분수열/최대 길이 연속부분수열.java
@@ -32,6 +32,8 @@ class Main {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int[] arr = new int[n];
+		for(int i=0; i<n; i++)
+			arr[i] = sc.nextInt();
 			
 		System.out.println(T.solution(n,m,arr));
 	}
