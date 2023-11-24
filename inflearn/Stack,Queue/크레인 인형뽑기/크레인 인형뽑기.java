473812f3ec090339a33470e739aed3e6e9e7b39f
LeeJongsu <lik9819@naver.com>
2023-11-24 18:19:05 +0900

    Create 크레인 인형뽑기.java

---
+++ inflearn/Stack,Queue/크레인 인형뽑기/크레인 인형뽑기.java
@@ -0,0 +1,60 @@
+// 예시 입력 
+/*
+0 0 0 0 0
+0 0 1 0 3
+0 2 5 0 1
+4 2 4 4 2
+3 5 1 3 1
+8
+1 5 3 5 1 2 1 4
+*/
+
+// 예시 출력 4
+
+import java.util.Scanner;
+import java.util.Stack;
+
+class Main {
+	
+	public int solution(int[] moves, int[][] board){
+		int answer = 0;
+		Stack<Integer> stack = new Stack<>();
+
+		for(int pos : moves) {
+			for(int i=0; i<board.length; i++) {
+				if(board[i][pos-1]!=0) {
+					int tmp = board[i][pos-1];
+					board[i][pos-1]=0;
+					if(!stack.isEmpty()&& stack.peek()==tmp) {
+						answer+=2;
+						stack.pop();
+					}
+					else stack.push(tmp);
+					break;
+				}
+			}
+		}
+	
+		return answer;
+	}
+	
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[][] board = new int[n][n];
+		
+		for(int i=0;i<n;i++)
+			for(int j=0;j<n;j++)
+				board[i][j]=sc.nextInt();
+		
+		int m = sc.nextInt();
+		int[] moves = new int[m];
+		for(int i=0; i<m; i++)
+			moves[i]=sc.nextInt();
+		
+		System.out.println(T.solution(moves, board));
+	}
+}
