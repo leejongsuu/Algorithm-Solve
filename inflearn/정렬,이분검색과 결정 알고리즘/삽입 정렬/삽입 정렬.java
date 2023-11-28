cb3d6d34fac6a73ce6895cb695b6952a03a81ae1
LeeJongsu <lik9819@naver.com>
2023-11-28 22:47:59 +0900

    Create 삽입 정렬.java

---
+++ inflearn/정렬,이분검색과 결정 알고리즘/삽입 정렬/삽입 정렬.java
@@ -0,0 +1,39 @@
+// 예시 입력
+/*
+6
+11 7 5 6 10 9
+*/
+
+//예시 출력 5 6 7 9 10 11
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int[] solution(int n, int[] arr){
+
+        for(int i=1; i<n; i++){
+            int tmp=arr[i], j;
+            for(j=i-1;j>=0; j--){
+                if(arr[j]>tmp) arr[j+1] = arr[j];
+                else break;
+            }
+            arr[j+1] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++)
+            arr[i]=sc.nextInt();
+
+        for(int i : T.solution(n, arr))
+            System.out.print(i+" ");
+    }
+}
