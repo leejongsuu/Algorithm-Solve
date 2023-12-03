4800e261782aa1fe85a67d2c05472c6037d38ec9
LeeJongsu <lik9819@naver.com>
2023-12-03 23:56:31 +0900

    Create 부분 집합 구하기.java

---
+++ inflearn/DFS/부분 집합 구하기/부분 집합 구하기.java
@@ -0,0 +1,40 @@
+// 예시 입력 3
+// 예시 출력 
+/*
+1 2 3
+1 2
+1 3
+1
+2 3
+2
+3
+*/
+
+import java.util.Scanner;
+
+public class Main {
+   static  int n;
+   static int[] ch;
+    public void DFS(int L){
+        if(L==n+1){
+            String tmp="";
+            for(int i=1;i<=n;i++){
+                if(ch[i]==1) tmp += (i + " ");
+            }
+            if(tmp.length()>0) System.out.println(tmp);
+        }else {
+            ch[L]=1;
+            DFS(L + 1);
+            ch[L]=0;
+            DFS(L + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        ch = new int[n+1];
+        T.DFS(1);
+    }
+}
