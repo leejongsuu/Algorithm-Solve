f0b69cad1aea5fb91325e9d5547a5ea3841def78
LeeJongsu <lik9819@naver.com>
2023-12-06 22:03:22 +0900

    Create 그래프 최단거리(BFS).java

---
+++ inflearn/BFS/그래프 최단거리(BFS)/그래프 최단거리(BFS).java
@@ -0,0 +1,70 @@
+// 예시 입력 
+/*
+6 9
+1 3
+1 4
+2 1
+2 5
+3 4
+4 5
+4 6
+6 2
+6 5
+*/
+
+//예시 출력
+/*
+2 : 3
+3 : 1
+4 : 1
+5 : 2
+6 : 2
+*/
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static int[] ch;
+    static int[][] graph;
+
+    public void BFS(int v){
+        Queue<Integer> Q = new LinkedList<>();
+        Q.offer(v);
+        int level = 1;
+        while(!Q.isEmpty()){
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                int x = Q.poll();
+                for (int j = 1; j <= n; j++) {
+                    if (graph[x][j] == 1 && ch[j] == 0) {
+                        Q.offer(j);
+                        ch[j] = level;
+                    }
+                }
+            }
+            level++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        graph = new int[n+1][n+1];
+        ch = new int[n + 1];
+        for(int i=0; i<m; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a][b]=1;
+        }
+
+        T.BFS(1);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i + " : " + ch[i]);
+        }
+    }
+}
