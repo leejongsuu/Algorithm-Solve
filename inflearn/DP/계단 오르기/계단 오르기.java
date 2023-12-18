2bfdd7683422fccb229c92c96d05af7167dffab1
LeeJongsu <lik9819@naver.com>
2023-12-19 00:18:28 +0900

    Create 계단 오르기.java

---
+++ inflearn/DP/계단 오르기/계단 오르기.java
@@ -0,0 +1,26 @@
+//예시 입력 7
+//예시 출력 21
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int[] dy;
+
+    public  int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dy[i] = dy[i - 2] + dy[i - 1];
+        }
+        return dy[n];
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dy = new int[n + 1];
+        System.out.println(T.solution(n));
+    }
+}
