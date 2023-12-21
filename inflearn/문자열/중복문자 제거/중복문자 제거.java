b1726052989c919fa69f607bdd2bddc76f87a384
LeeJongsu <lik9819@naver.com>
2023-12-21 20:50:06 +0900

    Update 중복문자 제거.java

--- inflearn/문자열/중복문자 제거/중복문자 제거.java
+++ inflearn/문자열/중복문자 제거/중복문자 제거.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
-class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		for(int i=0; i<str.length(); i++)
-		{
-			if(i==str.indexOf(str.charAt(i)))
-				System.out.print(str.charAt(i));
-		}
-	}
+public class Main {
+
+    public String solution(String str){
+        String answer = "";
+
+        for (int i = 0; i < str.length(); i++)
+            if(i==str.indexOf(str.charAt(i))) answer += str.charAt(i);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(T.solution(str));
+    }
 }
