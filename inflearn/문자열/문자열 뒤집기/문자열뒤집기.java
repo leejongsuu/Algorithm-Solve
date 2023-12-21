14fa9df9f5d35856936f0738d612757fedb72453
LeeJongsu <lik9819@naver.com>
2023-12-21 19:25:26 +0900

    Update 문자열뒤집기.java

--- inflearn/문자열/문자열 뒤집기/문자열뒤집기.java
+++ inflearn/문자열/문자열 뒤집기/문자열뒤집기.java
@@ -1,17 +1,39 @@
+//예시 입력
+/*
+3
+good
+Time
+Big
+*/
+//예시 출력
+/*
+doog
+emiT
+giB
+*/
 import java.util.Scanner;
 
-class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		String temp = sc.nextLine();
-		for(int i=0; i<n; i++)
-		{
-			String str = sc.nextLine();
-			str= new StringBuffer(str).reverse().toString();
-			System.out.println(str);
-		}
-	}
+public class Main {
+
+    static int n;
+    static String[] strArr;
+
+    public void  solution(){
+
+        for (int i=0; i< strArr.length; i++)
+            strArr[i] = new StringBuffer(strArr[i]).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        strArr = new String[n];
+
+        for (int i = 0; i < n; i++) strArr[i] = sc.next();
+        T.solution();
+        for(String str : strArr) System.out.println(str);
+
+    }
 }
