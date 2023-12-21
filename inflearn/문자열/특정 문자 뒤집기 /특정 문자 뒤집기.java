6ef02402faaee3fea9bc116bf0e4c328bf90decf
LeeJongsu <lik9819@naver.com>
2023-12-21 19:55:38 +0900

    Update 특정 문자 뒤집기.java

--- inflearn/문자열/특정 문자 뒤집기 /특정 문자 뒤집기.java
+++ inflearn/문자열/특정 문자 뒤집기 /특정 문자 뒤집기.java
@@ -1,28 +1,33 @@
 import java.util.Scanner;
 
-class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		char[] s = str.toCharArray();
-		int lt =0, rt= str.length()-1;
-		while(lt<rt) {
-			if(!Character.isAlphabetic(s[lt]))
-				lt++;
-			else if(!Character.isAlphabetic(s[rt]))
-				rt--;
-			else
-			{
-				char temp=s[lt];
-				s[lt]=s[rt];
-				s[rt]=temp;
-				lt++;
-				rt--;
-			}
-		}
-		for(char ch : s)
-			System.out.print(ch);
-	}
+public class Main {
+
+    public char[] solution(String str){
+
+        char[] ch = str.toCharArray();
+        int rt = str.length()-1;
+        for (int lt = 0; lt < rt; lt++) {
+            if (Character.isAlphabetic(ch[lt])) {
+                while (!Character.isAlphabetic(ch[rt])) {
+                    rt--;
+                }
+                char tmp = ch[lt];
+                ch[lt] = ch[rt];
+                ch[rt] = tmp;
+                rt--;
+            }
+        }
+        return ch;
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for(char c : T.solution(str))
+            System.out.print(c);
+    }
 }
