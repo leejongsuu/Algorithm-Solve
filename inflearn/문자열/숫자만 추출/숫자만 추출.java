6738fd70d2353f05c80e975bc5aacfe4dedbf59c
LeeJongsu <lik9819@naver.com>
2023-12-22 18:48:42 +0900

    Update 숫자만 추출.java

--- inflearn/문자열/숫자만 추출/숫자만 추출.java
+++ inflearn/문자열/숫자만 추출/숫자만 추출.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
-class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		str=str.replaceAll("[^0-9]","");
-		int i=0;
-		while(str.charAt(i)=='0') {		
-			i++;
-			if(str.charAt(i)!='0')
-				str=str.substring(i);
-		}
-		System.out.println(str);
-	}
+public class Main {
+
+    public String solution(String str){
+
+        str = str.replaceAll("[^0-9]", "");
+        while (str.charAt(0) == '0') {
+            str = str.substring(1);
+        }
+        return str;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(T.solution(str));
+    }
 }
