2c27a9742d871b32b5f8af8ec03366b569dbf906
LeeJongsu <lik9819@naver.com>
2023-12-25 21:49:12 +0900

    Update 보이는 학생.java

--- inflearn/배열/보이는 학생/보이는 학생.java
+++ inflearn/배열/보이는 학생/보이는 학생.java
@@ -1,32 +1,32 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Main {
-	public ArrayList<Integer> solution(int[] arr){
-		ArrayList<Integer> answer = new ArrayList<>();
-		int max = arr[0];
-		
-		answer.add(max);
-		for(int i=1; i<arr.length; i++)
-			if(arr[i]>max)
-			{
-				max=arr[i];
-				answer.add(max);
-			}
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int len= sc.nextInt();
-		int[] arr = new int[len];
-		
-		for(int i=0; i<len; i++)
-			arr[i]=sc.nextInt();
-		
-		System.out.println(T.solution(arr).size());
-	}
+public class Main {
+
+    public int solution(int n, int[] arr){
+
+        int answer = 1;
+        int max = arr[0];
+        for (int x : arr) {
+            if (x > max) {
+                answer++;
+                max=x;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        System.out.println(T.solution(n, arr));
+    }
 }
