2883b51f005d47e1cca9add94aeeb017be82de36
LeeJongsu <lik9819@naver.com>
2023-12-25 22:52:08 +0900

    Update 소수.java

--- inflearn/배열/소수/소수.java
+++ inflearn/배열/소수/소수.java
@@ -1,31 +1,29 @@
 // 예시 입력 20
 // 예시 출력 8
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
-class Main {
-	public ArrayList<Integer> solution(int n, int[] arr){
-		ArrayList<Integer> answer = new ArrayList<>();
+public class Main {
 
-		for(int i=2; i<n; i++) {
-			if(arr[i]==0)
-			{
-				answer.add(i);
-				for(int j=2; i*j<n; j++)
-					arr[i*j]=1;
-			}
-		}
-		
-		return answer;
-	}	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n= sc.nextInt()+1;
-		int[] arr = new int[n];
-		
-		System.out.println(T.solution(n, arr).size());
-	}
+    public int solution(int n) {
+        int answer = 0;
+
+        int[] arr = new int[n+1];
+        for (int i = 2; i <= n; i++) {
+            if(arr[i]==0) answer++;
+            for (int j = 2; i * j <= n; j++) arr[i * j] = 1;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(T.solution(n));
+    }
 }
