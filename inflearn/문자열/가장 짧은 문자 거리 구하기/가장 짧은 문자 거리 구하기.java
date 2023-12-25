83e6e2f4185695bf16c4c72c1a841d8441794774
LeeJongsu <lik9819@naver.com>
2023-12-25 16:47:57 +0900

    Update 가장 짧은 문자 거리 구하기.java

--- inflearn/문자열/가장 짧은 문자 거리 구하기/가장 짧은 문자 거리 구하기.java
+++ inflearn/문자열/가장 짧은 문자 거리 구하기/가장 짧은 문자 거리 구하기.java
@@ -1,89 +1,42 @@
+//예시 입력 teachermode e
+//예시 출력 1 0 12 1 0 1 2 2 1 0
 
 import java.util.Scanner;
 
-class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.next();
-		char ch = sc.next().charAt(0);
-		
-		int[] len = new int[str.length()];
-		int p = 101;
-		
-		for(int i=0; i< str.length(); i++) {
-			if(str.charAt(i)==ch) 
-				p=0;
-			else
-				++p;
-			len[i]=p;
-		}
-		
-		for(int i=str.length()-1; i>-1; i--) {
-			if(str.charAt(i)==ch)
-				p=0;
-			else
-				++p;
-			if(p<len[i])
-				len[i]=p;
-		}
-		for(int n : len)
-			System.out.print(n+" ");
-	}
+public class Main {
+
+    public int[] solution(String str, char c){
+
+        int[] answer = new int[str.length()];
+
+        int p = 101;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i)==c) p = 0;
+            else p++;
+            answer[i] = p;
+        }
+
+        for (int i = str.length()-1; i >=0; i--) {
+            if(str.charAt(i)==c) p = 0;
+            else p++;
+            answer[i] = Math.min(answer[i], p);
+        }
+
+        return answer;
+    }
+
+
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+
+        for (int x : T.solution(str, c)) {
+            System.out.print(x+" ");
+        }
+    }
 }
-
-// import java.util.Scanner;
-
-// class Main {
-	
-// 	public static void main(String[] args) {
-		
-// 		Scanner sc = new Scanner(System.in);
-		
-// 		String str = sc.next();
-// 		char ch = sc.next().charAt(0);
-		
-// 		int[] arr = new int[str.length()];
-// 		int len =0;
-		
-// 		for(int i=0; i<str.length(); i++)
-// 		{
-// 			if(str.charAt(i)==ch)
-// 			{
-// 				arr[i]=0;
-// 				len++;
-// 			}
-// 			else
-// 				arr[i]=-1;
-// 		}
-// 		int[] arr2 = new int[len];
-// 		for(int i=0,j=0;i< str.length(); i++)
-// 		{
-// 			if(str.charAt(i)==ch)
-// 			{
-// 				arr2[j]=i;
-// 				j++;
-// 			}
-// 		}
-// 		for(int i=0; i<str.length(); i++)
-// 		{
-// 			int min = Math.abs(arr2[0]-i);
-// 			if(str.charAt(i)!=ch)
-// 			{
-// 				for(int j=1; j<arr2.length; j++)
-// 				{
-// 					if((Math.abs(arr2[j]-i))<min)
-// 						min=Math.abs(arr2[j]-i);
-// 				}
-// 				System.out.print(min+" ");
-// 			}
-// 			else
-// 			{
-// 				System.out.print(0 + " ");
-// 			}
-// 		}
-// 	}
-// }
-
