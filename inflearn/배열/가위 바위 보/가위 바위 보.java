d7f7761eba114c691c9a72e6f8de181e7ef82f84
LeeJongsu <lik9819@naver.com>
2023-12-25 22:02:53 +0900

    Update 가위 바위 보.java

--- inflearn/배열/가위 바위 보/가위 바위 보.java
+++ inflearn/배열/가위 바위 보/가위 바위 보.java
@@ -1,36 +1,37 @@
 import java.util.Scanner;
 
-class Main {
-	public String solution(int n, int[] a, int[] b){
-		String answer="";
-		
-		for(int i=0; i<n; i++) {
-			switch(a[i]-b[i]) {
-			case -2 : answer+="A"; break;
-			case -1 : answer+="B"; break;
-			case 0 : answer+="D"; break;
-			case 1 : answer+="A"; break;
-			case 2 : answer+="B"; break;
-			}
-		}
-			
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n= sc.nextInt();
-		int[] a=  new int[n];
-		int[] b = new int[n];
-		
-		for(int i=0; i<n; i++)
-			a[i]=sc.nextInt();
-		for(int i=0; i<n; i++)
-			b[i]=sc.nextInt();
-		
-		for(int i=0; i<n; i++)
-			System.out.println(T.solution(n,a,b).charAt(i));
-	}
+public class Main {
+
+    public char[] solution(int n, int[] a, int[] b) {
+
+        char[] ch = new char[n];
+        for (int i = 0; i < n; i++) {
+            int result = a[i] - b[i];
+            switch (result) {
+                case -2 : ch[i] = 'A'; break;
+                case -1 : ch[i] = 'B'; break;
+                case 0 : ch[i] = 'D'; break;
+                case 1 : ch[i] = 'A'; break;
+                case 2 : ch[i] ='B'; break;
+            }
+        }
+
+        return ch;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) b[i] = sc.nextInt();
+
+        for (char c : T.solution(n, a, b)) {
+            System.out.println(c);
+        }
+    }
 }
