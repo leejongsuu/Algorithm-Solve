9dbb5364f634656c2207b3df35bf0df1527394d9
LeeJongsu <lik9819@naver.com>
2023-12-25 18:32:39 +0900

    Update 큰 수 출력하기.java

--- inflearn/배열/큰 수 출력하기/큰 수 출력하기.java
+++ inflearn/배열/큰 수 출력하기/큰 수 출력하기.java
@@ -1,18 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int cnt = sc.nextInt();
-		int[] arr = new int[cnt];
-		for(int i=0; i<cnt; i++)
-			arr[i]=sc.nextInt();
-		System.out.print(arr[0]+" ");
-		for(int i=1; i<cnt; i++)
-			if(arr[i]>arr[i-1])
-				System.out.print(arr[i]+" ");
-	}
+public class Main {
+
+    public ArrayList<Integer> solution(int n, int[] arr){
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i < n; i++) {
+            if(arr[i]>arr[i-1]) list.add(arr[i]);
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt()+1;
+        int[] arr = new int[n];
+
+        arr[0] = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) arr[i] = sc.nextInt();
+
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x+" ");
+        }
+    }
 }
