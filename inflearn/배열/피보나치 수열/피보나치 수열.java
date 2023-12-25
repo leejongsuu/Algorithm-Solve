380a5a67521c92e81c1aa513fca6c592474a9c73
LeeJongsu <lik9819@naver.com>
2023-12-25 22:31:26 +0900

    Update 피보나치 수열.java

--- inflearn/배열/피보나치 수열/피보나치 수열.java
+++ inflearn/배열/피보나치 수열/피보나치 수열.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
-class Main {
-	public int[] solution(int n, int[] arr){
-		
-		arr[0]=1;
-		arr[1]=1;
-		for(int i=2; i<n; i++)
-			arr[i]=arr[i-1]+arr[i-2];
-			
-		return arr;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n= sc.nextInt();
-		int[] arr = new int[n];
-		
-		for(int num : T.solution(n, arr) )
-			System.out.print(num+" ");
-	}
+public class Main {
+
+    public int[] solution(int n) {
+
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i = 2; i < n; i++) answer[i] = answer[i - 1] + answer[i - 2];
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int x : T.solution(n)) {
+            System.out.print(x+" ");
+        }
+    }
 }
