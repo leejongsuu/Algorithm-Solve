18880bd42eca28aa89003417a96a4fdb263655c3
LeeJongsu <lik9819@naver.com>
2023-12-25 17:28:34 +0900

    Update 문자열 압축.java

--- inflearn/문자열/문자열 압축/문자열 압축.java
+++ inflearn/문자열/문자열 압축/문자열 압축.java
@@ -1,58 +1,29 @@
-###풀이 1
 import java.util.Scanner;
 
-class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next()+" ";
-		String answer="";
-		int count = 1;
-		for(int i=0; i<str.length()-1; i++)
-			if(str.charAt(i)==str.charAt(i+1))
-				count++;
-			else
-			{
-				answer+=str.charAt(i);
-				if(count>1)
-					answer+=String.valueOf(count);
-				count=1;
-			}
-		System.out.println(answer);
-	}
-}
+public class Main {
 
-###풀이2
+    public String solution(String str){
 
-import java.util.Scanner;
+        String answer = "";
+        int len = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if(str.charAt(i)==str.charAt(i+1)) len++;
+            else {
+                answer += str.charAt(i);
+                if(len>1) answer += String.valueOf(len);
+                len = 1;
+            }
+        }
+        return answer;
+    }
 
-class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-		char[] ch =str.toCharArray();
-		for(int i=0; i<ch.length; i++)
-		{
-			int count =1;
-			if(Character.isAlphabetic(ch[i]))
-			{
-				for(int j=i+1; j<ch.length; j++)
-				{
-					if(ch[i]==ch[j])
-					{
-						count++;
-						ch[j]='!';
-					}
-					else
-						break;
-				}
-				System.out.print(ch[i]);
-				if(count>1)
-					System.out.print(count);
-			}
-		}
-	}
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next()+" ";
+
+        System.out.println(T.solution(str));
+    }
 }
