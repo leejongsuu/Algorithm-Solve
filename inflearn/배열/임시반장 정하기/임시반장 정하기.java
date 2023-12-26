653adb5e1ee3dcac90d7833f7fb9f63e6081b7fa
LeeJongsu <lik9819@naver.com>
2023-12-27 02:14:26 +0900

    Update 임시반장 정하기.java

--- inflearn/배열/임시반장 정하기/임시반장 정하기.java
+++ inflearn/배열/임시반장 정하기/임시반장 정하기.java
@@ -36,39 +36,46 @@ public int solution(int n, int[][] arr){
 // 맞은 풀이
 import java.util.Scanner;
 
-class Main {
-	
-	public int solution(int n, int[][] arr){
-		
-		int answer=0, max=Integer.MIN_VALUE;
-		for(int i=0; i<n; i++) {
-			int cnt =0;
-			for(int j=0; j<n; j++)
-				for(int k=0; k<5; k++)
-					if(i!=j && arr[i][k]==arr[j][k]) {
-						cnt++;
-						break;
-					}
-			if(cnt>max) {
-				max=cnt;
-				answer=i;
-			}
-		}
-		
-		return answer+1;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n= sc.nextInt();
-		int[][] arr = new int[n][5];
-		
-		for(int i=0; i<n; i++)
-			for(int j=0; j<5; j++)
-				arr[i][j]=sc.nextInt();
-		
-		System.out.println(T.solution(n, arr));
-	}
+public class Main {
+
+    public int solution(int n, int[][] arr) {
+
+        int answer = 0;
+        int max = Integer.MIN_VALUE;
+        int[] st = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        st[i]++;
+                        st[j]++;
+                        break;
+                    }
+                }
+            }
+            if (st[i] > max) {
+                max = st[i];
+                answer = i + 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][5];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(T.solution(n, arr));
+    }
 }
