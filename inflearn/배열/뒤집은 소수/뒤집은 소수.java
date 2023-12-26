b7302570785897b32f65d9e143f768683066907d
LeeJongsu <lik9819@naver.com>
2023-12-26 14:37:13 +0900

    Update 뒤집은 소수.java

--- inflearn/배열/뒤집은 소수/뒤집은 소수.java
+++ inflearn/배열/뒤집은 소수/뒤집은 소수.java
@@ -5,46 +5,45 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Main {
-	
-//소수 판별 
-	public boolean isPrime(int num) {
-		if(num==1) return false;
-		for(int i=2; i<num; i++) 
-			if(num%i==0)
-				return false;
-		return true;
-	}
-	
-//자연수 뒤집
-	public ArrayList<Integer> solution(int n, String[] str){
-		ArrayList<Integer> answer = new ArrayList<>();
-        int[] arr = new int[n];
-        
-		for(int i=0; i<n; i++) {
-			String rev = new StringBuffer(str[i]).reverse().toString();
-			arr[i]=Integer.parseInt(rev);
-			if(isPrime(arr[i])) answer.add(arr[i]);
-		}	
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n= sc.nextInt();
-		String[] str = new String[n];
-		
-		for(int i=0; i<n;) {
-			String tmp = sc.next();
-			if(tmp.charAt(0)!='0') {
-				str[i]=tmp;
-				i++;
-			}
-		}
-		
-		for(int result : T.solution(n, str))
-			System.out.print(result+" ");
-	}
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+     //소수 판별
+    public boolean number(int num) {
+
+        if(num==1) return false;
+        for (int i = 2; i < num; i++) {
+            if(num%i==0) return false;
+        }
+        return true;
+    }
+
+    public ArrayList<Integer> solution(int n, String[] str) {
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String reStr = new StringBuffer(str[i]).reverse().toString();
+            int x = Integer.parseInt(reStr);
+            if(number(x)) answer.add(x);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] str = new String[n];
+
+        for (int i = 0; i < n; i++) str[i] = sc.next();
+
+        for(int x : T.solution(n, str)) System.out.print(x + " ");
+
+    }
 }
