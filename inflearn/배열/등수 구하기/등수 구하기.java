28bf6c744a9ffcbcf98171e2eef11dfb3a5c70db
LeeJongsu <lik9819@naver.com>
2023-12-26 18:27:32 +0900

    Update 등수 구하기.java

--- inflearn/배열/등수 구하기/등수 구하기.java
+++ inflearn/배열/등수 구하기/등수 구하기.java
@@ -5,34 +5,30 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Main {
-	
-	public int[] solution(int n, int[] arr){
+public class Main {
 
-		int[] answer = new int[n];
-		
-		for(int i=0; i<n; i++) {
-			int cnt=1;
-			for(int j=0; j<n; j++) {
-				if(arr[j]>arr[i]) cnt++;
-			}
-			answer[i]=cnt;
-		}
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n= sc.nextInt();
-		int[] arr = new int[n];
-		
-		for(int i=0; i<n; i++)
-			arr[i]=sc.nextInt();
 
-		for(int i : T.solution(n, arr))
-			System.out.print(i+" ");
-		
-	}
+    public int[] solution(int n, int[] arr) {
+
+        int[] answer = new int[n];
+        Arrays.fill(answer, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) if(arr[j]>arr[i]) answer[i]++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        for(int x : T.solution(n,arr)) System.out.print(x + " ");
+    }
 }
