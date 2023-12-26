36ff3e2e44d043299bee36350b5545290150b98b
LeeJongsu <lik9819@naver.com>
2023-12-26 15:49:44 +0900

    Update 점수계산.java

--- inflearn/배열/점수계산/점수계산.java
+++ inflearn/배열/점수계산/점수계산.java
@@ -3,35 +3,33 @@
 
 import java.util.Scanner;
 
-class Main {
-	
-	public int solution(int n, int[] arr){
+public class Main {
 
-		int answer=0;
-		int p=1;
+    public int solution(int n, int[] arr) {
 
-		for(int i=0; i<n; i++) 
-			if(arr[i]==1) {
-				answer+=p;
-				p++;
-			}
-			else {
-				p=1;
-			}
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n= sc.nextInt();
-		int[] arr = new int[n];
-		for(int i=0; i<n; i++)
-			arr[i]=sc.nextInt();
-		
-		System.out.println(T.solution(n, arr));
-		
-	}
+        int answer = 0;
+        int pt = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                answer += pt;
+                pt++;
+            } else pt = 1;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        System.out.println(T.solution(n, arr));
+    }
 }
