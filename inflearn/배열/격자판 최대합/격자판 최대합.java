d7645a9eb9cbd0d57b88e2f1c9cfd0d3554b0de4
LeeJongsu <lik9819@naver.com>
2023-12-26 18:59:37 +0900

    Update 격자판 최대합.java

--- inflearn/배열/격자판 최대합/격자판 최대합.java
+++ inflearn/배열/격자판 최대합/격자판 최대합.java
@@ -8,112 +8,52 @@
 
 // 예시 출력 155
 
-
-//풀이 1
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
-class Main {
-	
-	public int solution(int n, int[][] arr){
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		int answer=0;
-		int sum=0;
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++)
-				sum+=arr[i][j];
-			list.add(sum);
-			sum=0;
-		}
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				sum+=arr[j][i];
-			}
-			list.add(sum);
-			sum=0;
-		}
-		
-		for(int i=0; i<n; i++) {
-			sum+=arr[i][i];
-		}
-		list.add(sum);
-		sum=0;
-		
-		for(int i=n-1; i>=0; i--) {
-			sum+=arr[i][i];
-		}
-		list.add(sum);
-		
-		Collections.sort(list);
-		answer = list.get(list.size()-1);
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n= sc.nextInt();
-		int[][] arr = new int[n][n];
-		
-		for(int i=0; i<n; i++)
-			for(int j=0; j<n; j++)
-				arr[i][j]=sc.nextInt();
-
-		System.out.println(T.solution(n, arr));
-	}
-}
+public class Main {
 
 
-// 풀이 2
-import java.util.Scanner;
+    public int solution(int n, int[][] arr) {
 
-class Main {
-	
-	public int solution(int n, int[][] arr){
-		
-		int answer = Integer.MIN_VALUE;
-		int sum1, sum2;
-		
-		for(int i=0; i<n; i++) {
-			sum1=sum2=0;
-			for(int j=0; j<n; j++) {
-				sum1+=arr[i][j];
-				sum2+=arr[j][i];
-			}
-			answer=Math.max(answer, sum1);
-			answer=Math.max(answer, sum2);
-		}
-		sum1=sum2=0;
-		for(int i=0; i<n; i++) {
-			sum1+=arr[i][i];
-			sum2=arr[i][n-1-i];
-		}
-		answer=Math.max(answer, sum1);
-		answer=Math.max(answer, sum2);
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n= sc.nextInt();
-		int[][] arr = new int[n][n];
-		
-		for(int i=0; i<n; i++)
-			for(int j=0; j<n; j++)
-				arr[i][j]=sc.nextInt();
+        int answer = Integer.MIN_VALUE;
 
-		System.out.println(T.solution(n, arr));
-	}
+        int sum1, sum2;
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+
+        sum1 = sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+
+        System.out.println(T.solution(n, arr));
+    }
 }
 
