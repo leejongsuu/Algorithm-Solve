b86716d0e2181c10aa123db1c47751f26380d063
LeeJongsu <lik9819@naver.com>
2023-12-28 00:57:48 +0900

    Update 공통원소 구하기.java

--- inflearn/Two pointers,Sliding window/공통원소 구하기/공통원소 구하기.java
+++ inflearn/Two pointers,Sliding window/공통원소 구하기/공통원소 구하기.java
@@ -12,44 +12,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Main {
-	
-	public ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
-		
-		ArrayList<Integer> answer = new ArrayList<>();
-		Arrays.sort(a);
-		Arrays.sort(b);
-		
-		int p1=0,p2=0;
-		
-		while(p1<n && p2<m) {
-			if(a[p1]<b[p2]) p1++;
-			else if (a[p1]>b[p2]) p2++;
-			else {
-				answer.add(a[p1]);
-				p1++;
-				p2++;
-			}
-		}
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int[] a = new int[n];
-		for(int i=0; i<n; i++)
-			a[i]=sc.nextInt();
-		
-		int m = sc.nextInt();
-		int[] b = new int[m];
-		for(int i=0; i<m; i++)
-			b[i]=sc.nextInt();
-		
-		for(int i : T.solution(n, m, a, b))
-			System.out.print(i+" ");
-	}
+public class Main {
+
+
+    public ArrayList<Integer> solution(int[] a, int[] b) {
+
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(a); Arrays.sort(b);
+
+        int lt = 0, rt = 0;
+        while (lt < a.length && rt < b.length) {
+            if(a[lt]<b[rt]) lt++;
+            else if(a[lt]>b[rt]) rt++;
+            else {
+                answer.add(a[lt]);
+                lt++;
+                rt++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+
+        int m = sc.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) b[i] = sc.nextInt();
+
+        for(int x : T.solution(a,b)) System.out.print(x + " ");
+    }
 }
