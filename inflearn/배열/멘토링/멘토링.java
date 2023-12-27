580b85dabe5d5d0781e49171475009632ed75264
LeeJongsu <lik9819@naver.com>
2023-12-28 00:16:18 +0900

    Update 멘토링.java

--- inflearn/배열/멘토링/멘토링.java
+++ inflearn/배열/멘토링/멘토링.java
@@ -1,3 +1,14 @@
+//예시 입력
+/*
+4 3
+3 4 1 2
+4 3 2 1
+3 1 4 2
+*/
+
+//예시 출력 3
+
+//풀이 1
 import java.util.Scanner;
 
 class Main {
@@ -38,4 +49,41 @@ class Main {
 		
 		System.out.println(T.solution(n, m ,arr));
 	}
+}
+
+//풀이 2
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int n, int m, int[][] arr) {
+
+        int answer = n * n - n;
+        int[][] number = new int[n][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                for (int k = j - 1; k >= 0; k--) {
+                    if (number[arr[i][j] - 1][arr[i][k] - 1] == 0) {
+                        number[arr[i][j] - 1][arr[i][k] - 1] = 1;
+                        answer--;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[m][n];
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) arr[i][j] = sc.nextInt();
+
+        System.out.println(T.solution(n, m, arr));
+    }
 }
