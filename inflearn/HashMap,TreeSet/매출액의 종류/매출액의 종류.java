2b9cbd8951a83b6fe8277f16857759f545ff0814
LeeJongsu <lik9819@naver.com>
2023-12-29 00:17:03 +0900

    Update 매출액의 종류.java

--- inflearn/HashMap,TreeSet/매출액의 종류/매출액의 종류.java
+++ inflearn/HashMap,TreeSet/매출액의 종류/매출액의 종류.java
@@ -2,7 +2,7 @@
 /*
 7 4
 20 12 20 10 23 17 10
-  */
+*/
 
 //예시 출력 3 4 4 3
 
@@ -10,39 +10,37 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-class Main {
-	
-	public ArrayList<Integer> solution(int n,int m, int[] arr){
-		ArrayList<Integer> answer = new ArrayList<>();
-		HashMap<Integer, Integer> map = new HashMap<>();
-		
-		for(int i=0; i<m-1; i++)
-			map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
-		int lt=0;
-		for(int rt=m-1; rt<n; rt++) {
-			map.put(arr[rt], map.getOrDefault(arr[rt], 0)+1);
-			answer.add(map.size());
-			map.put(arr[lt], map.get(arr[lt])-1);
-			if(map.get(arr[lt])==0)
-				map.remove(arr[lt]);
-			lt++;
-		}
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int[] arr = new int[n];
-		
-		for(int i=0; i<n; i++)
-			arr[i]=sc.nextInt();
-		
-		for(int i : T.solution(n, m, arr))
-			System.out.print(i+ " ");
-	}
+public class Main {
+
+
+    public ArrayList<Integer> solution(int n, int m, int[] arr) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int rt;
+        for (rt = 0; rt < m - 1; rt++) map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+
+        for (int lt = 0; rt < n; rt++, lt++) {
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+            answer.add(map.size());
+            map.put(arr[lt], map.get(arr[lt]) - 1);
+            if(map.get(arr[lt])==0) map.remove(arr[lt]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        for(int x : T.solution(n, m, arr)) System.out.print(x+" ");
+    }
 }
