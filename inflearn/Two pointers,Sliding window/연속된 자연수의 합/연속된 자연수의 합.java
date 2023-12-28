4926d10fbcaec55f9d6343a757692917203a4b6a
LeeJongsu <lik9819@naver.com>
2023-12-28 17:26:14 +0900

    Update 연속된 자연수의 합.java

--- inflearn/Two pointers,Sliding window/연속된 자연수의 합/연속된 자연수의 합.java
+++ inflearn/Two pointers,Sliding window/연속된 자연수의 합/연속된 자연수의 합.java
@@ -38,27 +38,28 @@ class Main {
 //풀이 2
 import java.util.Scanner;
 
-class Main {
-	
-	public int solution(int n){
-		
-		int answer=0, cnt = 1;
-		n--;
-		while(n>0) {
-			cnt++;
-			n=n-cnt;
-			if(n%cnt==0) answer++;
-		}
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-			
-		System.out.println(T.solution(n));
-	}
+public class Main {
+
+    public int solution(int n) {
+
+        int answer = 0;
+        n--;
+
+        for (int i = 2; n > 0; i++) {
+            n = n - i;
+            if(n%i==0) answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(T.solution(n));
+    }
 }
