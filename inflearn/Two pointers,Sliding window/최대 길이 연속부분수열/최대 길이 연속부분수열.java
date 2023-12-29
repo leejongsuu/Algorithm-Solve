6d436f84070c3cd9827aa7562f717507216c948b
LeeJongsu <lik9819@naver.com>
2023-12-29 19:26:40 +0900

    Update 최대 길이 연속부분수열.java

--- inflearn/Two pointers,Sliding window/최대 길이 연속부분수열/최대 길이 연속부분수열.java
+++ inflearn/Two pointers,Sliding window/최대 길이 연속부분수열/최대 길이 연속부분수열.java
@@ -14,13 +14,14 @@ class Main {
 		
 		int answer=0, cnt=0, lt=0;
 		for(int rt=0; rt<n; rt++) {
-			if(arr[rt]==0) cnt++;
-			while(cnt>m) {
-				if(arr[lt]==0) cnt--;
-				lt++;
+			if(arr[rt]==0) {
+				cnt++;
+				while(cnt>m) {
+					if(arr[lt]==0) cnt--;
+					lt++;
 			}
-			answer=Math.max(answer, rt-lt+1);
 		}
+		answer=Math.max(answer, rt-lt+1);
 		
 		return answer;
 	}
