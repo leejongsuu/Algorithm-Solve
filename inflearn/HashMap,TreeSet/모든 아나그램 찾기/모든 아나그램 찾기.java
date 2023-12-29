4db0a35b40fe3520ce63a6df42aadbebc138bb6a
LeeJongsu <lik9819@naver.com>
2023-12-30 01:59:53 +0900

    Update 모든 아나그램 찾기.java

--- inflearn/HashMap,TreeSet/모든 아나그램 찾기/모든 아나그램 찾기.java
+++ inflearn/HashMap,TreeSet/모든 아나그램 찾기/모든 아나그램 찾기.java
@@ -1,49 +1,37 @@
-//예시 입력
-/*
-bacaAacba
-abc
-*/
-
-//예시 출력 3
-
 import java.util.HashMap;
 import java.util.Scanner;
 
-class Main {
-	
-	public int solution(String aStr, String bStr){
-		int answer = 0;
-		HashMap<Character,Integer> aMap = new HashMap<>();
-		HashMap<Character,Integer> bMap = new HashMap<>();
-		
-		for(int i=0; i<bStr.length()-1; i++)
-			aMap.put(aStr.charAt(i), aMap.getOrDefault(aStr.charAt(i), 0)+1);
-		for(int i=0; i<bStr.length(); i++)
-			bMap.put(bStr.charAt(i), bMap.getOrDefault(bStr.charAt(i), 0)+1);
-		
-		int lt = 0;
-		for(int rt=bStr.length()-1; rt<aStr.length(); rt++) {
-			aMap.put(aStr.charAt(rt), aMap.getOrDefault(aStr.charAt(rt), 0)+1);
-			if(aMap.equals(bMap))
-				answer++;
-			if(aMap.get(aStr.charAt(lt))==1)
-				aMap.remove(aStr.charAt(lt));
-			else {
-				aMap.put(aStr.charAt(lt), aMap.get(aStr.charAt(lt))-1);
-			}
-			lt++;
-		}
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		String aStr = sc.nextLine();
-		String bStr = sc.nextLine();
+public class Main {
 
-		System.out.println(T.solution(aStr, bStr));
-	}
+    public int solution(String a, String b) {
+
+        int answer = 0;
+
+        HashMap<Character, Integer> S = new HashMap<>();
+        HashMap<Character, Integer> T = new HashMap<>();
+
+        for (char c : b.toCharArray()) T.put(c, T.getOrDefault(c, 0) + 1);
+        int lt = 0, rt;
+        for (rt = 0; rt < b.length() - 1; rt++) S.put(a.charAt(rt), S.getOrDefault(a.charAt(rt), 0) + 1);
+        for (; rt < a.length(); rt++) {
+            S.put(a.charAt(rt), S.getOrDefault(a.charAt(rt), 0) + 1);
+            if(S.equals(T)) answer++;
+            S.put(a.charAt(lt), S.get(a.charAt(lt)) - 1);
+            if(S.get(a.charAt(lt))==0) S.remove(a.charAt(lt));
+            lt++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(T.solution(a, b));
+    }
 }
