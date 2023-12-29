13003ef6400537f7e06c1162b47b3b234b85a8d7
LeeJongsu <lik9819@naver.com>
2023-12-30 02:43:27 +0900

    Update K번째 큰 수.java

--- inflearn/HashMap,TreeSet/K번째 큰 수/K번째 큰 수.java
+++ inflearn/HashMap,TreeSet/K번째 큰 수/K번째 큰 수.java
@@ -10,37 +10,40 @@ import java.util.Collections;
 import java.util.Scanner;
 import java.util.TreeSet;
 
-class Main {
-	
-	public int solution(int a, int b, int[] arr){
-		
-		int answer = -1;
-		TreeSet<Integer> ts = new TreeSet<>(Collections.reverseOrder());
-		
-		for(int i=0; i<a; i++)
-			for(int j=i+1; j<a; j++)
-				for(int k= j+1; k<a; k++)
-					ts.add(arr[i]+arr[j]+arr[k]);
-		int cnt =0;
-		for(int i : ts) {
-			cnt++;
-			if(cnt==b) return i;
-		}
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int[] arr = new int[a];
-		
-		for(int i=0; i<a; i++)
-			arr[i]=sc.nextInt();
-		
-		System.out.println(T.solution(a, b, arr));
-	}
+public class Main {
+
+
+    public int solution(int n, int m, int[] arr) {
+
+        int answer = -1;
+        TreeSet<Integer> tree = new TreeSet<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    tree.add(arr[i] + arr[j] + arr[k]);
+                }
+            }
+        }
+        int cnt = 1;
+        for (int x : tree) {
+            if(cnt==m) return x;
+            cnt++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        System.out.println(T.solution(n, m, arr));
+    }
 }
