55d8dbf4e6033393b2ef4d7bbcdff2f08571ed34
LeeJongsu <lik9819@naver.com>
2023-12-30 22:56:12 +0900

    Update 쇠막대기.java

--- inflearn/Stack,Queue/쇠막대기/쇠막대기.java
+++ inflearn/Stack,Queue/쇠막대기/쇠막대기.java
@@ -4,33 +4,34 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-class Main {
-	
-	public int solution(String str){
-		int answer = 0;
-		Stack<Character> stack = new Stack<>();
-		
-		stack.push(str.charAt(0));
-		for(int i =1; i<str.length(); i++) {
-			if(str.charAt(i)=='(')
-				stack.push(str.charAt(i));
-			else {
-				stack.pop();
-				if(str.charAt(i)==str.charAt(i-1)) answer++;
-				else answer+=stack.size();
-			}
-		}
-		
-		return answer;
-	}
-	
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.next();
-		
-		System.out.println(T.solution(str));
-	}
+public class Main {
+
+
+    public int solution(String str) {
+
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+
+        stack.push(str.charAt(0));
+        for (int i = 1; i < str.length(); i++) {
+            if(str.charAt(i)=='(') stack.push(str.charAt(i));
+            else{
+                stack.pop();
+                if (str.charAt(i) == str.charAt(i - 1)) answer++;
+                else answer += stack.size();
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(T.solution(str));
+    }
 }
