ff7405ce24bd625d40f08d5a18377787acce498c
LeeJongsu <lik9819@naver.com>
2023-12-30 21:40:14 +0900

    Update 후위식 연산.java

--- inflearn/Stack,Queue/후위식 연산/후위식 연산.java
+++ inflearn/Stack,Queue/후위식 연산/후위식 연산.java
@@ -3,34 +3,43 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-class Main {
-	
-	public int solution(String str){
-		int answer = 0;
-		Stack<Integer> stack = new Stack<>();
-		
-		for(char c : str.toCharArray()) {
-			if(Character.isDigit(c)) stack.push(c-48);
-			else {
-				int rt = stack.pop();
-				int lt = stack.pop();
-				if(c=='+') stack.push(lt+rt);
-				else if(c=='-') stack.push(lt-rt);
-				else if(c=='*') stack.push(lt*rt);
-				else if(c=='/') stack.push(lt/rt);
-			}
-		}
-		answer=stack.get(0);
-		return answer;
-	}
-	
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.next();
-		
-		System.out.println(T.solution(str));
-	}
+public class Main {
+
+
+    public int solution(String str) {
+
+        int answer;
+        Stack<Integer> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                stack.push(c - '0');
+            } else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+                switch (c){
+                    case '+' : stack.push(lt+ rt); break;
+
+                    case '*' : stack.push(lt * rt); break;
+
+                    case '/' :  stack.push(lt / rt); break;
+
+                    case '-' :  stack.push(lt - rt); break;
+                }
+            }
+        }
+        answer = stack.pop();
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(T.solution(str));
+    }
 }
