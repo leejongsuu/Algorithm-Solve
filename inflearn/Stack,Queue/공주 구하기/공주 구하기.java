e3323872ba18001e2dc60b194ad7b6de7aba4de1
LeeJongsu <lik9819@naver.com>
2023-12-30 23:12:38 +0900

    Update 공주 구하기.java

--- inflearn/Stack,Queue/공주 구하기/공주 구하기.java
+++ inflearn/Stack,Queue/공주 구하기/공주 구하기.java
@@ -7,24 +7,26 @@ import java.util.Scanner;
 
 class Main {
 
-  // 풀이 1 큐 이용
-	public int solution(int n, int k){
-		int answer = 0;
-		Queue<Integer> queue = new LinkedList<>();
-		for(int i=1; i<n+1; i++)
-			queue.offer(i);
-		
-		while(queue.size()!=1) {
-			int i = 1;
-			while(i<k) {
-				queue.offer(queue.poll());
-				i++;
-			}
-			queue.poll();
-		}
-		answer=queue.poll();
-		return answer;
-	}
+// 풀이 1 큐 이용
+public int solution(int n, int m) {
+
+        int answer;
+        Queue<Integer> Q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) Q.offer(i);
+
+        int cnt = 1;
+        while (Q.size() != 1) {
+            int x = Q.poll();
+            if (cnt == m) cnt = 1;
+            else {
+                Q.offer(x);
+                cnt++;
+            }
+        }
+
+        answer = Q.poll();
+        return answer;
+    }
 
   //풀이 2 스택 이용
   public int solution2(int n, int k){
