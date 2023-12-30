3dd33951dd98d26e3bd308f2632ff4ce8f8f8ac2
LeeJongsu <lik9819@naver.com>
2023-12-30 15:46:45 +0900

    Update 괄호 문자 제거.java

--- inflearn/Stack,Queue/괄호 문자 제거/괄호 문자 제거.java
+++ inflearn/Stack,Queue/괄호 문자 제거/괄호 문자 제거.java
@@ -1,6 +1,40 @@
 // 예시 입력 (A(BC)D)EF(G(H)(IJ)K)LM(N)
 // 예시 출력 EFLM
 
+
+//풀이 1
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+
+    public String solution(String str) {
+
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if(c=='(') stack.push('(');
+            else if(c==')') stack.pop();
+            else if(stack.isEmpty()) answer += c;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(T.solution(str));
+    }
+}
+
+//풀이 2
 import java.util.Scanner;
 import java.util.Stack;
 
