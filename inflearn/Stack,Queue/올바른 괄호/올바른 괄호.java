263848acd8049381e97dba50801378ab7755bbdc
LeeJongsu <lik9819@naver.com>
2023-12-30 15:28:11 +0900

    Update 올바른 괄호.java

--- inflearn/Stack,Queue/올바른 괄호/올바른 괄호.java
+++ inflearn/Stack,Queue/올바른 괄호/올바른 괄호.java
@@ -4,37 +4,32 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-class Main {
-	
-	public boolean solution(String str){
-		boolean answer = true;
-		Stack<Character> stack = new Stack<>();
-		
-		for(char c : str.toCharArray()) {
-			if(c=='(')
-				stack.add('(');
-			else {
-				if(stack.isEmpty())
-					return false;
-				stack.pop();
-			}
-		}
-		if(!stack.isEmpty()) 
-			return false;
-		
-		return answer;
-	}
-	
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.next();
-		
-		if(T.solution(str))
-			System.out.println("YES");
-		else
-			System.out.println("NO");
-	}
+public class Main {
+
+
+    public String solution(String str) {
+
+        String answer = "NO";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if(c=='(') stack.push('(');
+            else if(stack.isEmpty()) return "NO";
+            else stack.pop();
+        }
+
+        if(stack.empty()) return "YES";
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(T.solution(str));
+    }
 }
