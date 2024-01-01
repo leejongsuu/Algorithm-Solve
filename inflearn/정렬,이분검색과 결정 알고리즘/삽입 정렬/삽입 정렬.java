926bf71681fec4b77a45e0ad86fb154377cb9900
LeeJongsu <lik9819@naver.com>
2024-01-01 23:03:44 +0900

    Update 삽입 정렬.java

--- inflearn/정렬,이분검색과 결정 알고리즘/삽입 정렬/삽입 정렬.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/삽입 정렬/삽입 정렬.java
@@ -10,30 +10,29 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int[] solution(int n, int[] arr){
+    public int[] solution(int n, int[] arr) {
 
-        for(int i=1; i<n; i++){
-            int tmp=arr[i], j;
-            for(j=i-1;j>=0; j--){
-                if(arr[j]>tmp) arr[j+1] = arr[j];
+        for (int i = 1; i < n; i++) {
+            int tmp = arr[i], j;
+            for (j = i - 1; j >= 0; j--) {
+                if (tmp < arr[j]) arr[j + 1] = arr[j];
                 else break;
             }
             arr[j+1] = tmp;
         }
+
         return arr;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
-        for(int i=0; i<n; i++)
-            arr[i]=sc.nextInt();
-
-        for(int i : T.solution(n, arr))
-            System.out.print(i+" ");
+        for(int x : T.solution(n, arr)) System.out.print(x+" ");
     }
 }
