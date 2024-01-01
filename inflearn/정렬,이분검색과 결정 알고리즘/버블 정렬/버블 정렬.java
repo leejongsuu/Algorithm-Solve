556fcad78da517afb2babfb7c6e312b869eab5f7
LeeJongsu <lik9819@naver.com>
2024-01-01 23:03:23 +0900

    Update 버블 정렬.java

--- inflearn/정렬,이분검색과 결정 알고리즘/버블 정렬/버블 정렬.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/버블 정렬/버블 정렬.java
@@ -6,14 +6,13 @@
 
 // 예시 출력 5 7 11 13 15 23
 
-
 import java.util.Scanner;
 
 public class Main {
 
-    public int[] solution(int n, int[] arr){
+    public int[] solution(int n, int[] arr) {
 
-        for(int i=0; i<n-1; i++)
+        for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
@@ -21,20 +20,20 @@ public class Main {
                     arr[j + 1] = tmp;
                 }
             }
+        }
+
         return arr;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
-        for(int i=0; i<n; i++)
-            arr[i]=sc.nextInt();
-
-        for(int i : T.solution(n, arr))
-            System.out.print(i+" ");
+        for(int x : T.solution(n, arr)) System.out.print(x+" ");
     }
 }
