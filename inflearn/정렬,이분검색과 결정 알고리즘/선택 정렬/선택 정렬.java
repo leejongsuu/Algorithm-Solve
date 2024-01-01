5d184ec7dbf95b8fca6bf4d7a3ed366939179198
LeeJongsu <lik9819@naver.com>
2024-01-01 23:02:51 +0900

    Update 선택 정렬.java

--- inflearn/정렬,이분검색과 결정 알고리즘/선택 정렬/선택 정렬.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/선택 정렬/선택 정렬.java
@@ -10,12 +10,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int[] solution(int n, int[] arr){
+    public int[] solution(int n, int[] arr) {
 
-        for(int i=0; i<n-1; i++){
+        for (int i = 0; i < n - 1; i++) {
             int idx = i;
-            for(int j=i+1; j<n; j++)
-                if(arr[j]<arr[idx]) idx = j;
+            for (int j = i + 1; j < n; j++)
+                if (arr[idx] > arr[j]) idx = j;
             int tmp = arr[i];
             arr[i] = arr[idx];
             arr[idx] = tmp;
@@ -24,17 +24,15 @@ public class Main {
         return arr;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
-        for(int i=0; i<n; i++)
-            arr[i]=sc.nextInt();
-
-        for(int i : T.solution(n, arr))
-            System.out.print(i+" ");
+        for(int x : T.solution(n, arr)) System.out.print(x+" ");
     }
 }
