8e3e9f55202ac25d579d1cd55a103d2bad7796b1
LeeJongsu <lik9819@naver.com>
2024-01-02 23:40:42 +0900

    Update 중복 확인.java

--- inflearn/정렬,이분검색과 결정 알고리즘/중복 확인/중복 확인.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/중복 확인/중복 확인.java
@@ -6,29 +6,28 @@
 
 //예시 출력 D
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public char solution(int n, int[] arr){
-
-        char answer = 'U';
-        Arrays.sort(arr);
-        for(int i=0; i<n-1; i++)
-            if(arr[i]==arr[i+1]) return 'D';
-        return answer;
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        ArrayList<Integer> list = new ArrayList<>(n);
+
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            int x = sc.nextInt();
+            if (list.contains(x)) {
+                System.out.println("D");
+                return;
+            }
+            list.add(x);
         }
-        System.out.println(T.solution(n,arr));
+
+        System.out.println("U");
     }
 }
