34f262123b62e5fb38ed31473dda48b2e6e00ba8
LeeJongsu <lik9819@naver.com>
2024-01-03 23:53:27 +0900

    Update 뮤직비디오.java

--- inflearn/정렬,이분검색과 결정 알고리즘/뮤직비디오(결정알고리즘)/뮤직비디오.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/뮤직비디오(결정알고리즘)/뮤직비디오.java
@@ -11,42 +11,46 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int count(int[] arr, int capacity){
+    public int count(int mid, int[] arr) {
+
         int cnt = 1, sum = 0;
-        for(int i : arr){
-            if(sum+i>capacity) {
-                cnt++;
+        for (int x : arr) {
+            if (sum + x > mid) {
                 sum = 0;
+                cnt++;
             }
-            sum+=i;
+            sum += x;
         }
         return cnt;
     }
 
-    public int solution(int n, int m, int[] arr) {
+    public int solution(int m, int[] arr) {
+
         int answer = 0;
+
         int lt = Arrays.stream(arr).max().getAsInt();
         int rt = Arrays.stream(arr).sum();
-
-        while (lt<=rt){
-            int mid=(rt+lt)/2;
-            if(count(arr,mid)<=m){
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(mid, arr) <= m) {
                 answer = mid;
                 rt = mid - 1;
             } else lt = mid + 1;
         }
+
         return answer;
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
-        System.out.println(T.solution(n,m, arr));
+        System.out.println(T.solution(m, arr));
     }
 }
