35d387ce4f1f04ae26692fac7d3e0a3cdf5b9b99
LeeJongsu <lik9819@naver.com>
2024-01-06 17:41:05 +0900

    Update 마구간 정하기.java

--- inflearn/정렬,이분검색과 결정 알고리즘/마구간 정하기(결정알고리즘)/마구간 정하기.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/마구간 정하기(결정알고리즘)/마구간 정하기.java
@@ -9,45 +9,52 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Main {
 
-    public int count(int[] arr, int dist){
-        int cnt = 1;
-        int ep = arr[0];
-        for(int i=1; i<arr.length; i++)
-            if(arr[i]-ep>=dist){
-                cnt++;
-                ep=arr[i];
-            }
+    public int count(int mid, int[] arr) {
 
+        int cnt = 1, ep = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[ep] >= mid) {
+                ep = i;
+                cnt++;
+            }
+        }
         return cnt;
     }
 
-    public int solution(int n, int m, int[] arr) {
+    public int solution(int m, int[] arr) {
+
         int answer = 0;
         Arrays.sort(arr);
-        int lt=1;
-        int rt = arr[n-1]-arr[0];
 
-        while (lt<=rt){
-            int mid=(rt+lt)/2;
-            if(count(arr,mid)>=m){
+        int lt = 1;
+        int rt = arr[arr.length - 1] - arr[0];
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(mid, arr) < m) {
+                rt = mid - 1;
+            } else{
                 answer = mid;
-                lt = mid+1;
-            } else rt = mid -1;
+                lt = mid + 1;
+            }
         }
+
         return answer;
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
-        System.out.println(T.solution(n,m, arr));
+        System.out.println(T.solution(m, arr));
     }
 }
