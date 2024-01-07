cd33250a362828e3a52962571734b52dbdc6b170
LeeJongsu <lik9819@naver.com>
2024-01-08 00:13:51 +0900

    Update 송아지 찾기.java

--- inflearn/BFS/송아지 찾기/송아지 찾기.java
+++ inflearn/BFS/송아지 찾기/송아지 찾기.java
@@ -1,41 +1,47 @@
+//예시 입력 5 14
+//예시 출력 3
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-
 public class Main {
 
-    public int BFS(int s, int e){
-        int level = 0;
+    public int BFS(int s, int e) {
+
         ArrayList<Integer> list = new ArrayList<>();
         Queue<Integer> Q = new LinkedList<>();
         int[] dis = new int[]{1, -1, 5};
-        list.add(s);
+        int level = 0;
         Q.offer(s);
+        list.add(s);
         while (!Q.isEmpty()) {
             int len = Q.size();
-            for(int i =0; i<len; i++){
-                int p = Q.poll();
-                if(p==e) return level;
+            for (int i = 0; i < len; i++) {
+                int tmp = Q.poll();
+                if(tmp==e) return level;
                 else
-                    for(int x : dis){
-                        if(!list.contains(p + x)){
-                            list.add(p + x);
-                            Q.offer(p + x);
+                    for (int x : dis) {
+                        if (!list.contains(tmp + x)) {
+                            list.add(tmp + x);
+                            Q.offer(tmp + x);
                         }
                     }
             }
             level++;
         }
-        return level;
+        return 0;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         int s = sc.nextInt();
         int e = sc.nextInt();
-        System.out.println(T.BFS(s,e));
+
+        System.out.println(T.BFS(s, e));
     }
 }
