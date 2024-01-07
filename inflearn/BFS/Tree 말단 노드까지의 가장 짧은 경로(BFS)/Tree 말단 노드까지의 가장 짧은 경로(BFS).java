27335aee6811cc8c4bc658cdb797fd7c205d1693
LeeJongsu <lik9819@naver.com>
2024-01-08 00:22:29 +0900

    Update Tree 말단 노드까지의 가장 짧은 경로(BFS).java

--- inflearn/BFS/Tree 말단 노드까지의 가장 짧은 경로(BFS)/Tree 말단 노드까지의 가장 짧은 경로(BFS).java
+++ inflearn/BFS/Tree 말단 노드까지의 가장 짧은 경로(BFS)/Tree 말단 노드까지의 가장 짧은 경로(BFS).java
@@ -1,41 +1,49 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Node{
+class Node {
     int data;
     Node lt, rt;
-    public Node(int val){
-        data=val;
-        lt=rt=null;
+
+    public Node(int data) {
+        this.data = data;
+        lt = rt = null;
     }
 }
 
 public class Main {
+
     Node root;
-    public int BFS(Node root){
+
+    public int BFS(Node root) {
+
         Queue<Node> Q = new LinkedList<>();
-        Q.offer(root);
+
         int level = 0;
+        Q.offer(root);
         while (!Q.isEmpty()) {
             int len = Q.size();
-            for(int i=0; i<len; i++){
-                Node temp = Q.poll();
-                if(temp.lt==null & temp.rt == null) return level;
-                if(temp.lt !=null) Q.offer(temp.lt);
-                if(temp.rt !=null) Q.offer(temp.rt);
+            for (int i = 0; i < len; i++) {
+                Node tmp = Q.poll();
+                if (tmp.lt == null && tmp.rt == null) return level;
+                else if(tmp.rt ==null) Q.offer(tmp.lt);
+                else Q.offer(tmp.rt);
             }
             level++;
         }
         return level;
     }
 
-    public static void main(String[] args) {
-        Main tree = new Main();
-        tree.root = new Node(1);
-        tree.root.lt = new Node(2);
-        tree.root.rt = new Node(3);
-        tree.root.lt.lt = new Node(4);
-        tree.root.lt.rt = new Node(5);
-        System.out.println(tree.BFS(tree.root));
+    public static void main(String[] args){
+
+        Main T = new Main();
+
+        T.root = new Node(1);
+        T.root.lt = new Node(2);
+        T.root.rt = new Node(3);
+        T.root.lt.lt = new Node(4);
+        T.root.lt.rt = new Node(5);
+
+        System.out.println(T.BFS(T.root));
     }
 }
