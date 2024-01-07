a4b8a09c4082a8a408265b70a721d40cf7a3e6ab
LeeJongsu <lik9819@naver.com>
2024-01-07 23:30:09 +0900

    Update 부분 집합 구하기.java

--- inflearn/DFS/부분 집합 구하기/부분 집합 구하기.java
+++ inflearn/DFS/부분 집합 구하기/부분 집합 구하기.java
@@ -13,26 +13,34 @@
 import java.util.Scanner;
 
 public class Main {
-   static  int n;
-   static int[] ch;
-    public void DFS(int L){
+
+    static int n;
+    static int[] ch;
+
+    public void DFS(int L) {
+
         if(L==n+1){
-          for(int i=1; i<=n; i++)
-              if(ch[i]==1) System.out.print(i+ " ");
+            for (int i = 1; i <= n; i++)
+                if(ch[i]==1) System.out.print(i+" ");
             System.out.println();
-        }else {
-            ch[L]=1;
+        }
+        else {
+            ch[L] = 1;
             DFS(L + 1);
-            ch[L]=0;
+            ch[L] = 0;
             DFS(L + 1);
         }
+
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         n = sc.nextInt();
-        ch = new int[n+1];
+        ch = new int[n + 1];
+
         T.DFS(1);
     }
 }
