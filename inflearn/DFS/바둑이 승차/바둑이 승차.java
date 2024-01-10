e67869a4f4a906b1afc0fbb3e97bbbc42ce8d854
LeeJongsu <lik9819@naver.com>
2024-01-10 22:36:26 +0900

    Update 바둑이 승차.java

--- inflearn/DFS/바둑이 승차/바둑이 승차.java
+++ inflearn/DFS/바둑이 승차/바둑이 승차.java
@@ -12,29 +12,31 @@
 import java.util.Scanner;
 
 public class Main {
-    static int n, total, max=0;
+
+    static int total, n, max = 0;
     static int[] arr;
 
-    public void DFS(int L, int sum){
-        if(sum>total) return;
-        if(L==n) max = Math.max(max, sum);
+    public void DFS(int L, int sum) {
+        if (sum>total) return;
+
+        if (L==n) max = Math.max(max, sum);
         else {
             DFS(L + 1, sum + arr[L]);
             DFS(L + 1, sum);
         }
-
     }
 
     public static void main(String[] args) {
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         total = sc.nextInt();
         n = sc.nextInt();
         arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        T.DFS(0, 0);
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        T.DFS(0,0);
         System.out.println(max);
     }
 }
