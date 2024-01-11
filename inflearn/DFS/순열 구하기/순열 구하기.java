0c5d06a28cc525b077041d794395d0ac7696dc95
LeeJongsu <lik9819@naver.com>
2024-01-11 22:09:31 +0900

    Update 순열 구하기.java

--- inflearn/DFS/순열 구하기/순열 구하기.java
+++ inflearn/DFS/순열 구하기/순열 구하기.java
@@ -16,36 +16,41 @@
 import java.util.Scanner;
 
 public class Main {
-    static int n, m;
-    static int[] arr, pm, ch;
 
-    public void DFS(int L){
-       if(L==m) {
-           for(int x : pm) System.out.print(x+ " ");
-           System.out.println();
-       } else{
-           for (int i = 0; i < n; i++) {
-               if(ch[i]==0){
-                   ch[i] = 1;
-                   pm[L] = arr[i];
-                   DFS(L + 1);
-                   ch[i]=0;
-               }
-           }
-       }
+    static int n, m;
+    static int[] num, arr, ch;
+    
+    public void DFS(int L) {
+
+        if (L == m) {
+            for (int x : arr) System.out.print(x + " ");
+            System.out.println();
+        }
+        else {
+            for (int i = 0; i < n; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    arr[L] = num[i];
+                    DFS(L + 1);
+                    ch[i] = 0;
+                }
+            }
+        }
+
     }
 
     public static void main(String[] args) {
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         n = sc.nextInt();
         m = sc.nextInt();
-        arr = new int[n];
+        num = new int[n];
         ch = new int[n];
-        pm = new int[m];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+        arr = new int[m];
+        for (int i = 0; i < n; i++) num[i] = sc.nextInt();
+
         T.DFS(0);
     }
 }
