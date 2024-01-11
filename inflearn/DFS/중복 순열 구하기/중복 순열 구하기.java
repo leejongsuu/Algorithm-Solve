a74f23274d1a72107d51068bff7786e53437a117
LeeJongsu <lik9819@naver.com>
2024-01-11 21:31:52 +0900

    Update 중복 순열 구하기.java

--- inflearn/DFS/중복 순열 구하기/중복 순열 구하기.java
+++ inflearn/DFS/중복 순열 구하기/중복 순열 구하기.java
@@ -15,27 +15,34 @@
 import java.util.Scanner;
 
 public class Main {
-    static int n,m;
+
+    static int n;
+    static int m;
     static int[] arr;
-    public void DFS(int L){
-        if(L==m) {
-            for(int x : arr)
-                System.out.print(x+" ");
+
+    public void DFS(int L) {
+
+        if (L == m) {
+            for (int x : arr) System.out.print(x+" ");
             System.out.println();
-        }else {
-            for(int i=1; i<=n; i++){
-                arr[L]=i;
+        }
+        else {
+            for (int i=1; i<=n; i++){
+                arr[L] = i;
                 DFS(L + 1);
             }
         }
+
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         n = sc.nextInt();
         m = sc.nextInt();
         arr = new int[m];
+
         T.DFS(0);
     }
 }
