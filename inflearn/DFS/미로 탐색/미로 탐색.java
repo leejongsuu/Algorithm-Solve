feca4aca0471724a0f023dca24110a602d744c43
LeeJongsu <lik9819@naver.com>
2024-01-12 16:35:08 +0900

    Update 미로 탐색.java

--- inflearn/DFS/미로 탐색/미로 탐색.java
+++ inflearn/DFS/미로 탐색/미로 탐색.java
@@ -14,35 +14,39 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] maze = new int[7][7];
-    static int answer = 0;
+    static int n, answer = 0;
+    static int[] moveX = {-1, 0, 1, 0};
+    static int[] moveY = {0, -1, 0, 1};
+    static int[][] board;
 
     public void DFS(int x, int y) {
-       if(x==6&&y==6) answer++;
-       else{
-           for(int i=0; i<4; i++){
-               int nx = x + dx[i];
-               int ny = y + dy[i];
-               if (nx >= 0 && nx <= 6 && ny >= 0 && ny <= 6 && maze[nx][ny] == 0) {
-                   maze[nx][ny] = 1;
-                   DFS(nx, ny);
-                   maze[nx][ny] = 0;
-               }
-           }
-       }
+
+        if (x == n-1 && y == n-1) answer++;
+        else {
+            for (int i = 0; i < 4; i++) {
+                int nx = x + moveX[i];
+                int ny = y + moveY[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 0) {
+                    board[x][y] = 1;
+                    DFS(nx, ny);
+                    board[x][y] = 0;
+                }
+            }
+        }
+
     }
 
     public static void main(String[] args) {
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                maze[i][j] = sc.nextInt();
-            }
-        }
-        maze[0][0]=1;
+
+        n = sc.nextInt();
+        board = new int[n][n];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) board[i][j] = sc.nextInt();
+
         T.DFS(0, 0);
         System.out.println(answer);
     }
