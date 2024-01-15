8de5013aed32117c1124622f82ded3320718ec8a
LeeJongsu <lik9819@naver.com>
2024-01-15 17:30:28 +0900

    Update 섬나라 아일랜드.java

--- inflearn/BFS/섬나라 아일랜드/섬나라 아일랜드.java
+++ inflearn/BFS/섬나라 아일랜드/섬나라 아일랜드.java
@@ -15,9 +15,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-class Point{
-    int x;
-    int y;
+class Point {
+    int x, y;
 
     public Point(int x, int y) {
         this.x = x;
@@ -27,47 +26,49 @@ class Point{
 
 public class Main {
 
-    static int n, answer=0;
-    static int[][] box;
+    static int n;
     static int[] dx = {-1, 0, 1, 0, -1, -1, 1, 1};
     static int[] dy = {0, 1, 0, -1, 1, -1, 1, -1};
+    static int[][] board;
     static Queue<Point> Q = new LinkedList<>();
 
     public void BFS(int x, int y) {
-        Q.add(new Point(x, y));
+
+        Q.offer(new Point(x, y));
         while (!Q.isEmpty()) {
             Point p = Q.poll();
-            for (int i = 0; i < 8; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n && box[nx][ny] == 1) {
-                    box[nx][ny] = 0;
-                    Q.add(new Point(nx, ny));
+            board[p.x][p.y] = 0;
+            for (int j = 0; j < 8; j++) {
+                int nx = p.x + dx[j];
+                int ny = p.y + dy[j];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+                    Q.offer(new Point(nx, ny));
                 }
             }
         }
     }
 
     public static void main(String[] args) {
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
 
+        int answer = 0;
         n = sc.nextInt();
-        box = new int[n][n];
-        for (int i = 0; i < n; i++) {
+        board = new int[n][n];
+
+        for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++) {
-                box[i][j] = sc.nextInt();
+                board[i][j] = sc.nextInt();
             }
-        }
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++) {
-                if (box[i][j] == 1) {
-                    box[i][j] = 0;
-                    answer++;
+                if (board[i][j] == 1) {
                     T.BFS(i, j);
+                    answer++;
                 }
             }
-        }
 
         System.out.println(answer);
     }
