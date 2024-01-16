7d8ce152264dfd5ef690576d806ef4edf3e1ca8d
LeeJongsu <lik9819@naver.com>
2024-01-16 17:06:14 +0900

    Update 피자 배달 거리 구하기.java

--- inflearn/DFS/피자 배달 거리 구하기/피자 배달 거리 구하기.java
+++ inflearn/DFS/피자 배달 거리 구하기/피자 배달 거리 구하기.java
@@ -12,13 +12,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Point{
-    int x;
-    int y;
+class Point {
+    int y, x;
 
-    public Point(int x, int y) {
-        this.x = x;
+    public Point(int y, int x) {
         this.y = y;
+        this.x = x;
     }
 }
 
@@ -26,13 +25,14 @@ public class Main {
 
     static int n, m, len, answer = Integer.MAX_VALUE;
     static int[] combi;
-    static ArrayList<Point> home, pizza;
+    static ArrayList<Point> pizza, home;
 
-    public int searchMin(int[] arr) {
+    public int searchMin(int[] combi) {
+
         int sum = 0;
         for (Point h : home) {
             int min = Integer.MAX_VALUE;
-            for (int x : arr) {
+            for (int x : combi) {
                 Point p = pizza.get(x);
                 min = Math.min(min, Math.abs(h.x - p.x) + Math.abs(h.y - p.y));
             }
@@ -42,33 +42,35 @@ public class Main {
     }
 
     public void DFS(int L, int s) {
-        if (L == m) {
-            answer = Math.min(answer, searchMin(combi));
-        } else {
-            for (int i = s; i < len; i++) {
+
+        if(L==m) answer = Math.min(answer, searchMin(combi));
+        else {
+            for (int i =s; i<len; i++) {
                 combi[L] = i;
                 DFS(L + 1, i + 1);
             }
         }
+
     }
 
     public static void main(String[] args) {
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
         m = sc.nextInt();
         combi = new int[m];
-        home = new ArrayList<>();
         pizza = new ArrayList<>();
+        home = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++){
                 int data = sc.nextInt();
-                if (data == 1) home.add(new Point(i, j));
-                else if (data == 2) pizza.add(new Point(i, j));
+                if(data==1) home.add(new Point(i, j));
+                else if(data==2) pizza.add(new Point(i, j));
             }
-        }
+
         len = pizza.size();
 
         T.DFS(0, 0);
