a05cbc879e34ed35072240b1d9f25d2f861744d0
LeeJongsu <lik9819@naver.com>
2024-01-16 15:27:13 +0900

    Update 섬나라 아일랜드.java

--- inflearn/DFS/섬나라 아일랜드/섬나라 아일랜드.java
+++ inflearn/DFS/섬나라 아일랜드/섬나라 아일랜드.java
@@ -12,46 +12,57 @@
 
 //예시 출력 5
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+
+class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class Main {
 
-    static int n, answer = 0;
-    static int[][] box;
-    static int[] dx = {-1, 0, 1, 0, -1, -1, 1, 1};
-    static int[] dy = {0, 1, 0, -1, 1, -1, 1, -1};
+    static int n;
+    static int[] dx = {0, -1, 0, 1, -1, 1, 1, -1};
+    static int[] dy = {-1, 0, 1, 0, 1, 1, -1, -1};
+    static int[][] maze;
 
     public void DFS(int x, int y) {
-        for (int i = 0; i < 8; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && box[nx][ny] == 1) {
-                box[nx][ny] = 0;
+
+        maze[x][y] = 0;
+        for (int j = 0; j < 8; j++) {
+            int nx = x + dx[j];
+            int ny = y + dy[j];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && maze[nx][ny] == 1)
                 DFS(nx, ny);
-            }
         }
     }
 
     public static void main(String[] args) {
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt();
-        box = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) {
-                box[i][j] = sc.nextInt();
-            }
+        int answer = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (box[i][j] == 1) {
-                    box[i][j] = 0;
-                    answer++;
+        n = sc.nextInt();
+        maze = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) maze[i][j] = sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (maze[i][j] == 1) {
                     T.DFS(i, j);
+                    answer++;
                 }
-            }
-        }
+
         System.out.println(answer);
+
     }
 }
