9fcf29cd2f41244f102a922aaba8a74e07d1132f
LeeJongsu <lik9819@naver.com>
2024-01-16 15:22:20 +0900

    Update 섬나라 아일랜드.java

--- inflearn/BFS/섬나라 아일랜드/섬나라 아일랜드.java
+++ inflearn/BFS/섬나라 아일랜드/섬나라 아일랜드.java
@@ -27,23 +27,23 @@ class Point {
 public class Main {
 
     static int n;
-    static int[] dx = {-1, 0, 1, 0, -1, -1, 1, 1};
-    static int[] dy = {0, 1, 0, -1, 1, -1, 1, -1};
-    static int[][] board;
-    static Queue<Point> Q = new LinkedList<>();
+    static int[] dx = {0, -1, 0, 1, -1, 1, 1, -1};
+    static int[] dy = {-1, 0, 1, 0, 1, 1, -1, -1};
+    static int[][] maze;
 
     public void BFS(int x, int y) {
 
+        Queue<Point> Q = new LinkedList<>();
         Q.offer(new Point(x, y));
+
         while (!Q.isEmpty()) {
             Point p = Q.poll();
-            board[p.x][p.y] = 0;
+            maze[p.x][p.y] = 0;
             for (int j = 0; j < 8; j++) {
                 int nx = p.x + dx[j];
                 int ny = p.y + dy[j];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && maze[nx][ny] == 1)
                     Q.offer(new Point(nx, ny));
-                }
             }
         }
     }
@@ -54,22 +54,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int answer = 0;
+
         n = sc.nextInt();
-        board = new int[n][n];
+        maze = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) maze[i][j] = sc.nextInt();
 
         for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) {
-                board[i][j] = sc.nextInt();
-            }
-
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1) {
+            for (int j = 0; j < n; j++)
+                if (maze[i][j] == 1) {
                     T.BFS(i, j);
                     answer++;
                 }
-            }
 
         System.out.println(answer);
+
     }
 }
