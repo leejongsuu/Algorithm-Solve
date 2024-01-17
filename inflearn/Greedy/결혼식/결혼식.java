6d7fc9db853fc444f732cfd5e5f5425a3bc245e7
LeeJongsu <lik9819@naver.com>
2024-01-17 22:49:46 +0900

    Update 결혼식.java

--- inflearn/Greedy/결혼식/결혼식.java
+++ inflearn/Greedy/결혼식/결혼식.java
@@ -14,56 +14,58 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-class Order implements Comparable<Order>{
+class Time implements Comparable<Time> {
+
     int t;
     Character c;
 
-    public Order(int t, Character c) {
+    public Time(int t, Character c) {
         this.t = t;
         this.c = c;
     }
 
     @Override
-    public int compareTo(Order o) {
-        if(this.t==o.t) return this.c.compareTo(o.c);
+    public int compareTo(Time o) {
+        if (this.t == o.t) return this.c.compareTo(o.c);
         else return this.t - o.t;
     }
 }
 
-
 public class Main {
 
-    static int n;
-    static ArrayList<Order> Person;
+    public int solution(ArrayList<Time> list) {
 
-    public int Greedy() {
+        int answer = Integer.MIN_VALUE;
+        Collections.sort(list);
 
-        int answer = 0;
-        Collections.sort(Person);
-
-        int cnt =0;
-        for (Order o : Person) {
-            if(o.c.equals('s')){
-               cnt++;
-               answer = Math.max(answer, cnt);
-            } else if(o.c.equals('e')) cnt--;
+        int cnt = 0;
+        for (Time T : list) {
+            if (T.c.equals('s')) {
+                cnt++;
+                answer = Math.max(answer, cnt);
+            } else if (T.c.equals('e')) {
+                cnt--;
+            }
         }
 
         return answer;
     }
 
     public static void main(String[] args) {
+
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        Person = new ArrayList<>(n);
+
+        int n = sc.nextInt();
+        ArrayList<Time> list = new ArrayList<>(5);
+
         for (int i = 0; i < n; i++) {
             int start = sc.nextInt();
             int end = sc.nextInt();
-            Person.add(new Order(start,'s'));
-            Person.add(new Order(end,'e'));
+            list.add(new Time(start, 's'));
+            list.add(new Time(end, 'e'));
         }
 
-        System.out.println(T.Greedy());
+        System.out.println(T.solution(list));
     }
 }
