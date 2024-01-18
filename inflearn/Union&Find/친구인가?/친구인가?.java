23fd7fb39fa44abc60bd67b61f1194f445fc1efa
LeeJongsu <lik9819@naver.com>
2024-01-18 17:36:54 +0900

    Update 친구인가?.java

--- inflearn/Union&Find/친구인가?/친구인가?.java
+++ inflearn/Union&Find/친구인가?/친구인가?.java
@@ -13,36 +13,47 @@
 
 //예시 출력 NO
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
     static int[] unf;
 
-    public static int Find(int v) {
-        if(v==unf[v]) return v;
+    public int Find(int v) {
+        if (v == unf[v]) return v;
         else return unf[v] = Find(unf[v]);
     }
 
-    public static void Union(int a, int b) {
+    public void Union(int a, int b) {
         int fa = Find(a);
         int fb = Find(b);
-        if(fa!=fb) unf[fa] = fb;
+        if (fa != fb) unf[fa] = fb;
+    }
+
+    public String solution(int a, int b) {
+        if(Find(a)==Find(b)) return "YES";
+        else return "NO";
     }
 
     public static void main(String[] args) {
+
+        Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int m = sc.nextInt();
+
         unf = new int[n + 1];
         for (int i = 1; i <= n; i++) unf[i] = i;
-        for (int i = 1; i <= m; i++) {
+        for (int i = 0; i < m; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            Union(a, b);
+            T.Union(a, b);
         }
+
         int a = sc.nextInt();
         int b = sc.nextInt();
-        if(Find(a)==Find(b)) System.out.println("YES");
-        else System.out.println("NO");
+
+        System.out.println(T.solution(a, b));
     }
 }
