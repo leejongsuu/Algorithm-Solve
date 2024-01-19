d49b3249eac402aebde30f8d917478cf8d2f128a
LeeJongsu <lik9819@naver.com>
2024-01-19 17:48:11 +0900

    Update 원더랜드.java

--- inflearn/최소 신장트리/원더랜드/원더랜드.java
+++ inflearn/최소 신장트리/원더랜드/원더랜드.java
@@ -107,38 +107,49 @@ class Edge implements Comparable<Edge> {
 public class Main {
 
     static int[] ch;
-    static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
+    static ArrayList<ArrayList<Edge>> list = new ArrayList<>();
 
-    public static int solution(int v) {
+    public int dijk(int v) {
+
         int answer = 0;
         PriorityQueue<Edge> pQ = new PriorityQueue<>();
+
         pQ.offer(new Edge(v, 0));
         while (!pQ.isEmpty()) {
-            Edge e = pQ.poll();
-            int i = e.vex;
-            if (ch[i] == 0) {
-                ch[i]=1;
-                answer += e.cost;
-                for(Edge tmp : graph.get(i))
-                    if(ch[tmp.vex]==0) pQ.offer(new Edge(tmp.vex, tmp.cost));
+
+            Edge o = pQ.poll();
+            int now = o.vex;
+            if (ch[now] == 0) {
+                ch[now] = 1;
+                answer += o.cost;
+                for (Edge ob : list.get(now)) {
+                    if(ch[ob.vex]==0) pQ.offer(new Edge(ob.vex, ob.cost));
+                }
             }
         }
+
         return answer;
     }
 
     public static void main(String[] args) {
+
+        Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        ch = new int[n + 1];
-        for (int i = 0; i <=n; i++) graph.add(new ArrayList<>());
-        for (int i = 0; i < m; i++) {
+
+        int v = sc.nextInt();
+        int e = sc.nextInt();
+
+        ch = new int[v + 1];
+        for (int i = 0; i <= v; i++) list.add(new ArrayList<>());
+
+        for (int i = 0; i < e; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int c = sc.nextInt();
-            graph.get(a).add(new Edge(b, c));
-            graph.get(b).add(new Edge(a, c));
+            list.get(a).add(new Edge(b, c));
+            list.get(b).add(new Edge(a, c));
         }
-        System.out.println(solution(1));
+
+        System.out.println(T.dijk(1));
     }
 }
