4fd54ca6105c952ba6062addcd54821d73968316
LeeJongsu <lik9819@naver.com>
2024-01-24 17:36:01 +0900

    Update 계단 오르기.java

--- inflearn/DP/계단 오르기/계단 오르기.java
+++ inflearn/DP/계단 오르기/계단 오르기.java
@@ -7,20 +7,24 @@ public class Main {
 
     static int[] dy;
 
-    public  int solution(int n) {
+    public int solution(int n) {
+
         dy[1] = 1;
         dy[2] = 2;
         for (int i = 3; i <= n; i++) {
-            dy[i] = dy[i - 2] + dy[i - 1];
+            dy[i] = dy[i - 1] + dy[i - 2];
         }
         return dy[n];
     }
 
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         dy = new int[n + 1];
+
         System.out.println(T.solution(n));
     }
 }
