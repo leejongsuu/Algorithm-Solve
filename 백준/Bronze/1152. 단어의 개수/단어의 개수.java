e93237ab1522823e689786d735c7764c9fd1aba3
LeeJongsu <lik9819@naver.com>
2024-01-28 16:05:34 +0900

    Update 단어의 개수.java

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
-import java.util.Arrays;
-class Main {
-	
-	public static void main(String[] args) {
-	
-		Scanner sc = new Scanner(System.in);
-		
-		String str= sc.nextLine();
-		int a = 1;
-		if(str.length()==1 && str.charAt(0)==' ')
-		{
-			System.out.println(0);
-			return;
-		}
-		
-		str= str.trim();
-		
-		for(int i =0; i<str.length(); i++)
-		{
-			if(str.charAt(i)==' ')
-			{
-				a++;
-			}
-		}
-		System.out.println(a);
-	}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine().trim();
+        if (str.isEmpty()) {
+            System.out.println(0);
+        } else {
+
+            int cnt = 1;
+            for (char c : str.toCharArray()) {
+                if (c == ' ') {
+                    cnt++;
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
 }
