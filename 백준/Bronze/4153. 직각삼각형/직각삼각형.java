6f77616ae7dba6d2cef373a52372279927ee6cac
LeeJongsu <lik9819@naver.com>
2024-01-28 18:12:09 +0900

    [Bronze III] Title: 직각삼각형, Time: 208 ms, Memory: 17464 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -8,27 +5,30 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
 
         while (sc.hasNext()) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int c = sc.nextInt();
             if (a==0 || b==0 ||c==0) break;
-            list.add(a);
-            list.add(b);
-            list.add(c);
-            list.sort(Comparator.reverseOrder());
 
-            a = list.get(0);
-            b = list.get(1);
-            c = list.get(2);
-            if (Math.pow(b, 2) + Math.pow(c, 2) == Math.pow(a, 2)) {
+            if (a > c) {
+                int temp = a;
+                a = c;
+                c = temp;
+            }
+
+            if (b > c) {
+                int temp = b;
+                b = c;
+                c = temp;
+            }
+
+            if (a * a + b * b == c * c) {
                 System.out.println("right");
             } else {
                 System.out.println("wrong");
-            }
-            list.clear();
+            } 
         }
     }
 }
