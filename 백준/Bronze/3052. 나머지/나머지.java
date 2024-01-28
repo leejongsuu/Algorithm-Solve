c375cf8e2ed41d46ac0ab9bdef22937c03b48536
LeeJongsu <lik9819@naver.com>
2024-01-28 16:37:25 +0900

    Update 나머지.java

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,19 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int[] a = new int[10];
-		int num = 0;
-		for (int i = 0; i < 10; i++)
-			a[i] = s.nextInt() % 42;
-		for (int i = 0; i < 10; i++) {
-			for (int j = 9; j > i; j--)
-				if (a[i] == a[j]) {
-					num++;
-					break;
-				}
-		}
-		System.out.println(10 - num);
-	}
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int cnt = 0;
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt() % 42;
+            if (!list.contains(n)) {
+                cnt++;
+                list.add(n);
+            }
+        }
+        System.out.println(cnt);
+    }
 }
