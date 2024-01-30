2dd059c83e6f09bba8a5b411ed08bf07a5ef4a0a
LeeJongsu <lik9819@naver.com>
2024-01-30 22:02:26 +0900

    Update 분해합.java

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,17 +1,14 @@
-import java.io.*;
-
-
 public class Main {
 
-    public int solution (String str) {
-
+    public int solution(String str) {
+        
         //자릿수 길이
         int len = str.length();
 
-        int result = 0;
         int n = Integer.parseInt(str);
 
         for (int i = n - (len * 9); i < n; i++) {
+
             int num = i;
             int sum = 0;
 
@@ -20,24 +17,22 @@ public class Main {
                 num /= 10;
             }
 
-            if (sum + i == n) {
-                result = i;
-                break;
+            if ((sum + i) == n) {
+                return i;
             }
         }
-
-        return result;
+        return 0;
     }
 
     public static void main(String[] args) throws IOException {
 
         Main T = new Main();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //자릿수의 길이를 알기 위해 일단 문자열로 입력 받음.
+        //자릿수의 길이를 알기 위해 일단 문자열로 입력받음.
         String str = br.readLine();
 
         System.out.println(T.solution(str));
     }
 }
-
