7206c8c39ba69d50be5f9cdaa3e50b879adc6f13
LeeJongsu <lik9819@naver.com>
2024-02-02 18:00:22 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 116 ms, Memory: 13964 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,37 +1,43 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.io.IOException;
+ 
 public class Main {
-
-    static int floor, roomNum;
-    static int[][] dy = new int[15][15];
-
-    public int DFS(int f, int r) {
-
-        if (dy[f][r] != 0) {
-            return dy[f][r];
-        } else {
-            return dy[f][r] = DFS(f, r - 1) + DFS(f - 1, r);
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int i = 1; i <= 14; i++){
-            dy[0][i] = i;
-            dy[i][1] = 1;
-        }
-
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            floor = Integer.parseInt(br.readLine());
-            roomNum = Integer.parseInt(br.readLine());
-
-            System.out.println(T.DFS(floor, roomNum));
-        }
-    }
+ 
+	public static int[][] APT = new int[15][15];
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		make_APT();	// 아파트 만들기 
+		
+		int T = Integer.parseInt(br.readLine());
+ 
+		for (int i = 0; i < T; i++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			sb.append(APT[k][n]).append('\n');
+		}
+		System.out.println(sb);
+	}
+ 
+	
+	public static void make_APT() {
+		// 아파트 생성
+ 
+		for (int i = 0; i < 15; i++) {
+			APT[i][1] = 1; // i층 1호
+			APT[0][i] = i; // 0층 i호
+		}
+ 
+		for (int i = 1; i < 15; i++) { // 1층부터 14층까지
+ 
+			for (int j = 2; j < 15; j++) { // 2호부터 14호까지
+				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+			}
+		}
+	}
+ 
 }
