1e0d45ad15b50b6e4e16b03247815ed91b868778
LeeJongsu <lik9819@naver.com>
2024-02-06 12:36:52 +0900

    [Silver V] Title: 영화감독 숌, Time: 296 ms, Memory: 86512 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -2,26 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public int solution(int n) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        int num = 666, cnt = 1;
-
-        while (N != cnt) {
+        int cnt = 1, num = 666;
+        while (n!=cnt) {
             num++;
 
-            //int형인 num을 String으로 변환한 뒤, "666"이란 문자열이 있는지 검사
-            if (String.valueOf(num).contains("666")){
+            if (String.valueOf(num).contains("666")) {
                 cnt++;
             }
         }
+        return num;
+    }
 
-        System.out.println(num);
+    public static void main(String[] args) throws IOException {
+
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(T.solution(n));
     }
 }
