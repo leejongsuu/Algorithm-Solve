94f1d46b731947ecf9f2e59476d455d8e6cec0ba
LeeJongsu <lik9819@naver.com>
2024-02-07 18:05:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+
+    public static int solution(int n) {
+
+        int[] salt = new int[n + 1];
+        int[] bag = {3, 5};
+
+        Arrays.fill(salt, Integer.MAX_VALUE);
+        salt[0] = 0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = bag[i]; j <=n; j++) {
+                if (salt[j-bag[i]]==Integer.MAX_VALUE) continue;
+                salt[j] = Math.min(salt[j], salt[j - bag[i]] + 1);
+            }
+        }
+
+        return salt[n] == Integer.MAX_VALUE ? -1 : salt[n];
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(solution(n));
+    }
+}
