7a973a5c0ff182f468f337629bb36bf90f0cc174
LeeJongsu <lik9819@naver.com>
2024-02-08 13:13:32 +0900

    [Silver IV] Title: 수 찾기, Time: 592 ms, Memory: 47424 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -11,6 +11,7 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+
         String[] nInput = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(nInput[i]);
@@ -19,15 +20,16 @@ public class Main {
         Arrays.sort(arr);
 
         StringBuilder sb = new StringBuilder();
-
         int m = Integer.parseInt(br.readLine());
         String[] mInput = br.readLine().split(" ");
+
+
         for (int i = 0; i < m; i++) {
-            if (Arrays.binarySearch(arr, Integer.parseInt(mInput[i])) < 0) {
-                sb.append(0).append('\n');
-            } else {
+            if (Arrays.binarySearch(arr, Integer.parseInt(mInput[i])) >= 0) {
                 sb.append(1).append('\n');
-            }
+            } else {
+                sb.append(0).append('\n');
+            } 
         }
 
         System.out.println(sb);
