d01c7ea2b545f5a32319efdce968e0d312fde66f
LeeJongsu <lik9819@naver.com>
2024-02-15 20:33:33 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze II/2231. 분해합/분해합.java
+++ 백준/Bronze II/2231. 분해합/분해합.java
@@ -1,43 +1,34 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public int solution (String str) {
+    public static int solution(String str) {
 
-        //자릿수 길이
         int len = str.length();
+        int num = Integer.parseInt(str);
 
-        int result = 0;
-        int n = Integer.parseInt(str);
-
-        for (int i = n - (len * 9); i < n; i++) {
-            int num = i;
-            int sum = 0;
-
-            while (num != 0) {
-                sum += num % 10;
-                num /= 10;
+        for (int i = (num - (9 * len)); i < num; i++) {
+            int value = i;
+            int sum = i;
+            while (value != 0) {
+                sum += value % 10;
+                value /= 10;
             }
 
-            if (sum + i == n) {
-                result = i;
-                break;
-            }
+            if (sum == num) return i;
         }
 
-        return result;
+        return 0;
     }
 
     public static void main(String[] args) throws IOException {
 
-        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //자릿수의 길이를 알기 위해 일단 문자열로 입력 받음.
-        String str = br.readLine();
+        String input = br.readLine();
 
-        System.out.println(T.solution(str));
+        System.out.println(solution(input));
     }
 }
-
