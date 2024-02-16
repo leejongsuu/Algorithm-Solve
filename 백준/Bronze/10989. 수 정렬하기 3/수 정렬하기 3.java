2d60819dedc66f1dbfaa15fa300a659b4dfbf05a
LeeJongsu <lik9819@naver.com>
2024-02-16 23:16:57 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1636 ms, Memory: 336940 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -7,26 +7,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] cnt = new int[10001];
 
-        //수의 범위 (0~ 10000) 사실상 0은 제외
-        int[] count = new int[10001];
-
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            count[Integer.parseInt(br.readLine())]++;
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            cnt[Integer.parseInt(br.readLine())]++;
         }
 
-        br.close();
-
-        StringBuilder sb = new StringBuilder();
-
-        //0은 입력범위에서 없으므로 1부터 시작
         for (int i = 1; i < 10001; i++) {
-            //i 값이 계수가 0이 될 때 까지 출력
-            while (count[i] > 0) {
+            while (cnt[i] > 0) {
                 sb.append(i).append('\n');
-                count[i]--;
+                cnt[i]--;
             }
         }
 
