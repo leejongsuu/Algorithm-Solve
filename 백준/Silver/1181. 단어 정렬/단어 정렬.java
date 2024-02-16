c59d856e24eb48f49dded6cb26a6dd2bad8fa36c
LeeJongsu <lik9819@naver.com>
2024-02-16 23:21:00 +0900

    [Silver V] Title: 단어 정렬, Time: 448 ms, Memory: 26076 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -4,15 +4,14 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-
         String[] str = new String[n];
 
         for (int i = 0; i < n; i++) {
@@ -20,8 +19,10 @@ public class Main {
         }
 
         Arrays.stream(str)
-                .sorted(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()))
                 .distinct()
-                .forEach(System.out::println);
+                .sorted(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()))
+                .forEach(m -> sb.append(m).append('\n'));
+
+        System.out.println(sb);
     }
 }
