75e3326fcb922c7f69090e384029c0e0f2b8cfd1
LeeJongsu <lik9819@naver.com>
2024-02-19 00:19:07 +0900

    [Silver IV] Title: 수 찾기, Time: 556 ms, Memory: 43692 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,34 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    static int[] arr;
+
+    public int find(int key) {
+
+        if (Arrays.binarySearch(arr, key) >= 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        String[] nInput = br.readLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(nInput[i]);
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr);
 
-        StringBuilder sb = new StringBuilder();
-        int m = Integer.parseInt(br.readLine());
-        String[] mInput = br.readLine().split(" ");
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
 
-
-        for (int i = 0; i < m; i++) {
-            if (Arrays.binarySearch(arr, Integer.parseInt(mInput[i])) >= 0) {
-                sb.append(1).append('\n');
-            } else {
-                sb.append(0).append('\n');
-            } 
+        br.close();
+        for (int i = 0; i < M; i++) {
+            sb.append(T.find(Integer.parseInt(st.nextToken()))).append('\n');
         }
 
         System.out.println(sb);
