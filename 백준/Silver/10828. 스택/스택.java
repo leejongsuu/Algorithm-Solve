21af60c110602614ae7417fe2f5106a9e50ed78e
LeeJongsu <lik9819@naver.com>
2024-02-19 22:55:52 +0900

    [Silver IV] Title: 스택, Time: 196 ms, Memory: 19788 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,68 +2,62 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class Main {
 
     static int[] arr;
     static int index = -1;
 
-    public static void push(int x) {
+    static void push(int x) {
         arr[++index] = x;
     }
 
-    public static int pop() {
-        if (size() > 0) {
-            return arr[index--];
-        } else {
-            return -1;
-        }
-    }
-
-    public static int size() {
-        return index + 1;
-    }
-
-    public static int empty() {
+    static int pop() {
         if (size() == 0) {
-            return 1;
+            return -1;
         } else {
-            return 0;
+            return arr[index--];
         }
     }
 
-    public static int top() {
-        if (size() > 0) {
-            return arr[index];
-        } else {
-            return -1;
-        }
+    static int size() {
+        if (index < 0) return 0;
+        else return index + 1;
+    }
+
+    static int empty() {
+        if (size() == 0) return 1;
+        else return 0;
+    }
+
+    static int top() {
+        if (size() == 0) return -1;
+        else return arr[index];
     }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
 
-        int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-
-        String[] input;
-        for (int i = 0; i < n; i++) {
-            input = br.readLine().split(" ");
             switch (input[0]) {
-                case "push" :
+                case "push":
                     push(Integer.parseInt(input[1]));
                     break;
-                case "pop" :
+                case "pop":
                     sb.append(pop()).append('\n');
                     break;
-                case "size" :
+                case "size":
                     sb.append(size()).append('\n');
                     break;
-                case "empty" :
+                case "empty":
                     sb.append(empty()).append('\n');
                     break;
-                case "top" :
+                case "top":
                     sb.append(top()).append('\n');
                     break;
             }
