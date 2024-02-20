18e626062875e1ea110a65bd979b697d8d97a699
LeeJongsu <lik9819@naver.com>
2024-02-20 22:31:08 +0900

    [Silver IV] Title: 듣보잡, Time: 316 ms, Memory: 26676 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        TreeSet<String> answer = new TreeSet<>();
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+            if (map.containsKey(str)) {
+                answer.add(str);
+            }
+        }
+
+        sb.append(answer.size()).append('\n');
+        for (String s : answer) {
+            sb.append(s).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
