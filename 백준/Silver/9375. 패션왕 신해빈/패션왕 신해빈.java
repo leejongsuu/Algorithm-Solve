260d3b5d2210146e4908185aab3ca8de03fe0d20
LeeJongsu <lik9819@naver.com>
2024-02-24 21:11:20 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -7,7 +7,7 @@ public class Main {
 
     static int solution(String[] strArr) {
 
-        int sum = 1; // 모든 의상을 선택하지 않는 경우를 고려하여 1로 초기화합니다.
+        int sum = 1; //모든 의상을 선택하지 않는 경우를 고려하여 1로 초기화.
 
         HashMap<String, Integer> map = new HashMap<>();
         for (String s : strArr) {
@@ -15,10 +15,10 @@ public class Main {
         }
 
         for (Integer value : map.values()) {
-            sum *= (value + 1); // 각 의상 종류를 선택하지 않는 경우를 고려하여 1을 더해줍니다.
+            sum *= (value + 1); // 각 의상 종류를 선택하지 않는 경우를 고려하여 1을 더함.
         }
 
-        return sum - 1; // 모든 의상을 선택하지 않는 경우를 제외합니다.
+        return sum - 1; // 모든 의상을 선택하지 않는 경우를 제외.
     }
 
     public static void main(String[] args) throws IOException {
