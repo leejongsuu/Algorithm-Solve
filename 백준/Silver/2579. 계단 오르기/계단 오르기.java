9b026a53000616bb480bcf80e72fd3a427ed5178
LeeJongsu <lik9819@naver.com>
2024-02-24 19:39:57 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] stair = new int[N + 1];
+        int[] dy = new int[N + 1];
+
+
+        for (int i = 1; i <= N; i++) {
+            stair[i] = Integer.parseInt(br.readLine());
+        }
+
+        dy[1] = stair[1];
+        if (N >= 2) {
+            dy[2] = stair[1] + stair[2];
+        }
+
+        for (int i = 3; i <= N; i++) {
+            dy[i] = Math.max(dy[i - 3] + stair[i - 1], dy[i - 2]) + stair[i];
+        }
+
+        System.out.println(dy[N]);
+    }
+}
