0ec13a984d755b00862980d9fcc36476bc5516d4
LeeJongsu <lik9819@naver.com>
2024-02-25 20:53:42 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -5,20 +5,21 @@ import java.util.HashMap;
 
 public class Main {
 
-    static int solution(String[] strArr) {
+    static int solution(String[] str) {
 
-        int sum = 1; //모든 의상을 선택하지 않는 경우를 고려하여 1로 초기화.
+        int sum = 1;
 
         HashMap<String, Integer> map = new HashMap<>();
-        for (String s : strArr) {
+        for (String s : str) {
             map.put(s, map.getOrDefault(s, 0) + 1);
         }
 
-        for (Integer value : map.values()) {
-            sum *= (value + 1); // 각 의상 종류를 선택하지 않는 경우를 고려하여 1을 더함.
-        }
 
-        return sum - 1; // 모든 의상을 선택하지 않는 경우를 제외.
+        for (Integer value : map.values()) {
+            sum *= (value + 1);
+        }
+        
+        return sum - 1;
     }
 
     public static void main(String[] args) throws IOException {
@@ -29,15 +30,12 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < T; i++) {
-
             int N = Integer.parseInt(br.readLine());
-            String[] input = new String[N];
-
+            String[] str = new String[N];
             for (int j = 0; j < N; j++) {
-                input[j] = br.readLine().split(" ")[1];
+                str[j] = br.readLine().split(" ")[1];
             }
-
-            sb.append(solution(input)).append('\n');
+            sb.append(solution(str)).append('\n');
         }
 
         System.out.println(sb);
