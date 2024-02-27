3df2a9ca8fa25fb97baca0f3a8359cc5f2e29ae9
LeeJongsu <lik9819@naver.com>
2024-02-27 17:46:28 +0900

    [Silver II] Title: 최소 힙, Time: 332 ms, Memory: 26788 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Comparator.naturalOrder());
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pQ.isEmpty()) {
+                    sb.append(0).append('\n');
+                } else {
+                    sb.append(pQ.poll()).append('\n');
+                }
+            } else {
+                pQ.offer(x);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
