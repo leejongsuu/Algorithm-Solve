577efb5836ef9b7bd9441e528bf7edb35cdad9f1
LeeJongsu <lik9819@naver.com>
2024-02-28 21:09:22 +0900

    [Silver II] Title: 나무 자르기, Time: 1000 ms, Memory: 119428 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(N, M, arr));
+    }
+
+    static long solution(int n, int m, int[] arr) {
+
+        Arrays.sort(arr);
+        long lo = 0;
+        long hi = arr[n - 1];
+
+        while (lo < hi) {
+
+            long mid = (lo + hi) / 2;
+
+            long sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] > mid) {
+                    sum += arr[i] - mid;
+                }
+            }
+
+            if (sum >= m) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return lo - 1;
+    }
+
+}
