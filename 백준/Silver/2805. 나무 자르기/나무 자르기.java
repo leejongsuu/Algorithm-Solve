3aed7f1314e36e0a72cfcf9f01fbb83e4cecb2a1
LeeJongsu <lik9819@naver.com>
2024-02-29 15:41:22 +0900

    [Silver II] Title: 나무 자르기, Time: 524 ms, Memory: 119380 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -10,49 +10,49 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] tree = new int[N];
-
         int max = 0;
+        int[] tree = new int[N];
 
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             tree[i] = Integer.parseInt(st.nextToken());
-
-            if (max < tree[i]) {
+            if (tree[i] > max) {
                 max = tree[i];
             }
         }
 
-        System.out.println(solution(max, M, tree));
+        System.out.println(binarySearch(max, M, tree));
     }
 
-    static long solution(int hi, int m, int[] tree) {
+    static int binarySearch(int hi, int M, int[] tree) {
 
         int lo = 0;
 
         while (lo < hi) {
-            
             int mid = (lo + hi) / 2;
-            long sum = 0;
 
-            for (int h : tree) {
-                if (h - mid > 0) {
-                    sum += h - mid;
+            long sum = 0;
+            for (int i = 0; i < tree.length; i++) {
+                int sub = tree[i] - mid;
+                if (sub > 0) {
+                    sum += sub;
                 }
             }
 
-            if (sum < m) {
+            if (sum < M) {
                 hi = mid;
             } else {
                 lo = mid + 1;
             }
         }
-        
         return lo - 1;
     }
-
 }
+
+
+
+
+
