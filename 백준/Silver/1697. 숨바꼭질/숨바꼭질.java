25cd73525a960443e9cf0fbcad4eae311f258757
LeeJongsu <lik9819@naver.com>
2024-03-02 20:21:16 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 18404 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -6,48 +6,54 @@ import java.util.Queue;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
-        int K = Integer.parseInt(input[1]);
+        int M = Integer.parseInt(input[1]);
+
+        System.out.println(BFS(N, M));
+    }
+
+    static int BFS(int n, int m) {
 
         int size = 100001;
         boolean[] visited = new boolean[size];
 
         Queue<Integer> Q = new LinkedList<>();
-        Q.offer(N);
+        Q.offer(n);
 
         int level = 0;
         while (!Q.isEmpty()) {
             int len = Q.size();
             for (int i = 0; i < len; i++) {
+
                 int current = Q.poll();
-                if (K == current) {
-                    System.out.println(level);
-                    break;
+
+                if (current == m) {
+                    return level;
                 } else {
                     if (current - 1 >= 0 && !visited[current - 1]) {
                         Q.offer(current - 1);
                         visited[current - 1] = true;
-                    }
 
-                    if (current < K) {
-                        if (current != 0 && 2 * current <= size && !visited[2 * current]) {
+                    }
+                    if (current < m) {
+                        if (current != 0 && (current * 2 <= size) && !visited[current * 2]) {
                             Q.offer(2 * current);
                             visited[2 * current] = true;
                         }
                         if (current + 1 <= size && !visited[current + 1]) {
-                            visited[current + 1] = true;
                             Q.offer(current + 1);
+                            visited[current + 1] = true;
                         }
                     }
                 }
             }
             level++;
         }
+        return level;
     }
-}
+}   
