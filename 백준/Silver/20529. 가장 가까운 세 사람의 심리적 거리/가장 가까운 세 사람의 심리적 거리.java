03d9bbb3c7c1e4aa36e879059bcebddd899522cd
LeeJongsu <lik9819@naver.com>
2024-03-05 23:05:43 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 256 ms, Memory: 26504 KB -BaekjoonHub

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+
+            String[] str = br.readLine().split(" ");
+
+            if (N > 32) {
+                sb.append(0).append('\n');
+                continue;
+            }
+
+            int min = getMin(N, str);
+            sb.append(min).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int getMin(int N, String[] str) {
+        int min = Integer.MAX_VALUE;
+        for (int j = 0; j < N - 2; j++) {
+            for (int k = j + 1; k < N - 1; k++) {
+                for (int l = k + 1; l < N; l++) {
+                    int cnt = 0;
+                    for (int x = 0; x < 4; x++) {
+                        cnt += str[j].charAt(x) == str[k].charAt(x) ? 0 : 1;
+                        cnt += str[j].charAt(x) == str[l].charAt(x) ? 0 : 1;
+                        cnt += str[k].charAt(x) == str[l].charAt(x) ? 0 : 1;
+                    }
+                    min = Math.min(min, cnt);
+                    
+                    if (min == 0) return 0;
+                }
+            }
+        }
+        return min;
+    }
+}
