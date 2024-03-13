b995284dbd0f16ea315d609c183ac9975db4bf2e
LeeJongsu <lik9819@naver.com>
2024-03-13 20:48:19 +0900

    [Gold IV] Title: 테트로미노, Time: 736 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -6,8 +6,8 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, M, answer = Integer.MIN_VALUE;
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[] dx = {0, 1, 0, -1};
     static int[][] board;
     static boolean[][] visited;
 
@@ -32,7 +32,7 @@ public class Main {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 visited[i][j] = true;
-                DFS(i, j, 1, board[i][j]);
+                dfs(i, j, 1, board[i][j]);
                 visited[i][j] = false;
             }
         }
@@ -40,7 +40,7 @@ public class Main {
         System.out.println(answer);
     }
 
-    static void DFS(int y, int x, int L, int sum) {
+    private static void dfs(int y, int x, int L, int sum) {
 
         if (L == 4) {
             answer = Math.max(answer, sum);
@@ -50,22 +50,19 @@ public class Main {
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
+            if (ny >= 0 && nx >= 0 && ny < N && nx < M && !visited[ny][nx]) {
 
-            if (ny >= 0 && ny < N && nx >= 0 && nx < M && !visited[ny][nx]) {
-
-                //보라색(ㅗ) 테트로미노 만들기 위해 2번째 칸에서 탐색 한번 더 진행 
                 if (L == 2) {
                     visited[ny][nx] = true;
-                    DFS(y, x, L + 1, sum + board[ny][nx]);
+                    dfs(y, x, L + 1, sum + board[ny][nx]);
                     visited[ny][nx] = false;
                 }
 
                 visited[ny][nx] = true;
-                DFS(ny, nx, L + 1, sum + board[ny][nx]);
+                dfs(ny, nx, L + 1, sum + board[ny][nx]);
                 visited[ny][nx] = false;
             }
         }
-
     }
 }
 
