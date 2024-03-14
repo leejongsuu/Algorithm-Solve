ff30d15a4138cfc83b291d8df2345aa60af07a32
LeeJongsu <lik9819@naver.com>
2024-03-14 16:19:35 +0900

    [Silver III] Title: N과 M (5), Time: 332 ms, Memory: 29268 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr, num;
+    static boolean[] visited;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        num = new int[N];
+        visited = new boolean[N];
+        arr = new int[M];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int L) {
+
+        if (L == M) {
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[L] = num[i];
+                dfs(L + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
+
