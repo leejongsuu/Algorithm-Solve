f2a99220f49b6db97c73432321da6b523a4960a1
LeeJongsu <lik9819@naver.com>
2024-03-19 21:41:02 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class Main {
 
@@ -10,24 +9,26 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        ArrayList<Integer> list = new ArrayList<>();
-
         int N = Integer.parseInt(br.readLine());
+        int[] times = new int[N];
+
         String[] input = br.readLine().split(" ");
 
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(input[i]));
+            times[i] = Integer.parseInt(input[i]);
         }
 
-        Collections.sort(list);
+        // 오름차순으로 정렬
+        Arrays.sort(times);
 
-        int sum = 0;
-        int answer = 0;
+        int totalTime = 0; // 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값을 구하기 위한 변수
+        int cumulativeTime = 0; // 다음 사람의 인출 시작 시간을 위한 누적 시간 변수
+
         for (int i = 0; i < N; i++) {
-            sum = sum + list.get(i);
-            answer += sum;
+            cumulativeTime += times[i];
+            totalTime += cumulativeTime; // 각 사람의 누적 시간을 더하여 총 시간에 추가
         }
 
-        System.out.println(answer);
+        System.out.println(totalTime);
     }
 }
