28f1235f7b4c2f592362d8e1a3d68d40ad342f91
LeeJongsu <lik9819@naver.com>
2024-03-19 22:18:49 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -2,50 +2,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class fibo {
-    int x, y;
+class Data {
+    int zero_cnt, one_cnt;
 
-    public fibo(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Data(int zero_cnt, int one_cnt) {
+        this.zero_cnt = zero_cnt;
+        this.one_cnt = one_cnt;
     }
-
 }
 
 public class Main {
 
-    static fibo[] answer = new fibo[41];
-
-    public static fibo DFS(int n) {
-        if (answer[n]!=null) return answer[n];
-
-        if (n == 0) {
-            answer[0] = new fibo(1, 0);
-            return answer[0];
-        } else if (n == 1) {
-            answer[1] = new fibo(0, 1);
-            return answer[1];
-        } else {
-            fibo prev1 = DFS(n - 1);
-            fibo prev2 = DFS(n - 2);
-            answer[n] = new fibo(prev1.x + prev2.x, prev1.y + prev2.y);
-            return answer[n];
-        }
-    }
+    static Data[] dy = new Data[41];
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder();
-
         int T = Integer.parseInt(br.readLine());
+
+        dy[0] = new Data(1, 0);
+        dy[1] = new Data(0, 1);
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            fibo value = DFS(n);
-            sb.append(value.x).append(' ').append(value.y).append('\n');
+            int num = Integer.parseInt(br.readLine());
+            Data data = dfs(num);
+            sb.append(data.zero_cnt).append(' ').append(data.one_cnt).append('\n');
         }
 
         System.out.println(sb);
+    }
+
+    static Data dfs(int n) {
+        if (dy[n] != null) return dy[n];
+        else {
+            Data previous = dfs(n - 1);
+            Data twoPrevious = dfs(n - 2);
+            return dy[n] = new Data(previous.zero_cnt + twoPrevious.zero_cnt, previous.one_cnt + twoPrevious.one_cnt);
+        }
     }
 }
