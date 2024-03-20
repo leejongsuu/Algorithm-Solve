b55ac65f47e995f0a4f48050de3ed92a1fad164d
LeeJongsu <lik9819@naver.com>
2024-03-20 14:22:44 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,35 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static int[] unf;
-
-    static int Find(int v) {
-        if (v == unf[v]) return v;
-        else return unf[v] = Find(unf[v]);
-    }
-
-    static void Union(int a, int b) {
-        int fa = Find(a);
-        int fb = Find(b);
-        if (fa != fb) unf[fa] = fb;
-    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
         unf = new int[N + 1];
-        for (int i = 1; i <= N; i++) unf[i] = i;
+        for (int i = 1; i <= N; i++) {
+            unf[i] = i;
+        }
 
-        int M = Integer.parseInt(br.readLine());
         for (int i = 0; i < M; i++) {
-            String[] input = br.readLine().split(" ");
-            Union(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            Union(a, b);
         }
 
         int cnt = 0;
@@ -40,5 +34,18 @@ public class Main {
         }
 
         System.out.println(cnt);
+    }
+
+    static void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+        if (fa != fb) {
+            unf[fa] = fb;
+        }
+    }
+
+    static int Find(int v) {
+        if (v == unf[v]) return v;
+        return unf[v] = Find(unf[v]);
     }
 }
