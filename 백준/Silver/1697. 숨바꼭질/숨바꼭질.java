525eacd832795e51860a4437dc292393a7a5e700
LeeJongsu <lik9819@naver.com>
2024-03-23 22:03:40 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 18352 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -10,50 +12,51 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        System.out.println(BFS(N, M));
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs(N, K));
     }
 
-    static int BFS(int n, int m) {
+    static int bfs(int n, int k) {
 
-        int size = 100001;
-        boolean[] visited = new boolean[size];
+        boolean[] visited = new boolean[100001];
+        visited[n] = true;
 
-        Queue<Integer> Q = new LinkedList<>();
-        Q.offer(n);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(n);
 
         int level = 0;
-        while (!Q.isEmpty()) {
-            int len = Q.size();
+        while (!queue.isEmpty()) {
+            int len = queue.size();
             for (int i = 0; i < len; i++) {
+                int now = queue.poll();
 
-                int current = Q.poll();
-
-                if (current == m) {
+                if (now == k) {
                     return level;
-                } else {
-                    if (current - 1 >= 0 && !visited[current - 1]) {
-                        Q.offer(current - 1);
-                        visited[current - 1] = true;
+                }
 
+                if (now - 1 >= 0 && !visited[now - 1]) {
+                    visited[now - 1] = true;
+                    queue.offer(now - 1);
+                }
+
+                if (now < k) {
+                    if (!visited[now + 1]) {
+                        visited[now + 1] = true;
+                        queue.offer(now + 1);
                     }
-                    if (current < m) {
-                        if (current != 0 && (current * 2 <= size) && !visited[current * 2]) {
-                            Q.offer(2 * current);
-                            visited[2 * current] = true;
-                        }
-                        if (current + 1 <= size && !visited[current + 1]) {
-                            Q.offer(current + 1);
-                            visited[current + 1] = true;
-                        }
+                    if (2 * now < 100001 && !visited[2 * now]) {
+                        visited[2 * now] = true;
+                        queue.offer(2 * now);
                     }
                 }
             }
             level++;
         }
+
         return level;
     }
-}   
+}
