27eeb317a64ebc88b3dd76ca2ee18ca4a0accde7
LeeJongsu <lik9819@naver.com>
2024-03-23 21:45:09 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 132 ms, Memory: 14872 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N;
-    static int[][] relation;
 
     public static void main(String[] args) throws IOException {
 
@@ -16,56 +14,65 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        relation = new int[N + 1][N + 1];
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            relation[s][e] = 1;
-            relation[e][s] = 1;
+            list.get(s).add(e);
+            list.get(e).add(s);
         }
 
-        int minValue = BFS(1);
-        int result = 1;
+        int minValue = Integer.MAX_VALUE;
+        int answer = 1;
 
-        for (int i = 2; i <= N; i++) {
-            int bfs = BFS(i);
-            if (bfs < minValue) {
-                minValue = bfs;
-                result = i;
+        for (int i = 1; i <= N; i++) {
+            int num = bfs(i, N, minValue, list);
+            if (num < minValue) {
+                minValue = num;
+                answer = i;
             }
         }
 
-        System.out.println(result);
+        System.out.println(answer);
     }
 
-    static int BFS(int v) {
+    static int bfs(int v, int n, int min, ArrayList<ArrayList<Integer>> list) {
 
-        Queue<Integer> Q = new LinkedList<>();
-
-        boolean[] visited = new boolean[N + 1];
-        int[] distance = new int[N + 1];
-
-        Q.offer(v);
+        boolean[] visited = new boolean[n + 1];
         visited[v] = true;
 
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+
         int sum = 0;
+        int level = 1;
 
-        while (!Q.isEmpty()) {
-            int current = Q.poll();
+        while (!queue.isEmpty()) {
+            if (sum >= min) {
+                return Integer.MAX_VALUE;
+            }
 
-            for (int i = 1; i <= N; i++) {
-                if (relation[current][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    Q.offer(i);
-                    distance[i] = distance[current] + 1;
-                    sum += distance[i];
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                int now = queue.poll();
+                for (int x : list.get(now)) {
+                    if (!visited[x]) {
+                        visited[x] = true;
+                        sum += level;
+                        queue.offer(x);
+                    }
                 }
             }
+            level++;
         }
 
         return sum;
