fa83c56fb345a69ca0f4d5c0a9c6c2d2af388ad0
LeeJongsu <lik9819@naver.com>
2024-03-27 17:04:24 +0900

    [Gold V] Title: 적록색약, Time: 160 ms, Memory: 16336 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -5,69 +5,76 @@ import java.io.InputStreamReader;
 public class Main {
 
     static int N;
+    static int cnt = 0, cnt2 = 0;
     static int[] dy = {1, 0, -1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static char[][] color, abColor;
+    static char[][] board, board2;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        color = new char[N][N];
-        abColor = new char[N][N];
+        board = new char[N][N];
+        board2 = new char[N][N];
 
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < N; j++) {
-                char c = input.charAt(j);
-                color[i][j] = abColor[i][j] = c;
-                if (c == 'G') {
-                    abColor[i][j] = 'R';
+                char ch = input.charAt(j);
+                board[i][j] = ch;
+                if (ch == 'B') {
+                    board2[i][j] = ch;
+                } else {
+                    board2[i][j] = 'R';
                 }
             }
         }
 
-        int cnt = 0, abCnt = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (color[i][j] != '0') {
+                if (board[i][j] != '0') {
+                    char color = board[i][j];
                     cnt++;
-                    dfs(i, j, color[i][j]);
+                    board[i][j] = '0';
+                    dfs(i, j, color);
                 }
 
-                if (abColor[i][j] != '0') {
-                    abCnt++;
-                    dfs2(i, j, abColor[i][j]);
+                if (board2[i][j] != '0') {
+                    char color = board2[i][j];
+                    cnt2++;
+                    board2[i][j] = '0';
+                    dfs2(i, j, color);
                 }
             }
         }
 
-        System.out.println(cnt + " " + abCnt);
+        System.out.println(cnt + " " + cnt2);
     }
 
-    static void dfs(int y, int x, char cur) {
-        color[y][x] = '0';
+    static void dfs(int y, int x, char color) {
+
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny >= 0 && nx >= 0 && ny < N && nx < N && color[ny][nx] == cur) {
-                color[ny][nx] = '0';
-                dfs(ny, nx, cur);
+
+            if (ny >= 0 && nx >= 0 && ny < N && nx < N && board[ny][nx] == color) {
+                board[ny][nx] = '0';
+                dfs(ny, nx, color);
             }
         }
     }
 
-    static void dfs2(int y, int x, char cur) {
-        abColor[y][x] = '0';
+    static void dfs2(int y, int x, char color) {
+
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny >= 0 && nx >= 0 && ny < N && nx < N && abColor[ny][nx] == cur) {
-                abColor[ny][nx] = '0';
-                dfs2(ny, nx, cur);
+
+            if (ny >= 0 && nx >= 0 && ny < N && nx < N && board2[ny][nx] == color) {
+                board2[ny][nx] = '0';
+                dfs2(ny, nx, color);
             }
         }
     }
 }
-
