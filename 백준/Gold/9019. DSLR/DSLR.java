0121027f5cb35e460220e3018916b566c7b24033
LeeJongsu <lik9819@naver.com>
2024-03-28 19:14:50 +0900

    [Gold IV] Title: DSLR, Time: 2504 ms, Memory: 303552 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -6,20 +6,80 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            String cmd = solution(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            sb.append(cmd).append('\n');
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(bfs(A, B)).append('\n');
         }
 
         System.out.println(sb);
+    }
+
+    static String bfs(int A, int target) {
+
+        boolean[] visited = new boolean[10001];
+        visited[A] = true;
+
+        Queue<Register> Q = new LinkedList<>();
+        Q.offer(new Register(A, ""));
+
+        while (!Q.isEmpty()) {
+            Register now = Q.poll();
+            if (!visited[now.D()]) {
+                int value = now.D();
+                String nextCommand = now.command + "D";
+                visited[value] = true;
+                if (value == target) {
+                    return nextCommand;
+                } else {
+                    Q.offer(new Register(value, nextCommand));
+                }
+            }
+
+            if (!visited[now.S()]) {
+                int value = now.S();
+                String nextCommand = now.command + "S";
+                visited[value] = true;
+                if (value == target) {
+                    return nextCommand;
+                } else {
+                    Q.offer(new Register(value, nextCommand));
+                }
+            }
+
+            if (!visited[now.L()]) {
+                int value = now.L();
+                String nextCommand = now.command + "L";
+                visited[value] = true;
+                if (value == target) {
+                    return nextCommand;
+                } else {
+                    Q.offer(new Register(value, nextCommand));
+                }
+            }
+
+            if (!visited[now.R()]) {
+                int value = now.R();
+                String nextCommand = now.command + "R";
+                visited[value] = true;
+                if (value == target) {
+                    return nextCommand;
+                } else {
+                    Q.offer(new Register(value, nextCommand));
+                }
+            }
+        }
+
+        return "NotFound";
     }
 
     static class Register {
@@ -40,52 +100,11 @@ public class Main {
         }
 
         int L() {
-            return (num % 1000) * 10 + num / 1000;
+            return ((num % 1000) * 10) + (num / 1000);
         }
 
         int R() {
-            return (num / 10) + (num % 10) * 1000;
+            return ((num % 10) * 1000) + num / 10;
         }
     }
-
-
-    private static String solution(int a, int b) {
-
-        String answer = "";
-
-        boolean[] visited = new boolean[10001];
-
-        Queue<Register> Q = new LinkedList<>();
-        Q.offer(new Register(a, ""));
-
-        while (!Q.isEmpty()) {
-            Register now = Q.poll();
-
-            if (now.num == b) {
-                answer = now.command;
-                break;
-            }
-
-            if (!visited[now.D()]) {
-                visited[now.D()] = true;
-                Q.offer(new Register(now.D(), now.command + "D"));
-            }
-            if (!visited[now.S()]) {
-                visited[now.S()] = true;
-                Q.offer(new Register(now.S(), now.command + "S"));
-            }
-            if (!visited[now.L()]) {
-                visited[now.L()] = true;
-                Q.offer(new Register(now.L(), now.command + "L"));
-            }
-            if (!visited[now.R()]) {
-                visited[now.R()] = true;
-                Q.offer(new Register(now.R(), now.command + "R"));
-            }
-        }
-
-        return answer;
-    }
-
 }
-
