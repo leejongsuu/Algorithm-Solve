ffc609e7d1b4216470d15a6e73397472a41b1d57
LeeJongsu <lik9819@naver.com>
2024-03-28 20:13:15 +0900

    [Gold IV] Title: 테트로미노, Time: 656 ms, Memory: 34024 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -8,7 +8,7 @@ public class Main {
     static int N, M, answer = Integer.MIN_VALUE;
     static int[] dy = {1, 0, -1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static int[][] board;
+    static int[][] paper;
     static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
@@ -19,20 +19,20 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        board = new int[N][M];
+        paper = new int[N][M];
         visited = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < M; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+                paper[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 visited[i][j] = true;
-                dfs(i, j, 1, board[i][j]);
+                DFS(i, j, 1, paper[i][j]);
                 visited[i][j] = false;
             }
         }
@@ -40,7 +40,7 @@ public class Main {
         System.out.println(answer);
     }
 
-    private static void dfs(int y, int x, int L, int sum) {
+    static void DFS(int y, int x, int L, int sum) {
 
         if (L == 4) {
             answer = Math.max(answer, sum);
@@ -50,19 +50,15 @@ public class Main {
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
+
             if (ny >= 0 && nx >= 0 && ny < N && nx < M && !visited[ny][nx]) {
-
-                if (L == 2) {
-                    visited[ny][nx] = true;
-                    dfs(y, x, L + 1, sum + board[ny][nx]);
-                    visited[ny][nx] = false;
-                }
-
                 visited[ny][nx] = true;
-                dfs(ny, nx, L + 1, sum + board[ny][nx]);
+                DFS(ny, nx, L + 1, sum + paper[ny][nx]);
+                if (L == 2) {
+                    DFS(y, x, L + 1, sum + paper[ny][nx]);
+                }
                 visited[ny][nx] = false;
             }
         }
     }
 }
-
