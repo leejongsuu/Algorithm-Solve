717b7c8e22ce3af94b5a11bd65276bb374ab77ff
LeeJongsu <lik9819@naver.com>
2024-04-01 21:11:38 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        BigInteger big = new BigInteger(String.valueOf(A));
+        BigInteger bigInteger = big.modPow(BigInteger.valueOf(B), BigInteger.valueOf(C));
+
+        System.out.println(bigInteger);
+    }
+}
