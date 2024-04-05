3f26df1e8f37f25f67eb93ae8d2339942a3c9fdb
LeeJongsu <lik9819@naver.com>
2024-04-05 10:20:27 +0900

    [level 2] Title: 가장 큰 수, Time: 2.44 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,33 +2,33 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        
+        // 정수를 문자열로 변환하여 비교하기 위한 Comparator 구현
         Comparator<String> comp = new Comparator<String>() {
             @Override
             public int compare(String a, String b) {
-                return (b+a).compareTo(a+b);
+                // 두 문자열을 합쳐서 비교
+                return (b + a).compareTo(a + b);
             }
         };
-        
-        String[] strArr = new String[numbers.length];
-        for(int i=0; i<numbers.length; i++){
-            strArr[i] = String.valueOf(numbers[i]);
+
+        // 정수를 문자열로 변환한 후 정렬
+        String[] strNumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
         }
-        
-        Arrays.sort(strArr, comp);
-        
+        Arrays.sort(strNumbers, comp);
+
+        // 정렬된 문자열 배열을 이어 붙여서 결과 생성
         StringBuilder sb = new StringBuilder();
-        for(String str : strArr) {
-            sb.append(str);
+        for (String num : strNumbers) {
+            sb.append(num);
         }
-        
-        answer = sb.toString();
-        
-        if(answer.charAt(0)=='0') {
+
+        // 0으로 시작하는 경우 예외 처리
+        if (sb.charAt(0) == '0') {
             return "0";
         }
-        
-        return answer;
+
+        return sb.toString();
     }
 }
