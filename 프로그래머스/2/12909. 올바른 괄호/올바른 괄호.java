b9e498112a7c9da3c749f579b7c6d84387fe5815
LeeJongsu <lik9819@naver.com>
2024-04-07 10:54:05 +0900

    [level 2] Title: 올바른 괄호, Time: 5.77 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,18 +3,19 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        
+
         int count = 0;
         for(char c : s.toCharArray()) {
-            if(c=='(') count ++;
-            else if(c==')') count --;
+            if(c == '(') count ++;
+            else count --;
             
             if(count < 0) {
                 answer = false;
                 break;
             }
         }
-        if(count !=0) answer = false;
+        
+        if(count != 0) answer = false;
 
         return answer;
     }
