564a658622a4fcb11f2b43c9d5c0866e836bb4be
LeeJongsu <lik9819@naver.com>
2024-04-07 12:00:20 +0900

    [level 2] Title: 기능개발, Time: 0.30 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,31 +4,27 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer;
         
-        int[] days = new int[speeds.length];
-        for(int i=0; i<speeds.length; i++) {
-            int sub = 100 - progresses[i];
-            days[i] = sub / speeds[i];
-            if(sub % speeds[i] != 0) {
-                days[i]++;
-            }
-        }
-        
-        int cnt = 1, max = days[0];
+        Queue<Integer> Q = new LinkedList<>();
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i=1; i<days.length; i++) {
-            if(days[i] > max) {
-                max = days[i];
-                list.add(cnt);
-                cnt = 1;
-            } else {
-                cnt++;
+        
+        for(int i=0; i<speeds.length; i++) {
+            double remain = 100 - progresses[i];
+            double fNeedDay = remain / speeds[i];
+            
+            int date = (int) Math.ceil(fNeedDay);
+            
+            if(!Q.isEmpty() && Q.peek() < date) {
+                list.add(Q.size());
+                Q.clear();
             }
+            
+            Q.offer(date);
         }
-        list.add(cnt);
+        list.add(Q.size());
         
         answer = new int[list.size()];
-        for(int i=0; i < list.size(); i++) {
-            answer[i]=list.get(i);
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
         }
         
         return answer;
