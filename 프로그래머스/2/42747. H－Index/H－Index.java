0fc2dc3aa050b953ece426a803b9b6a20a23164c
LeeJongsu <lik9819@naver.com>
2024-04-07 09:53:31 +0900

    [level 2] Title: H-Index, Time: 1.07 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -9,24 +9,24 @@ class Solution {
         
         while(lo < hi) {
             int mid = (lo + hi) / 2;
-            
             int cnt = 0;
             for(int i : citations) {
-                if(i >= mid) {
-                    cnt++;
-                }
+                if(i>=mid) cnt++;
             }
             
-            if(cnt >= mid) {
-                lo = mid +1;
+            if(cnt>=mid) {
+                lo = mid + 1;
             } else {
                 hi = mid;
             }
         }
-        if(lo == 0) return answer;
-        else {
-            answer = lo - 1;
-            return answer;
+        
+        if(lo == 0) {
+            answer = 0;
+        } else {
+            answer = lo -1;
         }
+        
+        return answer;
     }
 }
