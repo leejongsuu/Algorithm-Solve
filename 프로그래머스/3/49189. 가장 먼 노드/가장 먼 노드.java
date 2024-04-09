068657daa5a72c05498dadde611838971f4b9f8f
LeeJongsu <lik9819@naver.com>
2024-04-09 22:40:46 +0900

    [level 3] Title: 가장 먼 노드, Time: 33.56 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -4,44 +4,48 @@ class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
         
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for(int i=0; i <= n; i++) {
-            list.add(new ArrayList<>());
+        // 인접 리스트 생성
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        for(int i = 0; i <= n; i++) {
+            adjList.add(new ArrayList<>());
         }
         
-        for(int i=0; i < edge.length; i++) {
+        // 그래프 정보를 인접 리스트에 저장
+        for(int i = 0; i < edge.length; i++) {
             int s = edge[i][0];
             int e = edge[i][1];
-            list.get(s).add(e);
-            list.get(e).add(s);
+            adjList.get(s).add(e);
+            adjList.get(e).add(s);
         }
         
-        answer = BFS(n, list);
+        // BFS를 통해 1번 노드부터 탐색하여 가장 멀리 떨어진 노드의 개수 구하기
+        answer = BFS(n, adjList);
         
         return answer;
     }
     
-    public int BFS(int n, ArrayList<ArrayList<Integer>> list) {
+    // BFS 구현
+    public int BFS(int n, ArrayList<ArrayList<Integer>> adjList) {
+        boolean[] visited = new boolean[n + 1]; // 방문 여부 체크
+        visited[1] = true; // 1번 노드 방문 처리
         
-        boolean[] visited = new boolean[n+1];
-        visited[1] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1); // 시작 노드인 1을 큐에 삽입
         
-        Queue<Integer> Q = new LinkedList<>();
-        Q.offer(1);
-        
-        int cnt = 1;
-        while(!Q.isEmpty()) {
-            cnt = Q.size();
-            for(int i=0; i < cnt; i++) {
-                int now = Q.poll();
-                for(int next : list.get(now)){
-                    if(!visited[next]) {
-                        visited[next] = true;
-                        Q.offer(next);
+        int count = 0; // 가장 멀리 떨어진 노드의 개수를 저장할 변수
+        while(!queue.isEmpty()) {
+            int size = queue.size(); // 현재 큐에 있는 노드의 개수
+            count = size; // 현재 레벨의 노드 개수를 count에 저장
+            for(int i = 0; i < size; i++) {
+                int current = queue.poll(); // 큐에서 하나의 노드를 꺼냄
+                for(int next : adjList.get(current)) { // 인접한 노드들에 대해서
+                    if(!visited[next]) { // 방문하지 않았다면
+                        visited[next] = true; // 방문 처리
+                        queue.offer(next); // 큐에 삽입
                     }
                 }
             }
         }
-        return cnt;
+        return count; // 가장 멀리 떨어진 노드의 개수 반환
     }
 }
