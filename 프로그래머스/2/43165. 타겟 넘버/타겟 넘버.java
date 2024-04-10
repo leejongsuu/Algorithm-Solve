0e268cfe4b0e0fdf1b915f1eb6feac4776a71570
LeeJongsu <lik9819@naver.com>
2024-04-10 16:50:55 +0900

    [level 2] Title: 타겟 넘버, Time: 0.42 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    static int cnt = 0;
+    
+    public int solution(int[] numbers, int target) {
+              
+        DFS(0, 0, numbers, target);
+        
+        return cnt;
+    }
+    
+    public void DFS(int L, int sum, int[] numbers, int target) {
+             
+        if(L==numbers.length){
+            if(sum == target) cnt++;
+        } else {
+                DFS(L+1, sum + numbers[L], numbers, target);
+                DFS(L+1, sum - numbers[L], numbers, target);
+        }
+    }
+}
