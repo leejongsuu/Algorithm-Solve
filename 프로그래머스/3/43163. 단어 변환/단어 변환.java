4a80720210844d1c3993b2714272f018689a2ef7
LeeJongsu <lik9819@naver.com>
2024-04-10 22:06:27 +0900

    [level 3] Title: 단어 변환, Time: 0.13 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -5,34 +5,32 @@ class Solution {
         
         boolean[] visited = new boolean[words.length];
 
-        Queue<char[]> Q = new LinkedList<>();
-        Q.offer(begin.toCharArray());
+        Queue<String> Q = new LinkedList<>();
+        Q.offer(begin);
         
-        char[] targetArr = target.toCharArray();
-        
-        int level = 1;
+        int level = 0;
         while(!Q.isEmpty()) {
             int len = Q.size();
             for(int i=0; i<len; i++) {
-                char[] current = Q.poll();
-                if(Arrays.equals(current, targetArr)) {
-                    return level -1;
+                String current = Q.poll();
+                if(current.equals(target)) {
+                    return level;
                 }
             
                 for(int j=0; j<words.length; j++) {
                     int cnt = 0;
-                    int index = 0;
                     String word = words[j];
+                    
                     for(int k=0; k<word.length(); k++) {
-                        if(current[k] != word.charAt(k)) {
+                        if(current.charAt(k) != word.charAt(k)) {
                             cnt++;
-                            index = k;
                         }
                         if(cnt > 1) break;
                     }
+                    
                     if(cnt == 1 && !visited[j]) {
                         visited[j] = true;
-                        Q.offer(word.toCharArray());
+                        Q.offer(word);
                     }
                 }
             }
