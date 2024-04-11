722aa822ef891cacdd1513dfb8517334c4fe26e1
LeeJongsu <lik9819@naver.com>
2024-04-11 20:51:32 +0900

    [level 3] Title: 단어 변환, Time: 0.18 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,40 +2,46 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
         
         boolean[] visited = new boolean[words.length];
-
+        
         Queue<String> Q = new LinkedList<>();
         Q.offer(begin);
         
         int level = 0;
         while(!Q.isEmpty()) {
-            int len = Q.size();
-            for(int i=0; i<len; i++) {
+            
+            int size = Q.size();
+            for(int i=0; i<size; i++) {
                 String current = Q.poll();
                 if(current.equals(target)) {
                     return level;
                 }
-            
+                
                 for(int j=0; j<words.length; j++) {
-                    int cnt = 0;
-                    String word = words[j];
-                    
-                    for(int k=0; k<word.length(); k++) {
-                        if(current.charAt(k) != word.charAt(k)) {
-                            cnt++;
-                        }
-                        if(cnt > 1) break;
-                    }
-                    
-                    if(cnt == 1 && !visited[j]) {
+                    if(check(current, words[j]) && !visited[j]) {
                         visited[j] = true;
-                        Q.offer(word);
+                        Q.offer(words[j]);
                     }
                 }
             }
+            
             level++;
         }
-        return 0;
+        return answer;
+    }
+    
+    public boolean check(String a, String b) {
+        int diffCount = 0;
+        
+        for(int i=0; i<a.length(); i++) {
+            if(a.charAt(i) != b.charAt(i)) {
+                diffCount++;
+            }
+            if(diffCount > 1) return false;
+        }
+        
+        return diffCount == 1;
     }
 }
