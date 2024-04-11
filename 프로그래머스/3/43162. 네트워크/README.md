3e16be4c799343444d5bb7114721c0b2ed96e39f
LeeJongsu <lik9819@naver.com>
2024-04-11 20:32:34 +0900

    [level 3] Title: 네트워크, Time: 2.61 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 77.3 MB, 시간: 1.08 ms
+메모리: 78.3 MB, 시간: 2.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 17:27:30
+2024년 04월 11일 20:32:33
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,52 +1,32 @@
-import java.util.*;
-
 class Solution {
     
     boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for(int i=0; i<n; i++) {
-            list.add(new ArrayList<>());
-        }
-        
         visited = new boolean[n];
-        boolean[][] check = new boolean[n][n];
-        for(int[] computer : computers) {
-            for(int i=0; i<n-1; i++) {
-                
-                if(computer[i]==0) continue;
-                
-                for(int j=i+1; j<n; j++){
-                    if(computer[j]==1) {
-                        if(!check[i][j]) {
-                            list.get(i).add(j);
-                            list.get(j).add(i);
-                            check[i][j] = check[j][i] = true;
-                        }
-                    }
-                }
-            }
-        }
         
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
                 answer++;
-                visited[i] = true;
-                DFS(i, list);
+                DFS(i, n, computers);
             }
         }
         
         return answer;
     }
     
-    public void DFS(int v, ArrayList<ArrayList<Integer>> list) {
+    public void DFS(int v, int n, int[][] computers) {
         
-        for(int next : list.get(v)) {
-            if(!visited[next]) {
-                visited[next] = true;
-                DFS(next, list);
+        visited[v] = true;
+        
+        for(int[] computer : computers) {
+            for(int i=0; i < n; i++) {
+                
+                if(computer[v] == 1 && computer[i] == 1 && !visited[i]) {
+                    DFS(i, n, computers);
+                }
             }
         }
     }
