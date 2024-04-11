3b0a06f50f50dbaff7af0facb952b770ba7ec403
LeeJongsu <lik9819@naver.com>
2024-04-11 20:11:29 +0900

    [level 3] Title: 순위, Time: 11.97 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -5,6 +5,10 @@ class Solution {
         int answer = 0;
         
         int[][] graph = new int[n+1][n+1];
+        for(int i=1; i<=n; i++) {
+            Arrays.fill(graph[i], Integer.MAX_VALUE);
+            graph[i][i] = 0;
+        }
         
         for(int[] result : results) {
             int win = result[0];
@@ -13,9 +17,9 @@ class Solution {
             graph[lose][win] = -1;
         }
         
-        for(int k =1; k<=n; k++) {
-            for(int i=1; i<=n; i++) {
-                for(int j=1; j<=n; j++) {
+        for(int k=1; k<=n; k++) {
+            for(int i=0; i<=n; i++) {
+                for(int j=0; j<=n; j++) {
                     if(graph[i][k] == 1 && graph[k][j] == 1) {
                         graph[i][j] = 1;
                         graph[j][i] = -1;
@@ -32,16 +36,12 @@ class Solution {
         for(int i=1; i<=n; i++) {
             boolean check = true;
             for(int j=1; j<=n; j++) {
-                if(i == j) continue;
-                if(graph[i][j] == 0) {
+                if(graph[i][j] == Integer.MAX_VALUE) {
                     check = false;
                     break;
-                } 
+                }
             }
-            
-            if(check) {
-                answer++;
-            }
+            if(check) answer++;
         }
         
         return answer;
