db6d6d8db2e77da0906145cfbb58af57d2ac8a69
LeeJongsu <lik9819@naver.com>
2024-04-12 23:09:38 +0900

    [level 1] Title: 체육복, Time: 0.51 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -5,32 +5,24 @@ class Solution {
         
         Arrays.sort(lost);
         
-        int answer = 0;
+        int answer = n;
         
-        int[] student = new int[n+2];
-        for(int r : reserve) {
-            int index = Arrays.binarySearch(lost, r);
-            if(index < 0) {
-                student[r]++;
-            } else {
-                lost[index] = -1;
-            }
-        }
-        
-        for(int l : lost) {
-            if(l==-1) continue;
-            
-            if(student[l-1] > 0) {
-                student[l-1]--;
-            } else if(student[l+1] > 0) {
-                student[l+1]--;
-            } else {
-                student[l]--;
-            }
-        }
+        int[] person = new int[n+2];
+        for(int l : lost) person[l]--;
+        for(int r : reserve) person[r]++;
         
         for(int i=1; i<=n; i++) {
-            if(student[i] >= 0) answer++;
+            if(person[i] == -1) {
+                if(person[i-1] == 1) {
+                    person[i-1]--;
+                    person[i]++;
+                } else if(person[i+1] == 1) {
+                    person[i+1]--;
+                    person[i]++;
+                } else {
+                    answer--;
+                }
+            }
         }
         
         return answer;
