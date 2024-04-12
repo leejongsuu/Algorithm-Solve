9ff5e0c2f789fcca1b6e145dee79c9e52a703d0b
LeeJongsu <lik9819@naver.com>
2024-04-12 17:08:53 +0900

    [level 1] Title: 모의고사, Time: 10.06 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        
+        int[] answer;
+        
+        List<Queue<Integer>> Qlist = new ArrayList<>();
+        for(int i=0; i<3; i++) {
+            Qlist.add(new LinkedList<>());
+        }
+        
+        Qlist.get(0).addAll(Arrays.asList(1,2,3,4,5));
+        Qlist.get(1).addAll(Arrays.asList(2,1,2,3,2,4,2,5));
+        Qlist.get(2).addAll(Arrays.asList(3,3,1,1,2,2,4,4,5,5));
+        
+        int[] person = new int[3];
+        
+        for(int ans : answers) {
+            for(int i=0; i<3; i++) {
+                int num = Qlist.get(i).poll();
+                if(ans == num) person[i]++;
+                Qlist.get(i).offer(num);
+            }
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        
+        int max = person[0];
+        list.add(1);
+        
+        for(int i=1; i<3; i++) {
+            
+            if(max==person[i]) {
+                list.add(i+1);
+            } else if(max < person[i]) {
+                max = person[i];
+                list.clear();
+                list.add(i+1);
+            }
+        }
+        
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i]=list.get(i);
+        }
+        
+        return answer;
+    }
+}
