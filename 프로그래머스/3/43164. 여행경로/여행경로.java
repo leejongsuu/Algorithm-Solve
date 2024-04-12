dc090d934aab07074b9f79c1367d4565b68909d7
LeeJongsu <lik9819@naver.com>
2024-04-12 09:15:37 +0900

    [level 3] Title: 여행경로, Time: 0.32 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -3,30 +3,29 @@ import java.util.*;
 class Solution {
     
     private Map<String, PriorityQueue<String>> flights;
-    private List<String> itinerary;
+    private List<String> list;
     
     public String[] solution(String[][] tickets) {
-        
         flights = new HashMap<>();
-        itinerary = new ArrayList<>();
+        list = new ArrayList<>();
         
         for(String[] ticket : tickets) {
-            flights.putIfAbsent(ticket[0], new PriorityQueue<>());
+            flights.putIfAbsent(ticket[0], new PriorityQueue<String>());
             flights.get(ticket[0]).offer(ticket[1]);
         }
         
         dfs("ICN");
         
-        return itinerary.toArray(new String[itinerary.size()]);
+        return list.toArray(new String[list.size()]);
     }
     
-    public void dfs(String airport) {
+    private void dfs(String airport) {
         
         PriorityQueue<String> destination = flights.get(airport);
-        while(destination!=null && !destination.isEmpty()) {
+        while(destination != null && !destination.isEmpty()) {
             dfs(destination.poll());
         }
         
-        itinerary.add(0, airport);
+        list.add(0, airport);
     }
 }
