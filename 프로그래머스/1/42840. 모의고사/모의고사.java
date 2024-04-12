921af00678b8d83e428010e5dd4009c5e8cd5114
LeeJongsu <lik9819@naver.com>
2024-04-12 17:43:50 +0900

    [level 1] Title: 모의고사, Time: 2.78 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,42 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
+
+        int[][] patterns = new int[][]{
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
         
-        int[] answer;
+        int[] hit = new int[3];
         
-        List<Queue<Integer>> Qlist = new ArrayList<>();
-        for(int i=0; i<3; i++) {
-            Qlist.add(new LinkedList<>());
-        }
-        
-        Qlist.get(0).addAll(Arrays.asList(1,2,3,4,5));
-        Qlist.get(1).addAll(Arrays.asList(2,1,2,3,2,4,2,5));
-        Qlist.get(2).addAll(Arrays.asList(3,3,1,1,2,2,4,4,5,5));
-        
-        int[] person = new int[3];
-        
-        for(int ans : answers) {
-            for(int i=0; i<3; i++) {
-                int num = Qlist.get(i).poll();
-                if(ans == num) person[i]++;
-                Qlist.get(i).offer(num);
+        for(int i=0; i < hit.length; i++) {
+            for(int j=0; j < answers.length; j++) {
+                if(patterns[i][j % patterns[i].length] == answers[j]) hit[i]++;
             }
         }
         
+        int max = Math.max(hit[0], Math.max(hit[1], hit[2]));
         List<Integer> list = new ArrayList<>();
         
-        int max = person[0];
-        list.add(1);
-        
-        for(int i=1; i<3; i++) {
-            
-            if(max==person[i]) {
-                list.add(i+1);
-            } else if(max < person[i]) {
-                max = person[i];
-                list.clear();
-                list.add(i+1);
-            }
+        for(int i=0; i<3; i++) {
+            if(hit[i] == max) list.add(i+1);
         }
         
         return list.stream().mapToInt(Integer::intValue).toArray();
