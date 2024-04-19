7ea903715eb256a3262d4dbd26e173026d88e0b5
LeeJongsu <lik9819@naver.com>
2024-04-19 19:34:43 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,12 +2,9 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
     
-        int len = String.valueOf(n).length();
         while (n > 0) {
-            int r = (int) Math.pow(10, len -1);
-            answer += n / r;
-            n %= r;
-            len --;
+            answer += n % 10;
+            n /= 10;
         }
 
         return answer;
