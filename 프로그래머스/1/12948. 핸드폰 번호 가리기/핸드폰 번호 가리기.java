25ef57041dc13e323b5ae75f94b4b1ee8f85ca56
LeeJongsu <lik9819@naver.com>
2024-04-23 23:46:28 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String phone_number) {
+        
+        char[] ch = phone_number.toCharArray();
+        for(int i = 0; i < phone_number.length() - 4; i++){
+            ch[i] = '*';
+        }
+        return String.valueOf(ch);
+    }
+}
