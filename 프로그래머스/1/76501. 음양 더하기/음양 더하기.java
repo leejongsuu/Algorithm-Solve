2e9c8e2e4a1f1b13bd62ad6a8ddef390038e4018
LeeJongsu <lik9819@naver.com>
2024-04-23 22:31:38 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -2,9 +2,8 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         
-        for(int i=0; i< absolutes.length; i++) {
-            if(!signs[i]) absolutes[i] *= -1;
-            answer += absolutes[i];
+        for(int i=0; i < absolutes.length; i++) {
+            answer += absolutes[i] * (signs[i] ? 1 : -1);
         }
         
         return answer;
