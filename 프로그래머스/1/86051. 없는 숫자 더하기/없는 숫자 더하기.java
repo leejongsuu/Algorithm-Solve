e392fb5fbe1ec58f9fcbf80a9129e674da25e60f
LeeJongsu <lik9819@naver.com>
2024-04-23 22:38:26 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.68 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,9 +1,8 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
         int answer = 9 * (10) / 2;
-        for(int i : numbers) {
-            answer -= i;
-        }
-        return answer;
+        return answer - Arrays.stream(numbers).sum();
     }
 }
