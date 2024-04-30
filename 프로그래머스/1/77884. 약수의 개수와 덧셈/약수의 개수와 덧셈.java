ff97f51a98fbdd41093b10c5040bc74c56c72551
LeeJongsu <lik9819@naver.com>
2024-04-30 20:55:27 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.02 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,18 +1,15 @@
 class Solution {
     public int solution(int left, int right) {
-        int sum = 0;
-        while(left <= right) {
-            int cnt = 0;
-            for(int i=1; i<=left; i++) {
-                if(left % i == 0) cnt++;
+        int answer = 0;
+        
+        for(int i=left; i<=right; i++) {
+            if(i % Math.sqrt(i) == 0) {
+                answer -= i;
+            } else {
+                answer += i;
             }
-            
-            if(cnt % 2 == 0) sum += left;
-            else sum -= left;
-            
-            left++;
         }
         
-        return sum;
+        return answer;
     }
 }
