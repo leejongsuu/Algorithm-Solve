5c3c2dd141c1cd85f848ebe252176194482ca2ed
LeeJongsu <lik9819@naver.com>
2024-05-07 10:09:52 +0900

    [level 1] Title: 3진법 뒤집기, Time: 18.33 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,24 +1,12 @@
-import java.util.Stack;
-
 class Solution {
-    
     public int solution(int n) {
-        
-        Stack<Integer> stack = new Stack<>();
-        while(n > 2) {
-            stack.push(n % 3);
+        String a = "";
+        while(n > 0) {
+            a = (n % 3) + a;
             n /= 3;
         }
-        stack.push(n);
         
-        int len = 0;
-        int sum = 0;
-        while(!stack.isEmpty()) {
-            sum += stack.pop() * (int) Math.pow(3, len++);
-        }
-        
-        return sum;
+        a = new StringBuilder(a).reverse().toString();
+        return Integer.parseInt(a, 3);
     }
 }
-    
-    
