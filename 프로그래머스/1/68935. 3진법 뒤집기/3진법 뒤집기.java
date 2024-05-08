b625636c3a33f567c16f94b44b5c184f94d40e87
LeeJongsu <lik9819@naver.com>
2024-05-08 15:13:10 +0900

    [level 1] Title: 3진법 뒤집기, Time: 12.07 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int n) {
+        
         String a = "";
         while(n > 0) {
-            a = (n % 3) + a;
+            a += n % 3;
+            System.out.println(a);
             n /= 3;
         }
         
-        a = new StringBuilder(a).reverse().toString();
         return Integer.parseInt(a, 3);
     }
 }
