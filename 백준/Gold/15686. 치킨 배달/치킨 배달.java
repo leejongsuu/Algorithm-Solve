da5f6140f8283ea5b62d2a3be9fc37ff6ccb373e
LeeJongsu <lik9819@naver.com>
2024-05-10 00:08:35 +0900

    [Gold V] Title: 치킨 배달, Time: 232 ms, Memory: 15760 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static class Point {
+        int y, x;
+
+        public Point(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    static int N, M, answer = Integer.MAX_VALUE;
+    static int[] combination;
+    static ArrayList<Point> home = new ArrayList<>();
+    static ArrayList<Point> chicken = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        combination = new int[M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num == 1) home.add(new Point(i, j));
+                if (num == 2) chicken.add(new Point(i, j));
+            }
+        }
+        DFS(0, 0);
+        System.out.println(answer);
+    }
+
+    private static void DFS(int L, int start) {
+        if (L == M) {
+            answer = Math.min(answer, cal_distance(combination));
+            return;
+        }
+
+        for (int i = start; i < chicken.size(); i++) {
+            combination[L] = i;
+            DFS(L + 1, i + 1);
+        }
+    }
+
+    private static int cal_distance(int[] combination) {
+
+        int sum_distance = 0;
+
+        for (Point h : home) {
+
+            int min_distance = Integer.MAX_VALUE;
+            for (int i : combination) {
+                Point c = chicken.get(i);
+                min_distance = Math.min(min_distance, (Math.abs(h.y - c.y) + Math.abs(h.x - c.x)));
+            }
+
+            sum_distance += min_distance;
+        }
+
+        return sum_distance;
+    }
+}
