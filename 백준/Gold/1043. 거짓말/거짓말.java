15c0b1c7a43429cc965af43fd2c0cd2445f286b1
LeeJongsu <lik9819@naver.com>
2024-05-14 21:48:20 +0900

    [Gold IV] Title: 거짓말, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -12,41 +12,33 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
         unf = new int[N + 1];
         for (int i = 1; i <= N; i++) unf[i] = i;
 
         st = new StringTokenizer(br.readLine(), " ");
         int num = Integer.parseInt(st.nextToken());
-
-
-        while (st.hasMoreTokens()) {
-            union(0, Integer.parseInt(st.nextToken()));
+        while (num > 0) {
+            Union(0, Integer.parseInt(st.nextToken()));
+            num--;
         }
 
         int[][] party = new int[M][];
-        for (
-                int i = 0;
-                i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int p = Integer.parseInt(st.nextToken());
-            party[i] = new int[p];
-
-            for (int j = 0; j < p; j++) {
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            party[i] = new int[Integer.parseInt(st.nextToken())];
+            for (int j = 0; j < party[i].length; j++) {
                 party[i][j] = Integer.parseInt(st.nextToken());
             }
 
-            for (int j = 0; j < p - 1; j++) {
-                union(party[i][j], party[i][j + 1]);
+            for (int j = 1; j < party[i].length; j++) {
+                Union(party[i][j - 1], party[i][j]);
             }
         }
 
         int count = M;
-        for (
-                int i = 0;
-                i < M; i++) {
+        for (int i = 0; i < M; i++) {
             for (int j = 0; j < party[i].length; j++) {
-                if (find(0) == find(party[i][j])) {
+                if (Find(0) == Find(party[i][j])) {
                     count--;
                     break;
                 }
@@ -56,14 +48,14 @@ public class Main {
         System.out.println(count);
     }
 
-    private static void union(int a, int b) {
-        int fa = find(a);
-        int fb = find(b);
+    private static void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
         if (fa != fb) unf[fa] = fb;
     }
 
-    private static int find(int v) {
+    private static int Find(int v) {
         if (v == unf[v]) return v;
-        else return unf[v] = find(unf[v]);
+        else return unf[v] = Find(unf[v]);
     }
 }
