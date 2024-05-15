72d31ed313b0f2fed29044eb0ead755530cc0aeb
LeeJongsu <lik9819@naver.com>
2024-05-15 19:47:10 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 17.39 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,14 +1,11 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i < food.length; i++) {
-            int count = food[i] / 2;
-            while(count > 0) {
-                sb.append(i);
-                count--;
+        String answer = "0";
+        for(int i = food.length - 1; i > 0; i--) {
+            for(int j = food[i] / 2; j > 0; j--) {
+                answer = i + answer + i;
             }
         }
-        String answer = sb.toString() + '0' + sb.reverse().toString();
         return answer;
     }
 }
