df068b392d75a727234c3d5a8eb5b62ae968de8c
LeeJongsu <lik9819@naver.com>
2024-05-15 19:23:05 +0900

    [level 1] Title: K번째수, Time: 0.53 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,17 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        
         int[] answer = new int[commands.length];
-        for(int i = 0; i < answer.length; i++) {
-            int start = commands[i][0] -1;
-            int end = commands[i][1];
-            int index = commands[i][2] -1;
-            int[] subArr = Arrays.copyOfRange(array, start, end);
-            Arrays.sort(subArr);
-            answer[i] = subArr[index];
-        }
         
+        for(int i = 0; i < commands.length; i++) {
+            int[] subArr = Arrays.copyOfRange(array, commands[i][0] - 1 , commands[i][1]);
+            Arrays.sort(subArr);
+            answer[i] = subArr[commands[i][2] - 1];
+        }
         return answer;
     }
 }
