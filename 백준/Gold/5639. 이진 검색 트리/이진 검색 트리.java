7f2116fb20b468a2ddada96e5caf41c5e4bb19e5
LeeJongsu <lik9819@naver.com>
2024-05-18 23:26:11 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 496 ms, Memory: 18204 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -3,43 +3,36 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-   static class Node {
-       int val;
-       Node lt , rt;
+    static class Node {
+        int val;
+        Node lt, rt;
 
-       public Node(int val) {
-           this.val = val;
-       }
+        public Node(int val) {
+            this.val = val;
+        }
 
-       public Node(int val, Node lt, Node rt) {
-           this.val = val;
-           this.lt = lt;
-           this.rt = rt;
-       }
-
-       void insert(int n) {
-           if (n < this.val) {
-               if (this.lt == null) this.lt = new Node(n);
-               else this.lt.insert(n);
-           } else {
-               if (this.rt == null) this.rt = new Node(n);
-               else this.rt.insert(n);
-           }
-       }
-   }
+        public void insert(int n) {
+            if (n < this.val) {
+                if (this.lt == null) this.lt = new Node(n);
+                else this.lt.insert(n);
+            } else {
+                if (this.rt == null) this.rt = new Node(n);
+                else this.rt.insert(n);
+            }
+        }
+    }
 
     static StringBuilder sb = new StringBuilder();
-
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Node root = new Node(Integer.parseInt(br.readLine()));
-
         while (true) {
-            String str = br.readLine();
-            if (str == null || str.equals("")) break;
+            String input = br.readLine();
+            if (input == null || input.equals("")) break;
 
-            root.insert(Integer.parseInt(str));
+            root.insert(Integer.parseInt(input));
         }
 
         postOrder(root);
