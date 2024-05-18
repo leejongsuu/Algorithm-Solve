907994c5e3e3400c5265fe192fce6b1c925fc669
LeeJongsu <lik9819@naver.com>
2024-05-18 20:51:49 +0900

    [level 1] Title: 폰켓몬, Time: 3.48 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,14 @@
-import java.util.HashMap;
+import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i : nums) {
-            map.put(i, map.getOrDefault(i, 0)+1);
+        // 해시맵을 사용하여 폰켓몬 종류 세기
+        HashSet<Integer> ponketmon = new HashSet<>();
+        for (int i : nums) {
+            ponketmon.add(i);
         }
-        
-        return Math.min(map.size(), nums.length / 2);
 
+        // 폰켓몬 종류 수와 선택할 수 있는 최대 마리 수(N/2) 중 작은 값 반환
+        return Math.min(ponketmon.size(), nums.length / 2);
     }
 }
