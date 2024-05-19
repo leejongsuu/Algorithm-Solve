11d8e556e22cb31f04f9ba469531ac07a94fc7d0
LeeJongsu <lik9819@naver.com>
2024-05-19 22:32:23 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,12 +1,15 @@
 class Solution {
     public String solution(int a, int b) {
-        int[] date = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] week = new String[]{"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        
+        int[] month = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
         
         for(int i = 0; i < a; i++) {
-            b += date[i];
+            b += month[i];
         }
-        b -=1;
-        return week[b % 7];
+        
+        b -= 1;
+        
+        return day[b % 7];
     }
 }
