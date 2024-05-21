5197f6857e6a4715f8780c58b0be8b0d818c04d5
LeeJongsu <lik9819@naver.com>
2024-05-21 16:40:39 +0900

    [level 1] Title: 과일 장수, Time: 0.49 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,13 +3,10 @@ import java.util.Arrays;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        
         Arrays.sort(score);
-                
-        for(int i = score.length; i >=m; i-=m) {
-            answer += score[i-m] * m;
+        for(int i = score.length; i >= m; i -= m) {
+            answer += score[i - m] * m;
         }
-        
         return answer;
     }
 }
