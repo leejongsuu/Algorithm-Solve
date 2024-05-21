41ed752f7ef7585cd9dd0b219dc1a0820e72c268
LeeJongsu <lik9819@naver.com>
2024-05-21 17:27:54 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.40 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int result = 0;
+        
+        for(int i = 1; i <=number; i++) {
+            result += gcd(i, limit, power);
+        }
+        return result;
+    }
+    
+    private static int gcd(int n, int limit, int power) {
+        int cnt = 0;
+        int len = (int) Math.sqrt(n);
+        for(int i = 1; i <= len; i++) {
+            if(n % i == 0) {
+                cnt++;
+            }
+        }
+        
+        cnt *=2;
+        if(n == (int)Math.pow(len, 2)) {
+            cnt--;
+        }
+        
+        return cnt > limit ? power : cnt;
+    }
+}
