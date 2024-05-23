99ab83250049ce6ef7a8f0964d2fcc6fc389b893
LeeJongsu <lik9819@naver.com>
2024-05-23 09:55:22 +0900

    [level 1] Title: 덧칠하기, Time: 1.46 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -2,12 +2,12 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         
         int count = 1;
-        int roll = section[0];
-        int len = section.length;
-        for(int i = 1; i < len; i++) {
-            if(roll + m - 1 < section[i]) {
+        int rolled = section[0];
+        
+        for(int i = 1; i < section.length; i++) {
+            if(rolled + m - 1 < section[i]) {
                 count++;
-                roll = section[i];
+                rolled = section[i];
             }
         }
         
