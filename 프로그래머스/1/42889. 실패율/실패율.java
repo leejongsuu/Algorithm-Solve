2d0f66077995f7cb97cd3d7db3a64f498ad5dc0f
LeeJongsu <lik9819@naver.com>
2024-05-26 16:29:20 +0900

    [level 1] Title: 실패율, Time: 0.42 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    
+    public int[] solution(int N, int[] stages) {
+        
+        int[] arr = new int[N+1];
+        for(int i : stages) {
+            if(i > N) continue;
+            arr[i]++;
+        }
+        
+        int denominator = stages.length;
+        ArrayList<Stage> list = new ArrayList<>();
+        for(int i = 1; i <= N; i++) {
+            denominator -= arr[i-1];
+            if(denominator <= 0) {
+                list.add(new Stage(i, 0));
+                continue;
+            }
+            double result = (double) arr[i] / denominator;
+            list.add(new Stage(i, result));
+        }
+        
+        Collections.sort(list);
+       
+        int[] result = new int[N];
+        for(int i = 0; i < N; i++) {
+            result[i] = list.get(i).number;
+        }
+        
+        return result;
+    }
+    
+    class Stage implements Comparable<Stage> {
+        int number;
+        double failureRate;
+        
+        public Stage(int number, double failureRate) {
+            this.number = number;
+            this.failureRate = failureRate;
+        }
+        
+        @Override
+        public int compareTo(Stage o) {
+            if(o.failureRate == this.failureRate) return this.number - o.number;
+            else return Double.compare(o.failureRate, this.failureRate);
+        }
+    }
+}
