e5d281fa035effa13b966c51319bf4045d4a1e1e
LeeJongsu <lik9819@naver.com>
2024-05-28 13:56:30 +0900

    [level 1] Title: 옹알이 (2), Time: 2.61 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -5,20 +5,18 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         
-        HashSet<String> set = new HashSet<>();
-        set.add("aya");
-        set.add("ye");
-        set.add("woo");
-        set.add("ma");
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("aya");
+        hashSet.add("ye");
+        hashSet.add("woo");
+        hashSet.add("ma");
         
         Pattern pattern = Pattern.compile("^(aya|ye|woo|ma)+$");
-        
-        for(String str : babbling) {
-            
-            if(pattern.matcher(str).matches()) {
+        for(String ba : babbling) {
+            if(pattern.matcher(ba).matches()) {
                 boolean flag = true;
-                for(String s : set) {
-                    if(str.contains(s + s)) {
+                for(String s : hashSet) {
+                    if(ba.contains(s+s)) {
                         flag = false;
                         break;
                     }
