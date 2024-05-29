cf1a0ea8d2877d07c32b3884ffa7f6d1911e5c87
LeeJongsu <lik9819@naver.com>
2024-05-29 17:15:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.88 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,23 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         
-        String answer = "";
-        
         HashMap<String, Integer> map = new HashMap<>();
-        
-        for(String par : participant) {
-            map.put(par, map.getOrDefault(par, 0)+1);
+        for(String key : participant) {
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
         
-        for(String com : completion) {
-            map.put(com, map.get(com)-1);
+        for(String key : completion) {
+            map.put(key, map.get(key) - 1);
         }
-                
+        
         for(String key : map.keySet()) {
             if(map.get(key) > 0) {
-                answer = key;
-                break;
+                return key;
             }
         }
         
-        return answer;
+        return "";
     }
 }
