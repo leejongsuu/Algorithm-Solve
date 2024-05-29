5b04a71e1ebc97fabfaeb6e3e87eefe5d7e051fc
LeeJongsu <lik9819@naver.com>
2024-05-29 16:35:32 +0900

    [level 1] Title: 둘만의 암호, Time: 0.08 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,30 +1,26 @@
 class Solution {
     public String solution(String s, String skip, int index) {
-        
-        char[] alphabet = new char[26];
-        for(int i = 0; i < 26; i++) {
-            alphabet[i] = (char) ((int) 'a' + i);
+        boolean[] skipSet = new boolean[26];
+        for (char ch : skip.toCharArray()) {
+            skipSet[ch - 'a'] = true;
         }
-        for(int i = 0; i < skip.length(); i++) {
-            alphabet[skip.charAt(i) -'a'] = '0';
-        }
-        
+
         int len = s.length();
         char[] result = new char[len];
         
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             char word = s.charAt(i);
             int cnt = 0;
             int j = word - 'a';
             while (cnt < index) {
-                j = (j+1) % 26;
-                if(alphabet[j] != '0') {
+                j = (j + 1) % 26;
+                if (!skipSet[j]) {
                     cnt++;
                 }
             }
-            result[i] = alphabet[j];
+            result[i] = (char) (j + 'a');
         }
         
-        return String.valueOf(result);
+        return new String(result);
     }
 }
