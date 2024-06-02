82cfe2ef32ce794e956bea53a477e0a9c8bcd204
LeeJongsu <lik9819@naver.com>
2024-06-02 10:40:51 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,12 +1,11 @@
 class Solution {
     public int solution(int[] ingredient) {
-        
         int answer = 0;
-        int len = ingredient.length;
-        int[] stack = new int[len];
+        
         int pos = 0;
-        for(int n : ingredient) {
-            stack[pos++] = n;
+        int[] stack = new int[ingredient.length];
+        for(int i : ingredient) {
+            stack[pos++] = i;
             if(pos > 3) {
                 if(stack[pos - 1] == 1 && stack[pos - 2] == 3 && stack[pos - 3] == 2 && stack[pos - 4] == 1) {
                     answer++;
