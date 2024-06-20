60340209e920844062a3d99463030d0a0559907a
LeeJongsu <lik9819@naver.com>
2024-06-20 21:33:27 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.47 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -3,15 +3,12 @@ import java.util.HashMap;
 class Solution {
     public String solution(String[] survey, int[] choices) {
         
-        HashMap<Character, Integer> personality = new HashMap<>();
-        personality.put('R', 0);
-        personality.put('T', 0);
-        personality.put('C', 0);
-        personality.put('F', 0);
-        personality.put('J', 0);
-        personality.put('M', 0);
-        personality.put('A', 0);
-        personality.put('N', 0);
+        char[][] personality = new char[][]{{'R','T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char[] ch : personality) {
+            map.put(ch[0], 0);
+            map.put(ch[1], 0);
+        }
         
         int len = choices.length;
         for(int i = 0; i < len; i++) {
@@ -19,18 +16,18 @@ class Solution {
             int value = 0;
             if(choices[i] < 4) {
                 value = 4 - choices[i];
-                personality.put(str.charAt(0), personality.get(str.charAt(0))+value);
+                map.put(str.charAt(0), map.get(str.charAt(0))+value);
             } else if(choices[i] > 4) {
                 value = choices[i] - 4;
-                personality.put(str.charAt(1), personality.get(str.charAt(1))+value);
+                map.put(str.charAt(1), map.get(str.charAt(1))+value);
             }
         }
         
         StringBuilder sb = new StringBuilder();
-        sb.append(personality.get('R') >= personality.get('T') ? 'R' : 'T');
-        sb.append(personality.get('C') >= personality.get('F') ? 'C' : 'F');
-        sb.append(personality.get('J') >= personality.get('M') ? 'J' : 'M');
-        sb.append(personality.get('A') >= personality.get('N') ? 'A' : 'N');
+        for(char[] ch : personality) {
+            sb.append(map.get(ch[0]) >= map.get(ch[1]) ? ch[0] : ch[1]);
+        }
+        
         
         return sb.toString();
     }
