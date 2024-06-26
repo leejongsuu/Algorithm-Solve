2fa7a3bf9a7cd982733544ae7f7e588922e2b25e
LeeJongsu <lik9819@naver.com>
2024-06-26 22:55:20 +0900

    [level 1] Title: 달리기 경주, Time: 0.07 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -3,21 +3,19 @@ import java.util.HashMap;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
         
-        HashMap<String, Integer> rankMap = new HashMap<>();
+        HashMap<String, Integer> indexMap = new HashMap<>();
         for(int i = 0; i < players.length; i++) {
-            rankMap.put(players[i], i);
+            indexMap.put(players[i], i);
         }
         
         for(String call : callings) {
-            int post = rankMap.get(call);
-            int pre = rankMap.get(players[post - 1]);
-            rankMap.put(call, pre);
-            rankMap.put(players[post-1], post);
-            players[post] = players[pre];
-            players[pre] = call;
+	        int idx = indexMap.get(call);
+            players[idx] = players[idx - 1];
+            players[idx - 1] = call;
+            indexMap.put(players[idx - 1], idx - 1);
+            indexMap.put(players[idx], idx);
         }
         
         return players;
     }
-    
 }
