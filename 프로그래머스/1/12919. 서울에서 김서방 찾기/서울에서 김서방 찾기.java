818eab94266519ae2d5b3289f2e689b6368841c0
LeeJongsu <lik9819@naver.com>
2024-07-06 00:02:14 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 5.80 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,14 +1,9 @@
-import java.util.HashMap;
+import java.util.List;
 
 class Solution {
-    
     public String solution(String[] seoul) {
+        int index = List.of(seoul).indexOf("Kim");
         
-        HashMap<String, Integer> posMap = new HashMap<>();
-        for(int i = 0; i < seoul.length; i++) {
-            posMap.put(seoul[i], i);
-        }
-        
-        return "김서방은 " + posMap.get("Kim") + "에 있다";
+        return "김서방은 " + index + "에 있다";
     }
 }
