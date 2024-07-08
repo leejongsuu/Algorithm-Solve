3f831d3792571eee5d534b1c8457e525260c3563
LeeJongsu <lik9819@naver.com>
2024-07-08 20:49:05 +0900

    [level 1] Title: 공원 산책, Time: 9.25 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,91 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    
+    static class Point {
+        int y, x;
+        public Point(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+    
+    public int[] solution(String[] park, String[] routes) {
+        
+        Map<String, Point> dirMap = new HashMap<>();
+        init(dirMap);
+        
+        int n = park.length;
+        int m = park[0].length();
+        
+        Point current = searchStart(n, m, park);
+        
+        for(String route : routes) {
+            String[] dir = route.split(" ");
+            
+            Point next = dirMap.get(dir[0]);
+            int ny = current.y + next.y * Integer.parseInt(dir[1]);
+            int nx = current.x + next.x * Integer.parseInt(dir[1]);
+            System.out.println(ny +" " +nx);
+            
+            if(isIn(ny, nx, n, m)) {
+                boolean flag = true;
+                switch (dir[0]) {
+                        case "N" -> 
+                            flag = isBlocked(park, ny, current.y, current.x, nx);
+                        case "S" -> 
+                            flag = isBlocked(park, current.y, ny, nx, current.x);
+                        case "E" -> 
+                            flag = isBlocked(park, current.y, ny, current.x, nx);
+                        case "W" -> 
+                            flag = isBlocked(park, current.y, ny, nx, current.x);
+                }
+                System.out.println(flag);
+                System.out.println("---------------");
+                if(!flag) {
+                    current = new Point(ny, nx);
+                }
+            }
+        }
+        
+        return new int[]{current.y, current.x};
+    }
+    
+    private boolean isBlocked(String[] park, int y1, int y2, int x1, int x2) {
+        System.out.println(y1 +" "+ y2);
+        System.out.println(x1 +" "+ x2);
+        for(int i = y1; i <= y2; i++) {
+            for(int j = x1; j <= x2; j++) {
+                if(park[i].charAt(j) == 'X') {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean isIn(int y, int x, int n, int m) {
+        return y >= 0 && x >= 0 && y < n && x < m;
+    }
+    
+    private void init(Map<String, Point> dirMap) {
+        dirMap.put("N", new Point(-1, 0));
+        dirMap.put("S", new Point(1, 0));
+        dirMap.put("E", new Point(0, 1));
+        dirMap.put("W", new Point(0, -1));
+    }
+    
+    private Point searchStart(int n, int m, String[] park) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(park[i].charAt(j) == 'S') {
+                    return new Point(i, j);
+                }
+            }
+        }
+        return new Point(-1, -1);
+    }
+    
+}
