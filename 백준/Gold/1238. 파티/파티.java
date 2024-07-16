af5eaf29c7ad7e23e42e444a2c3da77343858cba
LeeJongsu <lik9819@naver.com>
2024-07-16 20:50:39 +0900

    [Gold III] Title: 파티, Time: 180 ms, Memory: 19676 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,49 +1,87 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
+    static class Node implements Comparable<Node> {
+        int v, w;
+
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.w - o.w;
+        }
+    }
+
+    static int N, M, X;
+    static List<Node>[] adj, r_adj;
+
+    static final int INF = 100 * 10000 + 1;
+
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        final int INF = 100 * 10000 + 1;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
-
-        int[][] road = new int[N + 1][N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (i == j) continue;
-                road[i][j] = INF;
-            }
+        adj = new ArrayList[N + 1];
+        r_adj = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+            r_adj[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            road[s][e] = t;
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            adj[from].add(new Node(to, cost));
+            r_adj[to].add(new Node(from, cost));
         }
 
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    road[i][j] = Math.min(road[i][j], road[i][k] + road[k][j]);
+        int[] go = dijkstra(adj, X);
+        int[] back = dijkstra(r_adj, X);
+
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            max = Math.max(go[i] + back[i], max);
+        }
+
+        System.out.println(max);
+    }
+
+    private static int[] dijkstra(List<Node>[] lists, int x) {
+        Queue<Node> queue = new PriorityQueue<>();
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+
+        queue.add(new Node(x, 0));
+        dist[x] = 0;
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            int next = current.v;
+
+            for (Node node : lists[next]) {
+                if (dist[node.v] > dist[next] + node.w) {
+                    dist[node.v] = dist[next] + node.w;
+                    queue.add(new Node(node.v, dist[node.v]));
                 }
             }
         }
 
-        int max = 0;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(road[i][X] + road[X][i], max);
-        }
-
-        System.out.println(max);
+        return dist;
     }
 }
