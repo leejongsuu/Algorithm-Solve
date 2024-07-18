336d9071e4db1fbe2b66cefcc1e4d9a1cb5a9e6b
LeeJongsu <lik9819@naver.com>
2024-07-18 23:07:32 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] solution(int n, int m) {
-        
         int gcd = getGcd(n, m);
         int inc = (n * m) / gcd;
         
@@ -8,12 +7,12 @@ class Solution {
     }
     
     private int getGcd(int a, int b) {
+        
         while(b != 0) {
             int temp = b;
             b = a % b;
             a = temp;
         }
-        
         return a;
     }
 }
