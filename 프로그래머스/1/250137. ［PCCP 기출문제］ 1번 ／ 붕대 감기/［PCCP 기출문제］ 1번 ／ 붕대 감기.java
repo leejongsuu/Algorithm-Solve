6c3f330d9a0d1f67bb961b73a4b7134d3fa45769
LeeJongsu <lik9819@naver.com>
2024-08-11 18:52:49 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.10 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,37 +1,31 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-
         int t = bandage[0]; // 시전 시간
         int x = bandage[1]; // 초당 회복량
         int y = bandage[2]; // 추가 회복량
-        int len = attacks.length; // 몬스터 공격 횟수
-        int maxTime = attacks[len-1][0]; // 몬스터의 마지막 공격 시간
-        int hp = health; // 현재 체력량
-        int success = 0; // 연속 성공
         
-        for(int lt = 0, rt = 0; lt <= maxTime; lt++) { // 1초 간격으로 반복문을 돔
-            if(lt < attacks[rt][0]) {
-                success++;
-                if(hp < health) {
-                    hp += x;
-                    if(success >= t) {
-                        hp += y;
-                        success = 0;
-                    }
-                    if(hp > health) {
-                        hp = health;
-                    }
-                }
-            } else {
-                hp -= attacks[rt][1];
-                success = 0;
-                if(hp <= 0) {
-                    return -1;
-                }
-                rt++;
+        int currentHealth = health; // 현재 체력
+        int lastTime = 0; // 마지막 공격 시간 
+        
+        for(int[] attack : attacks) {
+            
+            // 지난 회복량 계산
+            int recoveryTime = attack[0] - lastTime - 1;
+            currentHealth += recoveryTime * x;
+            if(recoveryTime >= t) {
+                currentHealth += (recoveryTime / t) * y;
             }
+            currentHealth = Math.min(currentHealth, health);
+            
+            // 몬스터 공격 계산
+            currentHealth -= attack[1];
+            if(currentHealth <= 0) {
+                return -1;
+            }
+            
+            lastTime = attack[0];
         }
         
-        return hp;
+        return currentHealth;
     }
 }
