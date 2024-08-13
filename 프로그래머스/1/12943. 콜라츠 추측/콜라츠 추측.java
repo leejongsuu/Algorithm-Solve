4f00fc9f9378d89d60f4e989910b71ce050574d2
LeeJongsu <lik9819@naver.com>
2024-08-13 21:36:21 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,11 +1,17 @@
 class Solution {
     public int solution(int num) {
-        long n = (long) num;
         
-        for(int i=0; i<500; i++) {
-            if(n == 1) return i;
-            
-            n = (n%2 == 0) ? n/2 : 3*n + 1; 
+        int cnt = 0;
+        long result = num;
+        while(cnt < 500) {
+            if(result == 1) {
+                return cnt;
+            } else if(result % 2 == 0) {
+                result /= 2;
+            } else {
+                result = 3 * result + 1;
+            }
+            cnt++;
         }
         
         return -1;
