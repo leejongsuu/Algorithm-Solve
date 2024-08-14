21b8ca8cd92163d338388bf6025be1acdd9b464e
LeeJongsu <lik9819@naver.com>
2024-08-14 20:19:03 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(int n) {
-        
-        int sum = 0;
-        
         int size = (int) Math.sqrt(n);
+        int sum = 0;
         for(int i = 1; i <= size; i++) {
             if(n % i == 0) {
                 sum += i;
