9de366e1939c198cbcbcd8d70113cef80b0a4524
LeeJongsu <lik9819@naver.com>
2024-08-14 21:01:14 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.30 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,12 +1,13 @@
 class Solution {
     public String solution(int n) {
         
-        String answer = "";
+        char[] ch = new char[]{'수', '박'};
         
-        for(int i=0; i<n; i++) {
-            answer += (i%2 == 0 ? "수" : "박");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            sb.append(ch[i % 2]);
         }
         
-        return answer;
+        return sb.toString();
     }
 }
