8d0f64d16edf42a0e78be9016bd0a769b1314b47
LeeJongsu <lik9819@naver.com>
2024-08-19 22:25:42 +0900

    Update 최빈값 구하기.java

--- Study/최빈값 구하기.java
+++ Study/최빈값 구하기.java
@@ -8,21 +8,21 @@ class Solution {
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
         
-        int max = 0;
-        int max_cnt = 0;
-        boolean flag = true;
+        int max = -1;
+        int mostFrequentValue = -1;
+        boolean isUnique = true;  // 최빈값이 유일한지 확인하는 플래그
         
         for(Integer key : map.keySet()) {
             int cnt = map.get(key);
-            if(cnt > max_cnt) {
-                flag = true;
-                max_cnt = cnt;
-                max = key;
-            } else if(cnt == max_cnt) {
-                flag = false;
+            if(cnt > max) {
+                max = cnt;
+                mostFrequentValue = key;
+                isUnique = true;  // 새로운 최빈값이 발견되었으므로 유일함
+            } else if(cnt == max) {
+                isUnique = false;  // 최빈값이 여러 개일 경우
             }
         }
         
-        return flag ? max : -1;
+        return isUnique ? mostFrequentValue : -1;  // 유일한 경우 최빈값 반환, 아니면 -1 반환
     }
 }
