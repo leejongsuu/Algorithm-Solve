2872200e6cf48b689d30eed3542400b8ee7e35d7
LeeJongsu <lik9819@naver.com>
2024-08-19 21:24:59 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.70 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,12 +3,13 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         
-        int before = -1;
         ArrayList<Integer> list = new ArrayList<>();
-        for(int num : arr) {
-            if(num != before) {
-                list.add(num);
-                before = num;
+        int before = -1;
+        
+        for(int n : arr) {
+            if(n != before) {
+                list.add(n);
+                before = n;
             }
         }
         
