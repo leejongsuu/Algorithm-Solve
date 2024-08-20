ded214337a8571566a9ceb58f18837af58fec801
LeeJongsu <lik9819@naver.com>
2024-08-20 20:40:34 +0900

    [level 1] Title: 삼총사, Time: 0.08 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,20 +1,20 @@
 class Solution {
     
-    static int answer = 0;
+    static int count = 0;
     
     public int solution(int[] number) {
-        DFS(0, 0, 0, number);
-        return answer;
+        DFS(0, 0, 0, number, number.length);
+        return count;
     }
     
-    private void DFS(int L, int start, int sum, int[] number) {
-        
+    private void DFS(int L, int sum, int start, int[] number, int len) {
         if(L == 3) {
-            if(sum == 0) answer++;
-        } else {
-            for(int i=start; i<number.length; i++) {
-                DFS(L+1, i+1, sum + number[i], number);
-            }
+            if(sum == 0) count++;
+            return;
+        }
+        
+        for(int i = start; i < len; i++) {
+            DFS(L+1, sum + number[i], i + 1, number, len);
         }
     }
 }
