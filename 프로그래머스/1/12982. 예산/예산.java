b86b3852421d1bdec1c9c78ab3c65360470f9ecf
LeeJongsu <lik9819@naver.com>
2024-08-20 19:40:01 +0900

    [level 1] Title: 예산, Time: 1.29 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,18 +1,21 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] d, int budget) {
         
-        int answer = 0;
-        
         Arrays.sort(d);
         
+        int count = 0;
+        int sum = 0;
         for(int cost : d) {
-            if(budget - cost < 0) return answer;
-            answer++;
-            budget -= cost;
+            if(sum + cost > budget) {
+                break;
+            } else {
+                sum += cost;
+                count++;
+            }
         }
         
-        return answer;
+        return count;
     }
 }
