125897531df1160f12077c513df1d3b3ccc770e4
LeeJongsu <lik9819@naver.com>
2024-08-27 19:07:53 +0900

    [level 1] Title: 시저 암호, Time: 0.59 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,16 +1,33 @@
 class Solution {
+    
+    private static final int lower = (int) 'z';
+    private static final int upper = (int) 'Z';
+    
     public String solution(String s, int n) {
-        String answer = "";
         
-        char[] ch = s.toCharArray();
-        for(int i = 0; i < ch.length; i++) {
-            if(Character.isLowerCase(ch[i])) {
-                ch[i] = (char) ((ch[i] + n) % 'a' % 26 + 'a');
-            } else if(Character.isUpperCase(ch[i])) {
-                ch[i] = (char) ((ch[i] + n) % 'A' % 26 + 'A');
+        int len = s.length();
+        
+        char[] result = new char[len];
+        
+        for(int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            
+            if(c == ' ') {
+                result[i] = c;
+            } else if((int) c <= upper) {
+                result[i] = getEncryption(c, n, upper);
+            } else {
+                result[i] = getEncryption(c, n, lower);
             }
         }
-        
-        return String.valueOf(ch);
+        return String.valueOf(result);
+    }
+    
+    private char getEncryption(char c, int n, int boundary) {
+        int value = (int) c + n;
+        if(value > boundary) {
+            value -= 26;
+        }
+        return (char) value;        
     }
 }
