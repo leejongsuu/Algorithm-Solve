59a972a52ff1ec812a00edbcc5db9989337e2400
LeeJongsu <lik9819@naver.com>
2024-09-03 11:41:13 +0900

    [level 1] Title: 추억 점수, Time: 2.12 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -2,21 +2,23 @@ import java.util.HashMap;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int [photo.length];
         
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> memoryMap = new HashMap<>();
         for(int i = 0; i < name.length; i++) {
-            map.put(name[i], yearning[i]);
+            memoryMap.put(name[i], yearning[i]);
         }
         
+        int[] result = new int[photo.length];
         for(int i = 0; i < photo.length; i++) {
+            int sum = 0;
             for(int j = 0; j < photo[i].length; j++) {
-                if(map.containsKey(photo[i][j])) {
-                    answer[i] += map.get(photo[i][j]);
+                String key = photo[i][j];
+                if(memoryMap.containsKey(key)) {
+                    sum += memoryMap.get(key);
                 }
             }
+            result[i] = sum;
         }
-        
-        return answer;
+        return result;
     }
 }
