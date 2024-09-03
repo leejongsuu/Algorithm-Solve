571f024ca2557a2410723d5f635688bee521bbea
LeeJongsu <lik9819@naver.com>
2024-09-03 11:34:32 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.72 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,15 +2,12 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         
         String[] result = new String[n];
-        
         for(int i = 0; i < n; i++) {
             String binary = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
-            binary = binary
-                .substring(16 - n)
-                .replaceAll("1", "#")
-                .replaceAll("0", " ");
-            
-            result[i] = binary;
+            result [i] = binary
+                    .substring(16 - n)
+                    .replaceAll("1", "#")
+                    .replaceAll("0", " ");
         }
         
         return result;
