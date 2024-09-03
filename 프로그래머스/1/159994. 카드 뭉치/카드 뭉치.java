0000f8837e6b4f06cb71598203cb9fdaa8b46e01
LeeJongsu <lik9819@naver.com>
2024-09-03 11:46:44 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,11 +1,17 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        
+
+        int len1 = cards1.length;
+        int len2 = cards2.length;
         int lt = 0, rt =0;
-        for(String str : goal) {
-            if(lt < cards1.length && cards1[lt].equals(str)) lt++;
-            else if(rt < cards2.length && cards2[rt].equals(str)) rt++;
-            else return "No";
+        for(String word : goal) {
+            if(lt < len1 && word.equals(cards1[lt])) {
+                lt++;
+            } else if(rt < len2 && word.equals(cards2[rt])) {
+                rt++;
+            } else {
+                return "No";
+            }
         }
         
         return "Yes";
