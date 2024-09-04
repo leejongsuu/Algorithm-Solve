984b4627ed93fb1d2fdf4e8a5dfa56db42abb1b6
LeeJongsu <lik9819@naver.com>
2024-09-04 19:24:10 +0900

    [level 1] Title: 실패율, Time: 6.89 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,27 +1,6 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.PriorityQueue;
 
 class Solution {
-    public int[] solution(int N, int[] stages) {
-        
-        int[] arr = new int[N+2];
-        for(int i : stages) arr[i]++;
-        
-        ArrayList<Stage> list = new ArrayList<>();
-        int denominator = stages.length;
-        for(int i = 1; i <=N; i++) {
-            denominator -= arr[i-1];
-            if(denominator <= 0) {
-                list.add(new Stage(i, 0));
-                continue;
-            }
-            double cal = (double) arr[i] / denominator;
-            list.add(new Stage(i, cal));
-        }
-        
-        Collections.sort(list);
-        return list.stream().mapToInt(Stage::getNumber).toArray();
-    }
     
     class Stage implements Comparable<Stage> {
         int number;
@@ -32,14 +11,41 @@ class Solution {
             this.failureRate = failureRate;
         }
         
-        public int getNumber() {
-            return this.number;
-        }
-        
         @Override
         public int compareTo(Stage o) {
-            if(o.failureRate == this.failureRate) return this.number - o.number;
-            else return Double.compare(o.failureRate, this.failureRate);
+            // 실패율이 높은 순으로 정렬, 같으면 번호가 낮은 순으로 정렬
+            int flag = Double.compare(o.failureRate, this.failureRate);
+            if(flag == 0) {
+                return this.number - o.number;
+            } else {
+                return flag;
+            }
         }
+    }
+    
+    public int[] solution(int N, int[] stages) {
+        
+        PriorityQueue<Stage> priorityQueue = new PriorityQueue<>();
+        
+        // N + 1 크기의 배열을 사용
+        int[] user = new int[N + 2];
+        for(int stage : stages) {
+            user[stage]++;
+        }
+        
+        int totalPerson = stages.length;
+        for(int i = 1; i <= N; i++) {
+            // 도달한 사용자가 없으면 실패율은 0
+            double rate = totalPerson > 0 ? (double) user[i] / totalPerson : 0;
+            priorityQueue.offer(new Stage(i, rate));  // 스테이지 번호 i, 실패율 rate
+            totalPerson -= user[i];
+        }
+        
+        int[] result = new int[N];
+        for(int i = 0; i < N; i++) {
+            result[i] = priorityQueue.poll().number;  // 스테이지 번호 저장
+        }
+        
+        return result;
     }
 }
