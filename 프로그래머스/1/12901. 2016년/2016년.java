4551484e09e60464d6483bbb44d56c94af4169e9
LeeJongsu <lik9819@naver.com>
2024-09-04 11:33:23 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,28 +1,14 @@
-import java.util.HashMap;
-
 class Solution {
     public String solution(int a, int b) {
+
+        int[] months = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         
-        int[] thirty = new int[]{4,6,9,11};
-        int[] thirty_one = new int[]{1,3,5,7,8,10,12};
-        
-        HashMap<Integer, Integer> dayMap = new HashMap<>();
-        dayMap.put(2, 29);
-        for(int i = 0; i < thirty.length; i++) {
-            dayMap.put(thirty[i], 30);
-        }
-        for(int i = 0; i < thirty_one.length; i++) {
-            dayMap.put(thirty_one[i], 31);
+        for(int i = 0; i < a; i++) {
+            b += months[i];
         }
         
-        int day = 0;
+        String[] week = new String[]{"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
         
-        for(int month = 1; month < a; month++) {
-            day += dayMap.get(month);
-        }
-        day += b;
-        
-        String[] week = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-        return week[(day - 1) % 7];
+        return week[(b - 1) % 7];
     }
 }
