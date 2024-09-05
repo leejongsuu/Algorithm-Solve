2f9a1c2f17870c4cc555c9a40f3bd629eb5e5b4a
LeeJongsu <lik9819@naver.com>
2024-09-05 18:55:35 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,24 +1,23 @@
-import java.util.HashSet;
-
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         
-        HashSet<Integer> winSet = new HashSet<>();
-        for(int i : win_nums) winSet.add(i);
+        boolean[] lottoArr = new boolean[46];
+        for(int win_num : win_nums) {
+            lottoArr[win_num] = true;
+        }
         
+        int corrcet = 0;
         int zero_count = 0;
-        int correct = 0;
-        for(int i : lottos) {
-            if(winSet.contains(i)) {
-                correct++;
-            } else if(i == 0) {
+        for(int lotto : lottos) {
+            if(lottoArr[lotto]) {
+                corrcet++;
+            } else if(lotto == 0) {
                 zero_count++;
             }
         }
-    
-        int high = 7 - (correct + zero_count);
-        int low = 7 - correct;
         
+        int high = 7 - (corrcet + zero_count);    
+        int low = 7 - corrcet;
         if(high > 6) high = 6;
         if(low > 6) low = 6;
         
