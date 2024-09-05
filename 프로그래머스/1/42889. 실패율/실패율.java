283fc332d96580429379139452c5d0e1ec9da25d
LeeJongsu <lik9819@naver.com>
2024-09-05 16:52:04 +0900

    [level 1] Title: 실패율, Time: 38.16 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,4 +1,6 @@
-import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Solution {
     
@@ -11,9 +13,12 @@ class Solution {
             this.failureRate = failureRate;
         }
         
+        public int getNumber() {
+            return this.number;
+        }
+        
         @Override
         public int compareTo(Stage o) {
-            // 실패율이 높은 순으로 정렬, 같으면 번호가 낮은 순으로 정렬
             int flag = Double.compare(o.failureRate, this.failureRate);
             if(flag == 0) {
                 return this.number - o.number;
@@ -21,31 +26,31 @@ class Solution {
                 return flag;
             }
         }
+        
     }
     
     public int[] solution(int N, int[] stages) {
         
-        PriorityQueue<Stage> priorityQueue = new PriorityQueue<>();
+        Arrays.sort(stages);
         
-        // N + 1 크기의 배열을 사용
-        int[] user = new int[N + 2];
-        for(int stage : stages) {
-            user[stage]++;
+        ArrayList<Stage> resultList = new ArrayList<>();
+        
+        int len = stages.length;
+        int totalPerson = len;
+        
+        for(int i = 1, j = 0; i <= N; i++) {
+            int cnt = 0;
+            while(j < len && i == stages[j]) {
+                cnt++;
+                j++;
+            }
+            double rate = totalPerson == 0 ? 0 : (double) cnt / totalPerson;
+            resultList.add(new Stage(i, rate));
+            totalPerson -= cnt;
         }
         
-        int totalPerson = stages.length;
-        for(int i = 1; i <= N; i++) {
-            // 도달한 사용자가 없으면 실패율은 0
-            double rate = totalPerson > 0 ? (double) user[i] / totalPerson : 0;
-            priorityQueue.offer(new Stage(i, rate));  // 스테이지 번호 i, 실패율 rate
-            totalPerson -= user[i];
-        }
+        Collections.sort(resultList);
         
-        int[] result = new int[N];
-        for(int i = 0; i < N; i++) {
-            result[i] = priorityQueue.poll().number;  // 스테이지 번호 저장
-        }
-        
-        return result;
+        return resultList.stream().mapToInt(Stage::getNumber).toArray();
     }
 }
