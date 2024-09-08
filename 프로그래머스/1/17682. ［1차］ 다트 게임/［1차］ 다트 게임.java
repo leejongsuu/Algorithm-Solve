ba0f726c2dd6fb08e0833056755c7dd6589c3f45
LeeJongsu <lik9819@naver.com>
2024-09-08 16:27:08 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.34 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,41 +1,40 @@
 import java.util.Stack;
 
 class Solution {
-    public int solution(String dartResult) {
+     public int solution(String dartResult) {
+        Stack<Integer> scoreStack = new Stack<>();
+         
+         for(int i = 0; i < dartResult.length(); i++) {
+             char c = dartResult.charAt(i);
+             if(Character.isDigit(c)) {
+                 if(Character.isDigit(dartResult.charAt(i+1))) {
+                     scoreStack.push(10);
+                     i++;
+                 } else {
+                     scoreStack.push(c -'0');
+                 }
+                 continue;
+             }
+             
+             switch(c) {
+                 case 'D' -> scoreStack.push((int) Math.pow(scoreStack.pop(), 2));
+                 case 'T' -> scoreStack.push((int) Math.pow(scoreStack.pop(), 3));
+                 case '#' -> scoreStack.push(-1 * scoreStack.pop());
+                 case '*' -> {
+                     int first = scoreStack.pop();
+                     if(!scoreStack.isEmpty()) {
+                         int second = scoreStack.pop();
+                         scoreStack.push(2 * second);
+                     }
+                     scoreStack.push(2 * first);
+                 }
+             }
+         }
+         int result = 0;
+         for(int score : scoreStack) {
+             result += score;
+         }
 
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i = 0; i < dartResult.length(); i++) {
-            char ch = dartResult.charAt(i);
-            if(Character.isDigit(ch)) {
-                if(ch == '1' && dartResult.charAt(i+1) == '0') {
-                    stack.push(10);
-                    i++;
-                } else {
-                    stack.push(ch - '0');    
-                }
-                continue;
-            }
-            switch(ch) {
-                case 'D' -> stack.push((int) Math.pow(stack.pop(), 2));
-                case 'T' -> stack.push((int) Math.pow(stack.pop(), 3));
-                case '#' -> stack.push(-1 * stack.pop());
-                case '*' -> {
-                    int second = stack.pop();
-                    if(!stack.isEmpty()) {
-                        int first = stack.pop();
-                        stack.push(first * 2);
-                    }
-                    stack.push(second * 2);
-                }
-            }
-        }
-        
-        int result = 0;
-        for(int value : stack) {
-            result+= value;
-        }
-        
-        return result;
+         return result;
     }
 }
