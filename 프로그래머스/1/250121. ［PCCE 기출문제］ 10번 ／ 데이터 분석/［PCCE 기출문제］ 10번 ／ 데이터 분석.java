43436f843a77d0dc13d0beaf55b28f2110594c8b
LeeJongsu <lik9819@naver.com>
2024-09-11 21:16:10 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 28.55 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,26 +1,24 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Comparator;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
         
-        // 열 인덱스 매핑
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("code", 0);
-        map.put("date", 1);
-        map.put("maximum", 2);
-        map.put("remain", 3);
+        HashMap<String, Integer> indexMap = new HashMap<>();
+        indexMap.put("code", 0);
+        indexMap.put("date", 1);
+        indexMap.put("maximum", 2);
+        indexMap.put("remain", 3);
         
-        // ext와 sort_by에 해당하는 열 인덱스
-        int extIndex = map.get(ext);
-        int sortByIndex = map.get(sort_by);
+        int extIndex = indexMap.get(ext);
+        int sortIndex = indexMap.get(sort_by);
         
-        // 스트림을 사용하여 필터링 및 정렬
-        int[][] result = Arrays.stream(data)
-            .filter(row -> row[extIndex] < val_ext) // ext 기준으로 필터링
-            .sorted(Comparator.comparingInt(row -> row[sortByIndex])) // sort_by 기준으로 정렬
-            .toArray(int[][]::new);
+        return Arrays.stream(data)
+                    .filter(row -> row[extIndex] < val_ext)
+                    .sorted(Comparator.comparingInt(row -> row[sortIndex]))
+                    .toArray(int[][]::new);
         
         
-        return result;
     }
 }
