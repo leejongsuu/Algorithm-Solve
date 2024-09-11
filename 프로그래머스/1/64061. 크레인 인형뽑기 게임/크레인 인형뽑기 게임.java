bcc75c872045274b2058e041f481956b56ca2eae
LeeJongsu <lik9819@naver.com>
2024-09-11 20:47:44 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.18 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,37 +1,37 @@
 import java.util.Stack;
-import java.util.Arrays;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
         
-        int N = board.length;
-        Stack<Integer>[] stack = new Stack[N + 1];
-        for(int i = 0; i<=N; i++) stack[i] = new Stack<>();
+        int boom = 0;
+        int n = board.length;
         
-        for(int i = 0; i < N; i++) {
-            for(int j = N - 1; j >= 0; j--) {
-                if(board[j][i] == 0) continue;
-                stack[i + 1].push(board[j][i]);
+        Stack<Integer> drawStack = new Stack<>();
+        Stack<Integer>[] boardStack = new Stack[n];
+        for(int i = 0; i < n; i++) {
+            boardStack[i] = new Stack<>();
+        }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = n - 1; j >= 0; j--) {
+                if(board[j][i] != 0) {
+                    boardStack[i].push(board[j][i]);
+                }
             }
         }
         
-        Stack<Integer> result = new Stack<>();
-        for(int m : moves) {
-            if(stack[m].isEmpty()) continue;
+        for(int move : moves) {
+            if(boardStack[move - 1].isEmpty()) continue;
             
-            int n = stack[m].pop();
-            if(result.isEmpty()) {
-                result.push(n);
-            }
-            else if(result.peek() == n) {
-                result.pop();
-                answer+=2;
+            int draw = boardStack[move - 1].pop();
+            if(!drawStack.isEmpty() && drawStack.peek() == draw) {
+                boom += 2;
+                drawStack.pop();
             } else {
-                result.push(n);
-            }               
+                drawStack.push(draw);
+            }
         }
         
-        return answer;
+        return boom;
     }
 }
