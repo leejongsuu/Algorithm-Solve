07d561684d8662a0a46d1abdd85048b5c09df2ed
LeeJongsu <lik9819@naver.com>
2024-09-24 19:58:58 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.19 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,20 +1,45 @@
 class Solution {
+    
     public int[] solution(String[] wallpaper) {
+        
         int n = wallpaper.length;
         int m = wallpaper[0].length();
-        int minY = n, minX = m, maxY = 0, maxX = 0;
+        
+        int lux, luy, rdx, rdy;
+        lux = luy = rdx = rdy = 0;
+        
+        int[] start = init(n, m, wallpaper);
+        luy = start[0];
+        lux = start[1];
         
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(wallpaper[i].charAt(j) == '#') {
-                    if(i < minY) minY = i;
-                    if(j < minX) minX = j;
-                    if(i + 1 > maxY) maxY = i + 1;
-                    if(j + 1 > maxX) maxX = j + 1;
+                    if(j < lux) {
+                        lux = j;
+                    }
+                    if(i + 1 > rdy) {
+                        rdy = i + 1;
+                    }
+                    if(j + 1 > rdx) {
+                        rdx = j + 1;
+                    }
                 }
             }
         }
-
-        return new int[]{minY, minX, maxY, maxX};
+        
+        return new int[]{luy, lux, rdy, rdx};
+    }
+    
+    private int[] init(int n, int m, String[] wallpaper) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(wallpaper[i].charAt(j) == '#') {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        
+        return new int[]{-1, -1};
     }
 }
