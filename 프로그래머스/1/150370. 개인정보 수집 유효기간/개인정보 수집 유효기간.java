fc888e7da40f76c20c2cc9250a82842a89ee718a
LeeJongsu <lik9819@naver.com>
2024-09-26 12:35:10 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 14.44 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -4,33 +4,35 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         
-        ArrayList<Integer> list = new ArrayList<>();
+        int INF = getDays(today);
         
-        HashMap<Character, Integer> termsMap = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         for(String term : terms) {
             String[] splited = term.split(" ");
-            termsMap.put(splited[0].charAt(0), Integer.valueOf(splited[1]) * 28);
+            map.put(splited[0].charAt(0), Integer.parseInt(splited[1]) * 28);
         }
         
-        int INF = calculateDate(today);
+        ArrayList<Integer> list = new ArrayList<>();
         
         for(int i = 0; i < privacies.length; i++) {
             String[] splited = privacies[i].split(" ");
             
-            if(INF >= calculateDate(splited[0]) + termsMap.get(splited[1].charAt(0))) {
+            int sum = getDays(splited[0]) + map.get(splited[1].charAt(0));
+            if(INF >= sum) {
                 list.add(i + 1);
             }
         }
         return list.stream().mapToInt(Integer::intValue).toArray();
+        
     }
     
-    private int calculateDate(String str) {
-        String[] date = str.split("[.]");
+    private int getDays(String date) {
+        String[] splited = date.split("[.]");
         
-        int year = Integer.parseInt(date[0]);
-        int month = Integer.parseInt(date[1]);
-        int day = Integer.parseInt(date[2]);
+        int year = Integer.parseInt(splited[0]) - 1;
+        int month = Integer.parseInt(splited[1]) - 1;
+        int day = Integer.parseInt(splited[2]);
         
-        return (year * 28 * 12) + (month * 28) + day;
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }
