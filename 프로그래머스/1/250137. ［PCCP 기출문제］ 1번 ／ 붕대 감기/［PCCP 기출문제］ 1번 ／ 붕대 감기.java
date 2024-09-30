779e6450c92e7d1b681f6d38a1df40fbcdb59083
LeeJongsu <lik9819@naver.com>
2024-09-30 15:19:52 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.05 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,29 +1,28 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int t = bandage[0]; // 시전 시간
-        int x = bandage[1]; // 초당 회복량
-        int y = bandage[2]; // 추가 회복량
         
-        int currentHealth = health; // 현재 체력
-        int lastTime = 0; // 마지막 공격 시간 
+        int t = bandage[0];
+        int x = bandage[1];
+        int y = bandage[2];
         
-        for(int[] attack : attacks) {
+        int beforeTime = 0;
+        int currentHealth = health;
+        for(int i = 0; i < attacks.length; i++) {
+            int attackTime = attacks[i][0];
+            int demage = attacks[i][1];
             
-            // 지난 회복량 계산
-            int recoveryTime = attack[0] - lastTime - 1;
-            currentHealth += recoveryTime * x;
-            if(recoveryTime >= t) {
-                currentHealth += (recoveryTime / t) * y;
-            }
-            currentHealth = Math.min(currentHealth, health);
+            int time = attackTime - beforeTime - 1;
+            currentHealth += time * x;
+            currentHealth += (time / t) * y;
+            currentHealth = Math.min(health, currentHealth);
             
-            // 몬스터 공격 계산
-            currentHealth -= attack[1];
+            currentHealth -= demage;
+            
             if(currentHealth <= 0) {
                 return -1;
             }
             
-            lastTime = attack[0];
+            beforeTime = attackTime;
         }
         
         return currentHealth;
