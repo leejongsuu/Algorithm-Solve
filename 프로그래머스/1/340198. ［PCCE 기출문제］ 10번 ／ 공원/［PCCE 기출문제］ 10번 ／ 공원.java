4ea9e05b755d97a6c51d2f55c390ab00ca3df702
LeeJongsu <lik9819@naver.com>
2024-10-01 10:11:58 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 3.08 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,42 +1,37 @@
 import java.util.Arrays;
 
 class Solution {
-    
     public int solution(int[] mats, String[][] park) {
         
         int row = park.length;
         int col = park[0].length;
-        int maxSize = -1;
         
         Arrays.sort(mats);
         
         for(int m = mats.length - 1; m >= 0; m--) {
-            
-            for(int i = 0; i <= row - mats[m]; i++) {
-                boolean flag = false;
-                for(int j = 0; j <= col - mats[m]; j++) {
-                    if(canPlace(i, j, mats[m], park)) {
-                        flag = true;
-                        break;
+            int size = mats[m];
+            for(int i = 0; i <= row - size; i++) {
+                for(int j = 0; j <= col - size; j++) {
+                    if(canPlace(i, j, size, park)) {
+                        return size;
                     }
-                }
-                
-                if(flag) {
-                    return mats[m];
                 }
             }
         }
+        
         return -1;
     }
-
+    
     private boolean canPlace(int y, int x, int size, String[][] park) {
+        
         for(int i = y; i < y + size; i++) {
-             for(int j = x; j < x + size; j++) {
-                 if(!park[i][j].equals("-1")) {
-                     return false;
-                 }
-             }
-         }
+            for(int j = x; j < x + size; j++) {
+                if(!park[i][j].equals("-1")) {
+                    return false;
+                }
+            }
+        }
+        
         return true;
     }
 }
