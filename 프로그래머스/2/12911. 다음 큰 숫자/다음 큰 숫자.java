48396677b12adbf7fcad68a75519a126b2223d1f
LeeJongsu <lik9819@naver.com>
2024-10-02 14:25:00 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,24 +1,11 @@
 class Solution {
     public int solution(int n) {
-        
-        final int inf = getOneCount(n);
-        
+        int a = Integer.bitCount(n);
         while(true) {
             n++;
-            if(inf == getOneCount(n)) {
+            if(a == Integer.bitCount(n)) {
                 return n;
             }
         }
-    }
-    
-    private int getOneCount(int n) {
-        String str = Integer.toBinaryString(n);
-        int count = 0;
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == '1') {
-                count++;
-            }
-        }
-        return count;
     }
 }
