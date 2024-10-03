65abdbcac3dddeb3b1d06d2248341a13bcb4ee50
LeeJongsu <lik9819@naver.com>
2024-10-03 17:03:37 +0900

    [level 2] Title: 카펫, Time: 0.10 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         
-        int[] answer = new int[2];
+        int sqrt = (int) Math.sqrt(yellow);
         
-        for(int i=1; i<= yellow/2+1; i++) {
-            if(yellow % i == 0) {
-                int x = yellow / i;
-                int y = i;
-                int total = (x+2) * 2 + y * 2;
-                if(total == brown) {
-                    answer[0] = x+2;
-                    answer[1] = y+2;
-                    break;
+        for(int row = 1; row <= sqrt; row++) {
+            if(yellow % row == 0) {
+                int col = yellow / row;
+                
+                if(((row * 2 + 4) + col * 2) == brown) {
+                    return new int[]{col + 2, row + 2};
                 }
-            } 
+            }
         }
         
-        return answer;
+        return new int[]{-1, -1};
     }
 }
