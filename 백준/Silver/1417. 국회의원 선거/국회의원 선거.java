48f4cd3a048cad5c7b4e6b51c70901f75a63d06e
LeeJongsu <lik9819@naver.com>
2024-10-08 18:47:54 +0900

    [Silver V] Title: 국회의원 선거, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int dasom = Integer.parseInt(br.readLine());
+
+        int[] candidates = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            candidates[i] = Integer.parseInt(br.readLine());
+        }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int candidate : candidates) {
+            priorityQueue.offer(candidate);
+        }
+
+        int count = 0;
+        while (!priorityQueue.isEmpty()) {
+            int temp = priorityQueue.poll();
+            if (dasom > temp) {
+                break;
+            }
+            priorityQueue.offer(temp - 1);
+            dasom++;
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
