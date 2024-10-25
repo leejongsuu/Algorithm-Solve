93ba71160486920a90e879688c2c0db572331610
LeeJongsu <lik9819@naver.com>
2024-10-25 17:24:54 +0900

    [level 2] Title: 귤 고르기, Time: 69.99 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,58 +1,26 @@
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     
-    class Box implements Comparable<Box> {
-        int bno;
-        int count;
-        
-        public Box(int bno, int count) {
-            this.bno = bno;
-            this.count = count;
-        }
-        
-        public void setCount(int count) {
-            this.count = count;
-        }
-        
-        @Override
-        public int compareTo(Box o) {
-            return o.count - this.count;
-        }
-    }
-    
     public int solution(int k, int[] tangerine) {
-        HashMap<Integer, Integer> tangerineMap = new HashMap<>();
-        for(int num : tangerine) {
-            tangerineMap.put(num, tangerineMap.getOrDefault(num, 0) + 1);
+        
+        Map<Integer, Integer> tangerineMap = new HashMap<>();
+        
+        for(int t : tangerine) {
+            tangerineMap.put(t, tangerineMap.getOrDefault(t, 0) + 1);
         }
         
-        PriorityQueue<Box> queue = new PriorityQueue<>();
-        for(int key : tangerineMap.keySet()) {
-            queue.offer(new Box(key, tangerineMap.get(key)));
-        }
+        List<Integer> list = new ArrayList<>(tangerineMap.values());
+        list.sort((o1, o2) -> o2 - o1);
         
         int result = 0;
-        int number = 0;
-        int before = 0;
-        while(number < k && !queue.isEmpty()) {            
-            Box currentBox = queue.peek();
-            if(before != currentBox.bno) {
-                result++;
+        for(int cnt : list) {
+            k-=cnt;
+            result++;
+            if(k <= 0) {
+                break;
             }
-            before = currentBox.bno;
-            
-            int cnt = currentBox.count;
-            if(cnt == 1) {
-                queue.poll();
-            } else {
-                currentBox.setCount(cnt - 1);    
-            }
-            
-            number++;
         }
-        
         return result;
     }
 }
