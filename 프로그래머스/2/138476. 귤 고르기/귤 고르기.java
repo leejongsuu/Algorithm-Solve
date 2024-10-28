13bc1fc41708a7240c944d428a7d5d40a86212cb
LeeJongsu <lik9819@naver.com>
2024-10-28 16:28:07 +0900

    [level 2] Title: 귤 고르기, Time: 59.31 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,11 +1,9 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int k, int[] tangerine) {
         
         Map<Integer, Integer> tangerineMap = new HashMap<>();
-        
         for(int t : tangerine) {
             tangerineMap.put(t, tangerineMap.getOrDefault(t, 0) + 1);
         }
@@ -14,13 +12,14 @@ class Solution {
         list.sort((o1, o2) -> o2 - o1);
         
         int result = 0;
-        for(int cnt : list) {
-            k-=cnt;
+        for(int l : list) {
+            k -= l;
             result++;
             if(k <= 0) {
                 break;
             }
         }
+        
         return result;
     }
 }
