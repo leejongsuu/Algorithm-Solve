f210b068d35df93b7a304788cfb38d8e6e2cd72e
LeeJongsu <lik9819@naver.com>
2024-10-31 17:40:47 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        
-        int result = 1;
-        
+
         Map<String, Integer> map = new HashMap<>();
         for(String[] strArr : clothes) {
             map.put(strArr[1], map.getOrDefault(strArr[1], 0) + 1);
         }
         
+        int result = 1;
         for(int value : map.values()) {
             result *= (value + 1);
         }
         
-        return result -1;
+        return result - 1;
     }
 }
