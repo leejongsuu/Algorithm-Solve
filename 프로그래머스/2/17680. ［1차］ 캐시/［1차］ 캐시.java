b8d445e0a47150d6943a9d82e19781295e414163
LeeJongsu <lik9819@naver.com>
2024-10-31 17:34:05 +0900

    [level 2] Title: [1차] 캐시, Time: 85.74 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -7,17 +7,18 @@ class Solution {
         final int MISS = 5;
         
         int totalTime = 0;
-        Queue<String> cache = new LinkedList<>();
         
         if(cacheSize == 0) {
-            return cities.length * MISS;
+            return MISS * cities.length;
         }
         
+        Queue<String> cache = new LinkedList<>();
+        
         for(String city : cities) {
+            
             city = city.toLowerCase();
             
             boolean hit = false;
-            
             for(String c : cache) {
                 if(c.equals(city)) {
                     hit = true;
@@ -25,16 +26,12 @@ class Solution {
                     break;
                 }
             }
-            
             if(!hit && cache.size() >= cacheSize) {
                 cache.poll();
-            } 
-            
+            }
             cache.offer(city);
-            
             totalTime += hit ? HIT : MISS;
         }
-        
         return totalTime;
     }
 }
