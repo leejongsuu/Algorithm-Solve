4778dcb502d06028ff472e5a808aa28e44be7a18
LeeJongsu <lik9819@naver.com>
2024-11-04 21:15:19 +0900

    [level 2] Title: 튜플, Time: 67.83 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -3,31 +3,20 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         
-        StringTokenizer st = new StringTokenizer(s, "{}");
+        Set<Integer> tuple = new HashSet<>();
         
-        int size = st.countTokens() / 2 + 1;
-        String[] strArr = new String[size];
-        
-        for(int i = 0; st.hasMoreTokens(); i++) {
-            String token  = st.nextToken();
-            if(i % 2 == 1) {
-                continue;
-            }
-            strArr[i/2] = token;
-        }
-        
+        String[] strArr = s.replaceAll("[{}]", " ").trim().split(" , ");
         Arrays.sort(strArr, (o1, o2) -> o1.length() - o2.length());
         
+        int size = strArr.length;
         int[] result = new int[size];
-        Set<Integer> set = new HashSet<>();
         
         for(int i = 0; i < size; i++) {
-            String[] split = strArr[i].split(",");
-            for(int j = 0; j < split.length; j++) {
-                int iNum = Integer.parseInt(split[j]);
-                if(!set.contains(iNum)) {
-                    set.add(iNum);
-                    result[i] = iNum;
+            String[] elements = strArr[i].split(",");
+            for(int j = 0; j < elements.length; j++) {
+                int element = Integer.parseInt(elements[j]);
+                if(tuple.add(element)) {
+                    result[i] = element;
                     break;
                 }
             }
