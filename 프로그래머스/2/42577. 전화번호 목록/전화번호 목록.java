f6a476df1a8e2ca33b33d1d9bc030f5e11275114
LeeJongsu <lik9819@naver.com>
2024-11-04 22:28:36 +0900

    [level 2] Title: 전화번호 목록, Time: 261.10 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,22 +2,27 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        
-        boolean answer = true;
-        
+        // 전화번호를 저장할 HashSet 생성
         HashSet<String> set = new HashSet<>();
-        for(String phoneNumber : phone_book) {
+        
+        // 전화번호를 HashSet에 저장
+        for (String phoneNumber : phone_book) {
             set.add(phoneNumber);
         }
         
-        for(String str : phone_book) {
-            for(int i=1; i< str.length(); i++) {
-                if(set.contains(str.substring(0,i))) {
+        // 각 전화번호에 대해 접두사가 있는지 확인
+        for (String phoneNumber : phone_book) {
+            // 전화번호의 가능한 모든 접두사를 확인
+            for (int i = 1; i < phoneNumber.length(); i++) {
+                String prefix = phoneNumber.substring(0, i);
+                // HashSet에 접두사가 있다면 false 반환
+                if (set.contains(prefix)) {
                     return false;
                 }
             }
         }
         
-        return answer;
+        // 모든 접두사를 확인한 후에도 중복되는 접두사가 없다면 true 반환
+        return true;
     }
 }
