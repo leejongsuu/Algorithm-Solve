8f7883c28a830d1bc8a8c8933e4345d7d632657c
LeeJongsu <lik9819@naver.com>
2024-11-04 16:26:20 +0900

    [level 2] Title: 기능개발, Time: 2.87 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,38 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-    
-        int len = progresses.length;
-        int[] days = new int[len];
-    
-        Map<Integer, Integer> map = new HashMap<>();
         
-        for(int i = 0; i < len; i++) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < progresses.length; i++) {
             int remain = 100 - progresses[i];
+            
             int day = remain / speeds[i];
             if(remain % speeds[i] != 0) {
                 day++;
             }
-            days[i] = day;
-        }
-        
-        int max = 0;
-        int key = 0;
-        for(int day : days) {
-            if(day > max) {
-                map.put(++key, 1);
-                max = day;
-            } else {
-                map.put(key, map.get(key) + 1);
+            
+            if(!queue.isEmpty() && queue.peek() < day) {
+                list.add(queue.size());
+                queue.clear();
             }
+            
+            queue.offer(day);
         }
+        list.add(queue.size());
         
-        int[] result = new int[map.size()];
-        int index = 0;
-        for(int value : map.values()) {
-            result[index++] = value;
-        }
-        
-        return result;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
