29d79986ae668e4d263d8d246911e876fc52bc35
LeeJongsu <lik9819@naver.com>
2024-11-08 11:00:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.17 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 56.5 MB, 시간: 5.47 ms
+메모리: 54.7 MB, 시간: 8.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 21:07:29
+2024년 11월 08일 11:00:53
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,32 +2,52 @@ import java.util.*;
 
 class Solution {
     
+    class Point {
+        int y;
+        int x;
+        
+        public Point(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+    
     public int solution(int[][] maps) {
         
-        int row = maps.length;
-        int col = maps[0].length;
+        int n = maps.length;
+        int m = maps[0].length;
         
-        int[][] direction = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        int[] dy = new int[]{1, 0, -1, 0};
+        int[] dx = new int[]{0, 1, 0, -1};
         
-        Queue<int[]> Q = new LinkedList<>();
-        Q.offer(new int[]{0,0,1});
         
-        while(!Q.isEmpty()) {
-            int[] current = Q.poll();
-            
-            if(current[0] ==  row - 1 && current[1] == col -1) return current[2];
-            
-            for(int[] dir : direction) {
-                int ny = current[0] + dir[0];
-                int nx = current[1] + dir[1];
-                int distance = current[2];
-                if(ny>=0 && nx>=0 && ny < row && nx< col && maps[ny][nx]==1) {
-                    maps[ny][nx] = 0;
-                    Q.offer(new int[]{ny,nx, distance + 1});
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(0, 0));
+        
+        int level = 1;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                Point current = queue.poll();
+                for(int j = 0; j < 4; j++) {
+                    int ny = current.y + dy[j];
+                    int nx = current.x + dx[j];
+                    if(ny == n - 1 && nx == m - 1) {
+                        return level + 1;
+                    }
+                    if(isIn(ny, nx, n, m) && maps[ny][nx] == 1) {
+                        maps[ny][nx] = 0;
+                        queue.offer(new Point(ny, nx));
+                    }
                 }
             }
+            level++;
         }
         
         return -1;
+    }
+    
+    private boolean isIn(int y, int x, int n, int m) {
+        return y >= 0 && x >= 0 && y < n && x < m;
     }
 }
