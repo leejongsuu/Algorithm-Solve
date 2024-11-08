bef77e2eb23b228d8b29344082d0d87a7a899937
LeeJongsu <lik9819@naver.com>
2024-11-08 10:49:42 +0900

    [Silver II] Title: 한 줄로 서기, Time: 100 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -10,12 +10,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N + 1];
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
