ab3e85da42ae42b916e11cbcf6ac555f40ef324d
LeeJongsu <lik9819@naver.com>
2024-11-13 21:58:53 +0900

    [Silver V] Title: 영화감독 숌, Time: 276 ms, Memory: 87672 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -3,27 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    public int solution(int n) {
-
-        int cnt = 1, num = 666;
-        while (n!=cnt) {
-            num++;
-
-            if (String.valueOf(num).contains("666")) {
-                cnt++;
-            }
-        }
-        return num;
-    }
-
     public static void main(String[] args) throws IOException {
-
-        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        long result = 0;
         int n = Integer.parseInt(br.readLine());
 
-        System.out.println(T.solution(n));
+        for (long i = 666; ; i++) {
+            if (String.valueOf(i).contains("666")) {
+                n--;
+            }
+
+            if (n <= 0) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
     }
 }
