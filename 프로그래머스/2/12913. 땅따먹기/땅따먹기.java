ba020b3dd2fc7fe2e36d1da31aa6a923469e2253
LeeJongsu <lik9819@naver.com>
2024-11-17 12:19:45 +0900

    [level 2] Title: 땅따먹기, Time: 39.18 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,20 +1,10 @@
 class Solution {
     int solution(int[][] land) {
-        int size = land.length;
-        
-        int[][] dp = new int[size][4];
-        
-        for(int col = 0; col < 4; col++) {
-            dp[0][col] = land[0][col];
-        }
-        
-        for(int row = 1; row < size; row++) {
-            dp[row][0] = Math.max(Math.max(dp[row-1][1], dp[row-1][2]), dp[row-1][3]) + land[row][0];
-            dp[row][1] = Math.max(Math.max(dp[row-1][0], dp[row-1][2]), dp[row-1][3]) + land[row][1];
-            dp[row][2] = Math.max(Math.max(dp[row-1][0], dp[row-1][1]), dp[row-1][3]) + land[row][2];
-            dp[row][3] = Math.max(Math.max(dp[row-1][0], dp[row-1][1]), dp[row-1][2]) + land[row][3];
-        }
-        
-        return Math.max(Math.max(dp[size-1][0], dp[size-1][1]), Math.max(dp[size-1][2], dp[size-1][3]));
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
     }
 }
