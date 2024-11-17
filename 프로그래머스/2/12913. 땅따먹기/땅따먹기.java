801b1233ef5582ccf6b220fc072d82dff074cf8e
LeeJongsu <lik9819@naver.com>
2024-11-17 12:19:24 +0900

    [level 2] Title: 땅따먹기, Time: 39.18 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,31 +1,20 @@
 class Solution {
-    
     int solution(int[][] land) {
+        int size = land.length;
         
-        int n = land.length;
-        int m = land[0].length;
+        int[][] dp = new int[size][4];
         
-        int[][] dp = new int[n][m];
-        for(int col = 0; col < m; col++) {
+        for(int col = 0; col < 4; col++) {
             dp[0][col] = land[0][col];
         }
-       
-        for(int row = 1; row < n; row++) {
-            for(int col = 0; col < m; col++) {
-                int max = 0;
-                for(int k = 0; k < m; k++) {
-                    if(col == k) continue;
-                    max = Math.max(max, land[row][col] + dp[row - 1][k]);
-                }
-                dp[row][col] = max;
-            }
+        
+        for(int row = 1; row < size; row++) {
+            dp[row][0] = Math.max(Math.max(dp[row-1][1], dp[row-1][2]), dp[row-1][3]) + land[row][0];
+            dp[row][1] = Math.max(Math.max(dp[row-1][0], dp[row-1][2]), dp[row-1][3]) + land[row][1];
+            dp[row][2] = Math.max(Math.max(dp[row-1][0], dp[row-1][1]), dp[row-1][3]) + land[row][2];
+            dp[row][3] = Math.max(Math.max(dp[row-1][0], dp[row-1][1]), dp[row-1][2]) + land[row][3];
         }
         
-        int result = 0;
-        for(int col = 0; col < m; col++) {
-            result = Math.max(result, dp[n - 1][col]);
-        }
-        
-        return result;
+        return Math.max(Math.max(dp[size-1][0], dp[size-1][1]), Math.max(dp[size-1][2], dp[size-1][3]));
     }
 }
