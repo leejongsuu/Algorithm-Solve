83f6f8cb43295deb3f90e387fb433a19a5d9473b
LeeJongsu <lik9819@naver.com>
2024-11-21 12:55:43 +0900

    Create 14916_거스름돈.java

---
+++ Study/14916_거스름돈.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final int INF = 100001;
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n + 1];
+        int[] coin = new int[]{5, 2};
+
+        Arrays.fill(arr, INF);
+        arr[0] = 0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = coin[i]; j <= n; j++) {
+                arr[j] = Math.min(arr[j], arr[j - coin[i]] + 1);
+            }
+        }
+
+        System.out.println(arr[n] == INF ? -1 : arr[n]);
+    }
+}
