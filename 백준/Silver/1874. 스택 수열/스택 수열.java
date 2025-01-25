778518dc0e4d608db99063b8b1c76055676a8af8
LeeJongsu <lik9819@naver.com>
2025-01-25 17:45:51 +0900

    [Silver II] Title: 스택 수열, Time: 304 ms, Memory: 28624 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -8,14 +8,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
         int[] sequence = new int[N];
-        int max = 0;
-
         for (int i = 0; i < N; i++) {
             sequence[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, sequence[i]);
         }
 
         StringBuilder sb = new StringBuilder();
@@ -25,20 +21,17 @@ public class Main {
         for (int rt = 1; rt <= N; rt++) {
             stack.push(rt);
             sb.append('+').append('\n');
-            while (lt < N && !stack.isEmpty() && stack.peek() == sequence[lt]) {
+            while (lt < N && !stack.isEmpty() && stack.peek() >= sequence[lt]) {
                 stack.pop();
                 sb.append('-').append('\n');
                 lt++;
             }
         }
 
-        if (!stack.isEmpty()) {
+        if (stack.isEmpty()) {
+            System.out.println(sb.deleteCharAt(sb.length() - 1));
+        } else {
             System.out.println("NO");
-            return;
         }
-
-        sb.deleteCharAt(sb.length() - 1);
-        System.out.println(sb);
-
     }
 }
