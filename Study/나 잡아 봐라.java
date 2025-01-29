077cd960f7cb352e9c623013743200d430fc86eb
LeeJongsu <lik9819@naver.com>
2025-01-29 21:35:19 +0900

    Update 나 잡아 봐라.java

--- Study/나 잡아 봐라.java
+++ Study/나 잡아 봐라.java
@@ -15,36 +15,40 @@ public class Main {
         int c = 550;
         int b = 500;
         int result = BFS(c, b);
-
         System.out.println(result);
     }
 
     private static int BFS(int c, int b) {
 
-        final int INF = 200000;
+        final int INF = 200001;
+
+        int totalTime = 0;
+        int temp_c = c;
+        while (temp_c < INF) {
+            temp_c += totalTime++;
+        }
+
+        boolean[][] visited = new boolean[INF][totalTime + 1];
+        visited[b][0] = true;
 
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(b);
 
         int time = 0;
 
-        while (!queue.isEmpty()) {
-
-            if (c > INF) {
-                return time;
-            }
-
+        while (c < INF) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
 
                 int current = queue.poll();
-                if (c == current) {
+                if (current == c) {
                     return time;
                 }
 
                 int[] nextPositions = {current - 1, current + 1, current * 2};
                 for (int next : nextPositions) {
-                    if (next >= 0 && next <= INF && !queue.contains(next)) {
+                    if (next >= 0 && next < INF && !visited[next][time + 1]) {
+                        visited[next][time + 1] = true;
                         queue.offer(next);
                     }
                 }
