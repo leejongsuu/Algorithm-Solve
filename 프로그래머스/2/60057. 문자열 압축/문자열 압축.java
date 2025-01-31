83ea71da07c510c13d7359406329837b63aeb45b
LeeJongsu <lik9819@naver.com>
2025-01-31 14:54:58 +0900

    [level 2] Title: 문자열 압축, Time: 16.09 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,35 +1,28 @@
 class Solution {
     public int solution(String s) {
         int result = Integer.MAX_VALUE;
-        
         int len = s.length();
-        if(len == 1) return 1;
+        
+        if(len == 1) return 1; // 예외 처리
         
         for(int i = 1; i <= (len / 2); i++) {
             StringBuilder sb = new StringBuilder();
             int count = 1;
             String current = s.substring(0, i);
-            for(int j = i; j < len; j+= i) {
-                int tempLength = Math.min(len, j + i);
+            
+            for(int j = i; j <= len; j+=i) {
+                int tempLength = Math.min(j + i, len);
                 String next = s.substring(j, tempLength);
+                
                 if(current.equals(next)) {
                     count++;
                 } else {
-                    if(count == 1) {
-                        sb.append(current);
-                    } else {
-                        sb.append(count).append(current);
-                    }
+                    sb.append(count > 1 ? count + current : current);
                     current = next;
                     count = 1;
                 }
             }
-            if(count == 1) {
-                sb.append(current);
-            } else {
-                sb.append(count).append(current);
-            }
-            
+            sb.append(count > 1 ? count + current : current);
             result = Math.min(result, sb.length());
         }
         
