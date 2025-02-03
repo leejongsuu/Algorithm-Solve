54bc2400e416032ae3490df16c06617a6db405de
LeeJongsu <lik9819@naver.com>
2025-02-03 22:00:20 +0900

    [level 2] Title: [3차] 방금그곡, Time: 29.63 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -2,20 +2,13 @@ import java.util.*;
 
 class Solution {
     
-    class Music {
-        String title;
-        int playingTime;
-        
-        public Music(String title, int playingTime) {
-            this.title = title;
-            this.playingTime = playingTime;
-        }
-    }
-    
     public String solution(String m, String[] musicinfos) {
+        
+        String answer = "(None)";
+        int max = Integer.MIN_VALUE;
+        
         m = convertMelody(m);
-        List<Music> musicList = new ArrayList<>();
-    
+
         for(String musicinfo : musicinfos) {
             String[] infos = musicinfo.split(",");
             String startTime = infos[0];
@@ -33,17 +26,13 @@ class Solution {
             
             String entireMelody = sb.toString();
             
-            if(entireMelody.contains(m)) {
-                musicList.add(new Music(title, time));
+            if(entireMelody.contains(m) && time > max) {
+                answer = title;
+                max = time;
             }
         }
         
-        return musicList.stream()
-                .max(Comparator.comparingInt(music -> music.playingTime))
-                .map(music -> music.title)
-                .orElse("(None)");
-        
-        
+        return answer;
     }
     
     public String convertMelody(String melody) {
@@ -56,14 +45,8 @@ class Solution {
     }
     
     public int getMinutes(String startTime, String endTime) {
-        String[] start = startTime.split(":");
-        String[] end = endTime.split(":");
-        
-        int startHour = Integer.parseInt(start[0]);
-        int startMinute = Integer.parseInt(start[1]);
-        int endHour = Integer.parseInt(end[0]);
-        int endMinute = Integer.parseInt(end[1]);
-        
-        return (endHour * 60 + endMinute) - (startHour * 60 + startMinute);
+        int start = 60 * Integer.parseInt(startTime.substring(0,2)) + Integer.parseInt(startTime.substring(3));
+        int end = 60 * Integer.parseInt(endTime.substring(0,2)) + Integer.parseInt(endTime.substring(3));
+        return end - start;
     } 
 }
