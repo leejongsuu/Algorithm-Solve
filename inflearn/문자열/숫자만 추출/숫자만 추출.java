9e67b239c307cc22439221ba1bd0519a7533af74
LeeJongsu <lik9819@naver.com>
2025-02-06 16:42:14 +0900

    Update 숫자만 추출.java

--- inflearn/문자열/숫자만 추출/숫자만 추출.java
+++ inflearn/문자열/숫자만 추출/숫자만 추출.java
@@ -23,3 +23,16 @@ public class Main {
         System.out.println(T.solution(str));
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine().replaceAll("[^0-9]", "");
+        System.out.println(Integer.parseInt(str));
+    }
+}
