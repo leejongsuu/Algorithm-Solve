cfa73c1938eea2e84265f1623852921b9af3a276
LeeJongsu <lik9819@naver.com>
2025-02-06 16:13:03 +0900

    Update 문자열뒤집기.java

--- inflearn/문자열/문자열 뒤집기/문자열뒤집기.java
+++ inflearn/문자열/문자열 뒤집기/문자열뒤집기.java
@@ -37,3 +37,23 @@ public class Main {
 
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringBuilder reverse = new StringBuilder(br.readLine()).reverse();
+            sb.append(reverse).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
