32cf62235c15385cdbb9770c8817abd316bb106f
LeeJongsu <lik9819@naver.com>
2025-02-06 15:29:30 +0900

    [level 2] Title: 순위 검색, Time: 867.63 ms, Memory: 191 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -5,59 +5,62 @@ class Solution {
     Map<String, List<Integer>> infoMap = new HashMap<>();
     
     public int[] solution(String[] info, String[] query) {
-        
+
+        // 1. 조합 생성
         for(String s : info) {
             String[] parts = s.split(" ");
             int score = Integer.parseInt(parts[4]);
-            generateCombinations(0, "", parts, score);
+            generateCombinations(0, "", parts, score);   
         }
         
-        for(List<Integer> scores : infoMap.values()) {
-            Collections.sort(scores);
+        // 2. 이분 탐색 위한 정렬
+        for(List<Integer> scoreList : infoMap.values()) {
+            Collections.sort(scoreList);
         }
         
         int len = query.length;
         int[] result = new int[len];
         
+        // 3. 쿼리 탐색
         for(int i = 0; i < len; i++) {
             String[] parts = query[i].replace(" and ", "").split(" ");
             String key = parts[0];
-            int score = Integer.parseInt(parts[1]);
+            int targetScore = Integer.parseInt(parts[1]);
             
             if(infoMap.containsKey(key)) {
-                List<Integer> scores = infoMap.get(key);
-                // int index = Collections.binarySearch(scores, score);
-                // if(index < 0) index = - (index + 1);
-                // result[i] = scores.size() - index;
-                result[i] = binarySearch(key, score);
+                List<Integer> scoreList = infoMap.get(key);
+                result[i] = lowerBoundBinarySearch(targetScore, scoreList);
             }
         }
         
         return result;
     }
     
-    // 이분 탐색
-    private int binarySearch(String key, int score) {
-        List<Integer> list = infoMap.get(key);
-        int start = 0, end = list.size() - 1;
- 
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (list.get(mid) < score)
-                start = mid + 1;
-            else
-                end = mid - 1;
-        }
- 
-        return list.size() - start;
-    }
     
-    public void generateCombinations(int L , String current, String[] parts, int score) {
-        if(L == 4) {
-            infoMap.computeIfAbsent(current, k -> new ArrayList<>()).add(score);
+    public void generateCombinations(int depth, String current, String[] parts, int score) {
+        if(depth == 4) {
+            infoMap.computeIfAbsent(current, k -> new ArrayList<>()).add(score); 
             return;
         }
-        generateCombinations(L + 1 , current + parts[L], parts, score);
-        generateCombinations(L + 1 , current + "-", parts, score);
+        
+        generateCombinations(depth + 1, current + parts[depth], parts, score);
+        generateCombinations(depth + 1, current + "-", parts, score);
+    }
+    
+    public int lowerBoundBinarySearch(int target, List<Integer> scoreList) {
+
+        int lt = 0;
+        int rt = scoreList.size();
+        
+        while(lt < rt) {
+            int mid = (lt + rt) / 2;
+            if(scoreList.get(mid) < target) {
+                lt = mid +1;
+            } else {
+                rt = mid;
+            }
+        }
+        
+        return scoreList.size() - lt;
     }
 }
