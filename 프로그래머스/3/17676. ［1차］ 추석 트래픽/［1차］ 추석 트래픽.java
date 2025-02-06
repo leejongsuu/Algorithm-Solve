c940218f5088cd341f7098821eba9c39dc518961
LeeJongsu <lik9819@naver.com>
2025-02-06 14:05:23 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 47.66 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -2,66 +2,61 @@ import java.util.*;
 
 class Solution {
     
-    class Time {
-        int start;
-        int end;
+    class Log {
+        int startTime;
+        int endTime;
         
-        public Time(int start, int end) {
-            this.start = start;
-            this.end = end;
+        public Log(int startTime, int endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
         }
     }
     
     public int solution(String[] lines) {
+        
         int answer = 0;
         
-        List<Time> timeList = new ArrayList<>();
+        List<Log> logList = new ArrayList<>();
         
         for(String line : lines) {
-            String[] infos = line.split(" ");
-            int[] times = getTime(infos[1], infos[2]);
-            int startTime = times[0];
-            int endTime = times[1];
-            timeList.add(new Time(startTime, endTime));            
+            String[] parts = line.split(" ");
+            int[] timeParts = getTime(parts[1], parts[2]);
+            int startTime = timeParts[0];
+            int endTime = timeParts[1];
+            logList.add(new Log(startTime, endTime));
         }
         
-        for(Time time : timeList) {
-            answer = Math.max(answer, Math.max(getCountDuringOneSecond(time.start, timeList), getCountDuringOneSecond(time.end, timeList)));
+        for(Log log : logList) {
+            int endTime = log.endTime;
+            answer = Math.max(answer, getLogCount(endTime, logList));
         }
         
         return answer;
     }
     
-    public int[] getTime(String time, String duration) {
-        
-        final int oneThousand = 1000;
-        
-        String[] splited = time.split(":");
-        int iDuration = (int) (Double.parseDouble(duration.replace("s", "")) * oneThousand);
-        
-        int endTime = 0;
-        
-        endTime += 3600 * oneThousand * Integer.parseInt(splited[0]);
-        endTime += 60 * oneThousand * Integer.parseInt(splited[1]);
-        endTime += (int) (oneThousand * Double.parseDouble(splited[2]));
-        
-        int startTime = endTime - iDuration + 1;
-        
-        return new int[]{startTime, endTime};
-    }
-    
-    public int getCountDuringOneSecond(int time, List<Time> timeList) {
+    public int getLogCount(int startTime, List<Log> logList) {
         int count = 0;
-        
-        int startTime = time;
         int endTime = startTime + 1000;
         
-        for(Time logTime : timeList) {
-            if(logTime.start < endTime && logTime.end >= startTime) {
-                 count++;
+        for(Log log : logList) {
+            if(log.startTime < endTime && log.endTime >= startTime) {
+                count++;
             }
         }
         
         return count;
+    }
+    
+    public int[] getTime(String time, String sDuration) {
+        final int thousand = 1000;
+        int duration = (int) (thousand * Double.parseDouble(sDuration.replace("s", "")));
+        String[] timeParts = time.split(":");
+        
+        int endTime = thousand * 3600 * Integer.parseInt(timeParts[0]);
+        endTime += thousand * 60 * Integer.parseInt(timeParts[1]);
+        endTime += (int) (thousand * Double.parseDouble(timeParts[2]));
+        int startTime = endTime - duration + 1;
+        
+        return new int[]{startTime, endTime};
     }
 }
