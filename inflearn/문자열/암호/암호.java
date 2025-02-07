046577604dafb2bc3b1641f3f14c45c65bb1efb8
LeeJongsu <lik9819@naver.com>
2025-02-07 13:55:00 +0900

    Update 암호.java

--- inflearn/문자열/암호/암호.java
+++ inflearn/문자열/암호/암호.java
@@ -26,3 +26,29 @@ public class Main {
         System.out.println(T.solution(n, str));
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine().replace("#", "1").replace("*", "0");
+        
+        String[] secretes = new String[N];
+        for (int i = 0; i < N; i++) {
+            int index = i * 7;
+            secretes[i] = str.substring(index, index + 7);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append((char) Integer.parseInt(secretes[i], 2));
+        }
+
+        System.out.println(sb);
+    }
+}
