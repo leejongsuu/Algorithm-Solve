3588483bf75eb4cd5048da583a7b4c62c8134a42
LeeJongsu <lik9819@naver.com>
2025-02-07 15:10:00 +0900

    Update 점수계산.java

--- inflearn/배열/점수계산/점수계산.java
+++ inflearn/배열/점수계산/점수계산.java
@@ -33,3 +33,31 @@ public class Main {
         System.out.println(T.solution(n, arr));
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int result = 0;
+        int score = 1;
+
+        while (st.hasMoreTokens()) {
+            int answer = Integer.parseInt(st.nextToken());
+            if (answer == 1) {
+                result += score++;
+            } else {
+                score = 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
