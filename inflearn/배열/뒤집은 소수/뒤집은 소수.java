d727a4a2ccc8630b7b0f829920cd136f08b3793f
LeeJongsu <lik9819@naver.com>
2025-02-07 14:53:23 +0900

    Update 뒤집은 소수.java

--- inflearn/배열/뒤집은 소수/뒤집은 소수.java
+++ inflearn/배열/뒤집은 소수/뒤집은 소수.java
@@ -47,3 +47,45 @@ public class Main {
 
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final int INF = 100001;
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        boolean[] flag = new boolean[INF];
+        flag[0] = flag[1] = true;
+        for (int i = 2; i < INF; i++) {
+            if (!flag[i]) {
+                for (int j = 2; i * j < INF; j++) {
+                    flag[i * j] = true;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int num = reverse(st.nextToken());
+            if (!flag[num]) {
+                sb.append(num).append(' ');
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static int reverse(String sNum) {
+        return Integer.parseInt(new StringBuffer(sNum).reverse().toString());
+    }
+}
+
