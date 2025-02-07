d82f022252726306fabca7b43e4841182da95e13
LeeJongsu <lik9819@naver.com>
2025-02-07 14:28:55 +0900

    Update 피보나치 수열.java

--- inflearn/배열/피보나치 수열/피보나치 수열.java
+++ inflearn/배열/피보나치 수열/피보나치 수열.java
@@ -24,3 +24,25 @@ public class Main {
         }
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N];
+
+        StringBuilder sb = new StringBuilder("1 1 ");
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i < N; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+            sb.append(dp[i]).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
