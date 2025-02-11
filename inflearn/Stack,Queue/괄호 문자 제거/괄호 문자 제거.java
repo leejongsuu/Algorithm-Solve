68a54ca1b97993b4f38bce5f1b5189c862ac51b9
LeeJongsu <lik9819@naver.com>
2025-02-11 18:14:25 +0900

    Update 괄호 문자 제거.java

--- inflearn/Stack,Queue/괄호 문자 제거/괄호 문자 제거.java
+++ inflearn/Stack,Queue/괄호 문자 제거/괄호 문자 제거.java
@@ -63,3 +63,31 @@ class Main {
 		System.out.println(T.solution(str));
 	}
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                stack.pop();
+            } else if (stack.isEmpty()) {
+                sb.append(c);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
