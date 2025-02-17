6ad9de7c30f521b5f601a4a992fa2a91c0acbe3d
LeeJongsu <lik9819@naver.com>
2025-02-17 13:30:41 +0900

    Update 교육과정 설계.java

--- inflearn/Stack,Queue/교육과정 설계/교육과정 설계.java
+++ inflearn/Stack,Queue/교육과정 설계/교육과정 설계.java
@@ -38,3 +38,30 @@ public class Main {
         System.out.println(T.solution(require, str));
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String required = br.readLine();
+        String plan = br.readLine();
+
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : required.toCharArray()) queue.offer(c);
+
+        for (char c : plan.toCharArray()) {
+            if (queue.contains(c) && queue.poll() != c) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println(queue.isEmpty() ? "YES" : "NO");
+    }
+}
+
