d04ee850d71b0e48ea5aec8e579b7062d0a978c9
LeeJongsu <lik9819@naver.com>
2025-02-17 13:54:40 +0900

    Update 선택 정렬.java

--- inflearn/정렬,이분검색과 결정 알고리즘/선택 정렬/선택 정렬.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/선택 정렬/선택 정렬.java
@@ -36,3 +36,95 @@ public class Main {
         for(int x : T.solution(n, arr)) System.out.print(x+" ");
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : selectSort(N, arr)) {
+            sb.append(i).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+
+    static int[] swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        return arr;
+    }
+
+    static int[] selectSort(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            int idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[idx]) {
+                    idx = j;
+                }
+            }
+            swap(arr, i, idx);
+        }
+        return arr;
+    }
+}
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : selectSort(N, arr)) {
+            sb.append(i).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    static int[] selectSort(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            int idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[idx]) {
+                    idx = j;
+                }
+            }
+            swap(arr, i, idx);
+        }
+        return arr;
+    }
+}
