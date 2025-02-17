bff04860a5e61340aaa150f8d00f263fb7ea4ac6
LeeJongsu <lik9819@naver.com>
2025-02-17 14:13:35 +0900

    Update 삽입 정렬.java

--- inflearn/정렬,이분검색과 결정 알고리즘/삽입 정렬/삽입 정렬.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/삽입 정렬/삽입 정렬.java
@@ -36,3 +36,41 @@ public class Main {
         for(int x : T.solution(n, arr)) System.out.print(x+" ");
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : insertSort(N, arr)) {
+            sb.append(i).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+
+    static int[] insertSort(int n, int[] arr) {
+        for (int i = 1; i < n; i++) {
+            int tmp = arr[i], j;
+            for (j = i - 1; j >= 0; j--) {
+                if (tmp < arr[j]) arr[j + 1] = arr[j];
+                else break;
+                arr[j + 1] = tmp;
+            }
+        }
+        return arr;
+    }
+}
