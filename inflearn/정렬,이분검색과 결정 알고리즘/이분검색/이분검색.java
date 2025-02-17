f4220e4b011c588ed91430aabe6e77a8750a04f5
LeeJongsu <lik9819@naver.com>
2025-02-17 17:05:27 +0900

    Update 이분검색.java

--- inflearn/정렬,이분검색과 결정 알고리즘/이분검색/이분검색.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/이분검색/이분검색.java
@@ -40,3 +40,30 @@ public class Main {
 
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int index = Arrays.binarySearch(arr, M);
+        System.out.println(index + 1);
+    }
+}
