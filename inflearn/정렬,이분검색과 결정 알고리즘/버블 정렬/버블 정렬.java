16f7c730744110b8bbd4acc201a2a8340885da69
LeeJongsu <lik9819@naver.com>
2025-02-17 14:16:53 +0900

    Update 버블 정렬.java

--- inflearn/정렬,이분검색과 결정 알고리즘/버블 정렬/버블 정렬.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/버블 정렬/버블 정렬.java
@@ -37,3 +37,47 @@ public class Main {
         for(int x : T.solution(n, arr)) System.out.print(x+" ");
     }
 }
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : bubbleSort(N, arr)) {
+            sb.append(i).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+
+    static int[] bubbleSort(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+        return arr;
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
