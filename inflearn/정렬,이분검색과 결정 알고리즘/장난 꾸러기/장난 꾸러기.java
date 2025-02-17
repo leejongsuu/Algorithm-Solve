3dab3cdafdfa2efe2fe7e75a15dd5c2369b9b895
LeeJongsu <lik9819@naver.com>
2025-02-17 15:53:34 +0900

    Update 장난 꾸러기.java

--- inflearn/정렬,이분검색과 결정 알고리즘/장난 꾸러기/장난 꾸러기.java
+++ inflearn/정렬,이분검색과 결정 알고리즘/장난 꾸러기/장난 꾸러기.java
@@ -30,3 +30,36 @@ public class Main {
         T.solution(n,arr);
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+        int[] sorted = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sorted[i] = arr[i];
+        }
+
+        Arrays.sort(sorted);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            if (arr[i] != sorted[i]) {
+                sb.append(i + 1).append(' ');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
