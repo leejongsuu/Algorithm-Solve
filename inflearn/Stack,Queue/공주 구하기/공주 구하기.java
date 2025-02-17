fb1401f7df127bb96a8018ccc1a412d9f44d21dd
LeeJongsu <lik9819@naver.com>
2025-02-17 13:22:48 +0900

    Update 공주 구하기.java

--- inflearn/Stack,Queue/공주 구하기/공주 구하기.java
+++ inflearn/Stack,Queue/공주 구하기/공주 구하기.java
@@ -60,4 +60,30 @@ public int solution(int n, int m) {
 	}
 }
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) queue.offer(i);
+
+        while (queue.size() > 1) {
+            for (int i = 0; i < K - 1; i++) queue.offer(queue.poll());
+            queue.poll();
+        }
+
+        System.out.println(queue.poll());
+    }
+}
 
