3c9a12095299ca906a764364c007a7b528aa61c0
LeeJongsu <lik9819@naver.com>
2025-02-19 14:04:44 +0900

    Update 수열 추측하기.java

--- inflearn/DFS/수열 추측하기/수열 추측하기.java
+++ inflearn/DFS/수열 추측하기/수열 추측하기.java
@@ -54,3 +54,65 @@ public class Main {
         T.DFS(0, 0);
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, F;
+    static int[] combination;
+    static int[][] dp;
+    static boolean[] visited;
+    static boolean check = false;
+    static String answer = "";
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        F = Integer.parseInt(st.nextToken());
+
+        combination = new int[N];
+        visited = new boolean[N + 1];
+        dp = new int[N][N];
+        for (int r = 0; r < N; r++) {
+            combination[r] = makeCombination(N - 1, r);
+        }
+
+        DFS(0, 0, new int[N]);
+
+        System.out.println(answer);
+    }
+
+    private static int makeCombination(int n, int r) {
+        if (n == r || r == 0) return 1;
+        if (dp[n][r] > 0) return dp[n][r];
+        return dp[n][r] = makeCombination(n - 1, r - 1) + makeCombination((n - 1), r);
+    }
+
+    private static void DFS(int L, int sum, int[] arr) {
+        if (sum > F || check) return;
+
+        if (L == N) {
+            if (sum == F) {
+                for (int x : arr) {
+                    answer += x + " ";
+                }
+                check = true;
+            }
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[L] = i;
+                DFS(L + 1, sum + (i * combination[L]), arr);
+                visited[i] = false;
+            }
+        }
+    }
+}
