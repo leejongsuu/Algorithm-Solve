466450bc7216b3a4156816e97f9278b00f57f4cd
LeeJongsu <lik9819@naver.com>
2025-02-19 21:04:57 +0900

    Update 섬나라 아일랜드.java

--- inflearn/DFS/섬나라 아일랜드/섬나라 아일랜드.java
+++ inflearn/DFS/섬나라 아일랜드/섬나라 아일랜드.java
@@ -66,3 +66,59 @@ public class Main {
 
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] dr = {1, 0, -1, 0, 1, 1, -1, -1};
+    static int[] dc = {0, 1, 0, -1, 1, -1, 1, -1};
+    static int[][] board;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 1) {
+                    answer++;
+                    DFS(i, j);
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void DFS(int r, int c) {
+
+        board[r][c] = 0;
+
+        for (int i = 0; i < 8; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (isIn(nr, nc) && board[nr][nc] == 1) {
+                DFS(nr, nc);
+            }
+        }
+    }
+
+    private static boolean isIn(int r, int c) {
+        return r >= 0 && c >= 0 && r < N && c < N;
+    }
+}
