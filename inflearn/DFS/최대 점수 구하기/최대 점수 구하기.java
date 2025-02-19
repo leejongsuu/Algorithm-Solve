0cf010ca42870fb2aae32aea74a565ff2d40aaf1
LeeJongsu <lik9819@naver.com>
2025-02-19 12:51:03 +0900

    Update 최대 점수 구하기.java

--- inflearn/DFS/최대 점수 구하기/최대 점수 구하기.java
+++ inflearn/DFS/최대 점수 구하기/최대 점수 구하기.java
@@ -43,3 +43,50 @@ public class Main {
         System.out.println(max);
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M, max = Integer.MIN_VALUE;
+
+    static class Question {
+        int score, time;
+
+        public Question(int score, int time) {
+            this.score = score;
+            this.time = time;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        Question[] questions = new Question[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            questions[i] = new Question(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        DFS(0, 0, 0, questions);
+        System.out.println(max);
+    }
+
+    public static void DFS(int L, int score, int time, Question[] questions) {
+        if (time > M) return;
+
+        if (L == N) {
+            max = Math.max(max, score);
+            return;
+        }
+        
+        DFS(L + 1, score + questions[L].score, time + questions[L].time, questions);
+        DFS(L + 1, score, time, questions);
+    }
+}
