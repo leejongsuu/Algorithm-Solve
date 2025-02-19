a948c2af4dabe488cb0cb2c69ed185307bb59afe
LeeJongsu <lik9819@naver.com>
2025-02-19 22:10:59 +0900

    Update 결혼식.java

--- inflearn/Greedy/결혼식/결혼식.java
+++ inflearn/Greedy/결혼식/결혼식.java
@@ -69,3 +69,53 @@ public class Main {
         System.out.println(T.solution(list));
     }
 }
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Time implements Comparable<Time> {
+        char k;
+        int time;
+
+        public Time(char k, int time) {
+            this.k = k;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Time o) {
+            if (this.time == o.time) return this.k - o.k;
+            return this.time - o.time;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Time> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            list.add(new Time('s', Integer.parseInt(st.nextToken())));
+            list.add(new Time('e', Integer.parseInt(st.nextToken())));
+        }
+
+        Collections.sort(list);
+
+        int result = 0, count = 0;
+        for (Time time : list) {
+            if (time.k == 's') {
+                count++;
+                result = Math.max(result, count);
+            } else count--;
+        }
+
+        System.out.println(result);
+    }
+}
