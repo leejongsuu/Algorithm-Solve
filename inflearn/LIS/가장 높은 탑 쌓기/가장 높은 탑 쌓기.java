cc9839438af9398234708bcf6a8a1ca00b9f6d17
LeeJongsu <lik9819@naver.com>
2025-02-20 14:01:47 +0900

    Update 가장 높은 탑 쌓기.java

--- inflearn/LIS/가장 높은 탑 쌓기/가장 높은 탑 쌓기.java
+++ inflearn/LIS/가장 높은 탑 쌓기/가장 높은 탑 쌓기.java
@@ -68,3 +68,56 @@ public class Main {
         System.out.println(T.solution());
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Brick implements Comparable<Brick> {
+        int extent, height, weight;
+
+        public Brick(int extent, int height, int weight) {
+            this.extent = extent;
+            this.height = height;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Brick o) {
+            return o.extent - this.extent;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Brick[] bricks = new Brick[N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            bricks[i] = new Brick(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        Arrays.sort(bricks);
+
+        int maxHeight = 0;
+
+        int[] dp = new int[N];
+        dp[0] = bricks[0].height;
+
+        for (int i = 1; i < N; i++) {
+            int max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (bricks[j].extent > bricks[i].extent && bricks[j].weight > bricks[i].weight && dp[j] > max) {
+                    max = dp[j];
+                }
+            }
+            dp[i] = max + bricks[i].height;
+            maxHeight = Math.max(maxHeight, dp[i]);
+        }
+
+        System.out.println(maxHeight);
+    }
+}
