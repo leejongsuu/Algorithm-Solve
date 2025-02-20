99dc455a021e6935d806b9e1036d3601ad32ac95
LeeJongsu <lik9819@naver.com>
2025-02-20 12:29:05 +0900

    Update 계단 오르기.java

--- inflearn/DP/계단 오르기/계단 오르기.java
+++ inflearn/DP/계단 오르기/계단 오르기.java
@@ -28,3 +28,24 @@ public class Main {
         System.out.println(T.solution(n));
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+
+        for (int i = 0; i < 3; i++) dp[i] = i;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
