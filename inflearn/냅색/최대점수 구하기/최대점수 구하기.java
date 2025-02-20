307771081e00cc2f76126c52dbea007d1641a09b
LeeJongsu <lik9819@naver.com>
2025-02-20 14:32:01 +0900

    Update 최대점수 구하기.java

--- inflearn/냅색/최대점수 구하기/최대점수 구하기.java
+++ inflearn/냅색/최대점수 구하기/최대점수 구하기.java
@@ -48,3 +48,53 @@ public class Main {
         System.out.println(T.solution());
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Question implements Comparable<Question> {
+        int score, time;
+
+        public Question(int score, int time) {
+            this.score = score;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Question o) {
+            return this.time - o.time;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Question[] questions = new Question[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int score = Integer.parseInt(st.nextToken());
+            int time = Integer.parseInt(st.nextToken());
+            questions[i] = new Question(score, time);
+        }
+
+        Arrays.sort(questions);
+
+        int[] dp = new int[M + 1];
+        for (int i = 0; i < N; i++) {
+            int time = questions[i].time;
+            for (int j = M; j >= time; j--) {
+                dp[j] = Math.max(dp[j], dp[j - time] + questions[i].score);
+            }
+        }
+
+        System.out.println(dp[M]);
+    }
+}
