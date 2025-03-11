5e3f3c54bf7bb4b0479955d62d6851684d838645
LeeJongsu <lik9819@naver.com>
2025-03-11 15:12:49 +0900

    [Gold V] Title: 탑, Time: 716 ms, Memory: 86976 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,57 +1,58 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] topHeights = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            topHeights[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] result = solution(N, topHeights);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i : result) {
+            sb.append(i).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+
     static class Top {
-        int num;
+        int index;
         int height;
 
-        public Top(int num, int height) {
-            this.num = num;
+        public Top(int index, int height) {
+            this.index = index;
             this.height = height;
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    public static int[] solution(int n, int[] topHeights) {
+        int[] arr = new int[n];
 
         Stack<Top> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
+        stack.push(new Top(1, topHeights[0]));
 
-        for (int i = 1; i <= N; i++) {
-            int height = Integer.parseInt(st.nextToken());
-
-            if (stack.isEmpty()) {
-                sb.append(0).append(' ');
-                stack.push(new Top(i, height));
-            } else {
-                while (true) {
-                    if (stack.isEmpty()) {
-                        sb.append(0).append(' ');
-                        stack.push(new Top(i, height));
-                        break;
-                    }
-
-                    Top top = stack.peek();
-
-                    if (top.height < height) {
-                        stack.pop();
-                    } else {
-                        sb.append(top.num).append(' ');
-                        stack.push(new Top(i, height));
-                        break;
-                    }
-                }
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && stack.peek().height < topHeights[i]) {
+                stack.pop();
             }
+            if (!stack.isEmpty()) {
+                arr[i] = stack.peek().index;
+            }
+            stack.push(new Top(i + 1, topHeights[i]));
         }
 
-        System.out.println(sb);
+        return arr;
     }
 }
