1343e052e08c08d737ce4885f0a4801057f257a7
LeeJongsu <lik9819@naver.com>
2025-03-11 14:48:05 +0900

    Update 뒤집기.java

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,34 +1,32 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
+        char[] ch = br.readLine().toCharArray();
 
-        int zeroToOneCount = 0;
         int oneToZeroCount = 0;
+        int zeroToOneCount = 0;
 
-        if (input.charAt(0) == '0') {
+        if (ch[0] == '0') {
             zeroToOneCount++;
         } else {
             oneToZeroCount++;
         }
 
-        for (int i = 0; i < input.length() - 1; i++) {
-            if (input.charAt(i) != input.charAt(i + 1)) {
-                if (input.charAt(i + 1) == '1') {
-                    oneToZeroCount++;
-                } else {
+        for (int i = 1; i < ch.length; i++) {
+            if (ch[i] != ch[i - 1]) {
+                if (ch[i] == '0') {
                     zeroToOneCount++;
+                } else {
+                    oneToZeroCount++;
                 }
             }
         }
 
-        System.out.println(Math.min(oneToZeroCount, zeroToOneCount));
+        System.out.println(Math.min(zeroToOneCount, oneToZeroCount));
     }
 }
