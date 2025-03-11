c5f329593cab44f0e5678160092770aa053185ef
LeeJongsu <lik9819@naver.com>
2025-03-11 14:27:22 +0900

    Update 최대 부분 증가수열.java

--- inflearn/LIS/최대 부분 증가수열/최대 부분 증가수열.java
+++ inflearn/LIS/최대 부분 증가수열/최대 부분 증가수열.java
@@ -88,3 +88,36 @@ public class Main {
         System.out.println(T.solution(arr));
     }
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        int[] dp = new int[N];
+        for (int i = 1; i < N; i++) {
+            int max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i] && dp[j] > max) max = dp[j];
+            }
+            dp[i] = max + 1;
+            result = Math.max(result, dp[i]);
+        }
+
+        System.out.println(result);
+    }
+}
