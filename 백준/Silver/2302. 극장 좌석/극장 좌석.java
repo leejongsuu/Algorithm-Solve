40449b1aff261674c896659648bc8613bd2f0132
LeeJongsu <lik9819@naver.com>
2025-03-12 23:05:33 +0900

    Update 극장 좌석.java

--- 백준/Silver/2302. 극장 좌석/극장 좌석.java
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.java
@@ -53,3 +53,40 @@ public class Main {
         return dp[n] = fibo(n - 1) + fibo(n - 2);
     }
 }
+
+
+---
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[] vip = new int[M];
+        for (int i = 0; i < M; i++) {
+            vip[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[N + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        int result = 1;
+        int start = 0;
+        for (int i = 0; i < M; i++) {
+            int len = vip[i] - start - 1;
+            result *= dp[len];
+            start = vip[i];
+        }
+        result *= dp[N - start];
+
+        System.out.println(result);
+    }
+}
