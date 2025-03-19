7a70747a760cdd27ae0846ee2f339303c184b242
LeeJongsu <lik9819@naver.com>
2025-03-19 22:44:30 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 4.32 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -3,60 +3,63 @@ import java.util.*;
 class Solution {
     
     long answer = 0;
-    char[] operator = new char[]{'+', '-', '*'};
+    char[] operators = {'+', '*', '-'};
+    boolean[] visited = new boolean[3];
     List<String> expressionList = new ArrayList<>();
-
-    final int INF = 3;
     
     public long solution(String expression) {
         
-        StringTokenizer st = new StringTokenizer(expression, "[-][+][*]", true);
+        StringTokenizer st = new StringTokenizer(expression, "*+-", true);
         while(st.hasMoreTokens()) {
             expressionList.add(st.nextToken());
         }
         
-        generateCombinations(0, new char[3], new boolean[3]);
+        makePriorityOperator(0, new char[3]);
         
         return answer;
     }
     
-    public void generateCombinations(int L, char[] combination, boolean[] visited) {
-        if(L == INF) {
-            List<String> tempList = new ArrayList<>(expressionList);
-            
-            for(int i = 0; i < L; i++) {
-                int index = tempList.indexOf(String.valueOf(combination[i]));
-                while(index > -1) {
-                    long rightOperand = Long.parseLong(tempList.get(index + 1));
-                    long leftOperand = Long.parseLong(tempList.get(index - 1));
-                    long result = calculate(leftOperand, rightOperand, combination[i]);
-                    
-                    tempList.subList(index - 1, index + 2).clear();
-                    tempList.add(index - 1, String.valueOf(result));
-                    index = tempList.indexOf(String.valueOf(combination[i]));
-                }
-            }
-            
-            answer = Math.max(answer, Math.abs(Long.parseLong(tempList.get(0))));
+    public void makePriorityOperator(int L, char[] prOperators) {
+        if(L == 3) {
+            answer = Math.max(answer, calculatePrize(prOperators, new ArrayList<>(expressionList)));
             return;
         }
         
-        for(int i = 0; i < INF; i++) {
+        for(int i = 0; i < 3; i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                combination[L] = operator[i];
-                generateCombinations(L + 1, combination, visited);
+                prOperators[L] = operators[i];
+                makePriorityOperator(L + 1, prOperators);
                 visited[i] = false;
             }
         }
     }
     
-    public long calculate(long a, long b, char c) {
-        return switch(c) {
-                case '+' -> a + b;
-                case '-' -> a - b;
-                case '*' -> a * b;
-                default -> 0;
-        };
+    public long calculatePrize(char[] prOperators, List<String> list) {
+        for(int i = 0; i < 3; i++) {
+            while(true) {
+                String operator = String.valueOf(prOperators[i]);
+                int index = list.indexOf(operator);
+                
+                if(index < 0) break;
+                else {
+                    long a = Long.parseLong(list.get(index - 1));
+                    long b = Long.parseLong(list.get(index + 1));
+                    String result = String.valueOf(calculateOperator(a, b, prOperators[i]));
+                    list.add(index - 1, result);
+                    list.subList(index, index + 3).clear();
+                }
+            }
+        }
+        return Math.abs(Long.parseLong(list.get(0)));
     }
+    
+    public long calculateOperator(long a, long b, char c) {
+        return switch(c) {
+            case '*' -> a * b;
+            case '+' -> a + b;
+            case '-' -> a - b;
+            default -> -1;
+        };
+    } 
 }
