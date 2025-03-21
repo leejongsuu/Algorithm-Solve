406d05038a3e55951a7128f562549d02227b02e7
LeeJongsu <lik9819@naver.com>
2025-03-21 16:48:06 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.10 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,20 +1,19 @@
 class Solution {
     public String solution(String s) {
         
-        char[] ch = s.toLowerCase().toCharArray();
-        
-        int dis = 'a' - 'A';
-        
+        char[] ch = s.toCharArray();
         for(int i = 0, j = 0; i < s.length(); i++, j++) {
             if(ch[i] == ' ') {
                 j = -1;
                 continue;
             }
+             
             if(j % 2 == 0) {
-                ch[i] -= dis;
+                ch[i] = Character.toUpperCase(ch[i]);
+            } else {
+                ch[i] = Character.toLowerCase(ch[i]);
             }
         }
-        
         return String.valueOf(ch);
     }
 }
