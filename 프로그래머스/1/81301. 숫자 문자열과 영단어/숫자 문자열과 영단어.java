d0982ffc51034a706aa5e8b6d1078d5cfd8915df
LeeJongsu <lik9819@naver.com>
2025-03-21 16:59:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.24 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,12 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-
+        
+        StringBuffer sb = new StringBuffer();
+        
         String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        for(int i = 0; i < 10; i++) {
-            s = s.replaceAll(words[i], String.valueOf(i));
+        Map<String, Integer> digitMap = new HashMap<>();
+        for(int i = 0; i < words.length; i++) {
+            digitMap.put(words[i], i);
         }
         
-        return Integer.parseInt(s);
+        for(int lt = 0, rt = 1; rt <= s.length(); rt++) {
+            String key = s.substring(lt, rt);
+            if(Character.isDigit(key.charAt(0))) {
+                sb.append(key);
+                lt++;
+            } else if(digitMap.containsKey(key)) {
+                sb.append(digitMap.get(key));
+                lt = rt;
+            }
+        }
+        
+        return Integer.parseInt(sb.toString());
     }
 }
