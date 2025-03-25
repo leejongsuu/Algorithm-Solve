afc4b9c4a3c18401708f4747adef75397a83cbb8
LeeJongsu <lik9819@naver.com>
2025-03-25 15:16:21 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 39.92 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,24 +1,21 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
+    
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+
+        Map<String, Integer> dataMap = new HashMap<>();
+        dataMap.put("code", 0);
+        dataMap.put("date", 1);
+        dataMap.put("maximum", 2);
+        dataMap.put("remain", 3);
         
-        HashMap<String, Integer> indexMap = new HashMap<>();
-        indexMap.put("code", 0);
-        indexMap.put("date", 1);
-        indexMap.put("maximum", 2);
-        indexMap.put("remain", 3);
-        
-        int extIndex = indexMap.get(ext);
-        int sortIndex = indexMap.get(sort_by);
+        int extIndex = dataMap.get(ext);
+        int sortByIndex = dataMap.get(sort_by);
         
         return Arrays.stream(data)
-                    .filter(row -> row[extIndex] < val_ext)
-                    .sorted(Comparator.comparingInt(row -> row[sortIndex]))
-                    .toArray(int[][]::new);
-        
-        
+            .filter(row -> row[extIndex] < val_ext)
+            .sorted(Comparator.comparingInt(row -> row[sortByIndex]))
+            .toArray(int[][]::new);
     }
 }
