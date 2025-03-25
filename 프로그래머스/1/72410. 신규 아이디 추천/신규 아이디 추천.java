a780e3edf433fe33c261b6c7b7ca94c241902f6d
LeeJongsu <lik9819@naver.com>
2025-03-25 15:38:23 +0900

    [level 1] Title: 신규 아이디 추천, Time: 6.74 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,70 +1,67 @@
 class Solution {
-    public String solution(String new_id) {
-
-        String s = new KAKAOID(new_id)
-                .replaceToLowerCase()
-                .filter()
-                .toSingleDot()
-                .noStartEndDot()
-                .noBlank()
-                .noGreaterThan16()
-                .noLessThan2()
-                .getResult();
-
-        return s;
+    
+    public class ID {
+        String id;
+        
+        public ID(String id) {
+            this.id = id;
+        }
+        
+        public String getId() {
+            return this.id;
+        }
+        
+        public ID toLowerCase() {
+            id = id.toLowerCase();
+            return this;
+        }
+        
+        public ID toFiltering() {
+            id = id.replaceAll("[^a-z0-9-_.]", "");
+            return this;
+        }
+        
+        public ID replaceOneDot() {
+            id = id.replaceAll("[.]{2,}",".");
+            return this;
+        }
+        
+        public ID removeFirstOrEndDot() {
+            id = id.replaceAll("^[.]|[.]$","");
+            return this;
+        }
+        
+        public ID noBlank() {
+            if(id.isEmpty()) {
+                id += "a";
+            }
+            return this;
+        }
+        
+        public ID noGreaterThan16() {
+            if(id.length() >= 16) {
+                id = id.substring(0, 15);
+            }
+            id = id.replaceAll("[.]$", "");
+            return this;
+        }
+        
+        public ID addWords() {
+            while(id.length() <= 2) {
+                id += String.valueOf(id.charAt(id.length() - 1));
+            }
+            return this;
+        }
     }
-
-    private static class KAKAOID {
-        private String s;
-
-        KAKAOID(String s) {
-            this.s = s;
-        }
-
-        private KAKAOID replaceToLowerCase() {
-            s = s.toLowerCase();
-            return this;
-        }
-
-        private KAKAOID filter() {
-            s = s.replaceAll("[^a-z0-9._-]", "");
-            return this;
-        }
-
-        private KAKAOID toSingleDot() {
-            s = s.replaceAll("[.]{2,}", ".");
-            return this;
-        }
-
-        private KAKAOID noStartEndDot() {
-            s = s.replaceAll("^[.]|[.]$", "");
-            return this;
-        }
-
-        private KAKAOID noBlank() {
-            s = s.isEmpty() ? "a" : s;
-            return this;
-        }
-
-        private KAKAOID noGreaterThan16() {
-            if (s.length() >= 16) {
-                s = s.substring(0, 15);
-            }
-            s = s.replaceAll("[.]$", "");
-            return this;
-        }
-
-        private KAKAOID noLessThan2() {
-            StringBuilder sBuilder = new StringBuilder(s);
-            while (sBuilder.length() <= 2) {
-                sBuilder.append(sBuilder.charAt(sBuilder.length() - 1));
-            }
-            s = sBuilder.toString();
-            return this;
-        }
-
-        private String getResult() {
-            return s;
-        }
+    public String solution(String new_id) {
+        return new ID(new_id)
+            .toLowerCase()
+            .toFiltering()
+            .replaceOneDot()
+            .removeFirstOrEndDot()
+            .noBlank()
+            .noGreaterThan16()
+            .addWords()
+            .getId();
     }
 }
