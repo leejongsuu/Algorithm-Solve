3a01841238caf4c4b0836318406d785c41f0a5f6
LeeJongsu <lik9819@naver.com>
2025-03-25 11:52:34 +0900

    [level 1] Title: 소수 찾기, Time: 33.66 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,19 +1,26 @@
 class Solution {
+    boolean[] isNotPrime;
+    
     public int solution(int n) {
         
         int result = 0;
-        
-        boolean[] isNotPrime = new boolean[n+1];
+        boolean[] isNotPrimes = getIsNotPrimes(n);
         for(int i = 2; i <= n; i++) {
-            if(!isNotPrime[i]) {
-                result++;
-                for(int j = 2; i * j <= n; j++) {
-                    isNotPrime[i * j] = true;
-                }
-            }
+            if(!isNotPrimes[i]) result++;
         }
         
         return result;
-        
+    }
+    
+    public boolean[] getIsNotPrimes(int num) {
+        boolean[] isNotPrimes = new boolean[num + 1];
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(!isNotPrimes[i]) {
+                for(int j = 2; i * j <= num; j++) {
+                    isNotPrimes[i * j] = true;
+                }
+            }
+        }
+        return isNotPrimes;
     }
 }
