740243abbaa9dc37886e0f16366bb23676aefc73
LeeJongsu <lik9819@naver.com>
2025-03-26 10:11:18 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.24 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,22 +1,24 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
+        int lur = Integer.MAX_VALUE;
+        int luc = Integer.MAX_VALUE;
+        int rdr = Integer.MIN_VALUE;
+        int rdc = Integer.MIN_VALUE;
+        
         int n = wallpaper.length;
         int m = wallpaper[0].length();
-        
-        int luy = n, lux = m;
-        int rdy = 0, rdx = 0;
         
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(wallpaper[i].charAt(j) == '#') {
-                    if(i < luy) luy = i;
-                    if(j < lux) lux = j;
-                    if(i + 1 > rdy) rdy = i + 1;
-                    if(j + 1 > rdx) rdx = j + 1;
+                    lur = Math.min(lur, i);
+                    luc = Math.min(luc, j);
+                    rdr = Math.max(rdr, i + 1);
+                    rdc = Math.max(rdc, j + 1);
                 }
             }
         }
         
-        return new int[]{luy, lux, rdy, rdx};
+        return new int[]{lur, luc, rdr, rdc};
     }
 }
