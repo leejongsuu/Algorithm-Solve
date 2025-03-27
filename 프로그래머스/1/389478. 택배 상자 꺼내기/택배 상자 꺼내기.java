5227a7f2968f278fec46911102bbd8285a134a8b
LeeJongsu <lik9819@naver.com>
2025-03-27 10:47:35 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,37 +1,29 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        // 전체 층 수
         int totalRows = (n - 1) / w + 1;
-        // num의 위치
         int row = (num - 1) / w;
-        int col;
 
-        // num이 위치한 열 (지그재그 고려)
-        if (row % 2 == 0) {
-            // 왼 → 오
+        int col;
+        if(row % 2 == 0) {
             col = (num - 1) % w;
         } else {
-            // 오 → 왼
             col = w - 1 - (num - 1) % w;
         }
-
-        int count = 1; // 자신 포함
-
-        // 위층만 조사
-        for (int r = row + 1; r < totalRows; r++) {
-            int boxIdx;
-            if (r % 2 == 0) {
-                // 왼 → 오
-                boxIdx = r * w + col;
+        
+        int result = 1;
+        for(int r = row + 1; r < totalRows; r++) {
+            int boxId;
+            if(r % 2 == 0) {
+                boxId = r * w + col;
             } else {
-                // 오 → 왼
-                boxIdx = (r + 1) * w - 1 - col;
+                boxId = (r + 1) * w - col - 1;
             }
-
-            if (boxIdx >= n) break;
-            count++;
+            if(boxId >= n) {
+                break;
+            }
+            result++;
         }
-
-        return count;
+        
+        return result;
     }
 }
