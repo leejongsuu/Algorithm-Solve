9e750b6ebd4a4c0418db9908103e346a1056c2ba
LeeJongsu <lik9819@naver.com>
2025-03-27 11:05:52 +0900

    [level 2] Title: 구명보트, Time: 14.42 ms, Memory: 57.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,19 +1,37 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-
+        
         Arrays.sort(people);
         
-        int boat = 0;
-        
-        for(int lt = 0, rt = people.length - 1; lt <= rt; rt--) {
-            if(people[lt] + people[rt] <= limit) {
-                lt++;
+        int result = 1;
+        int sum = 0;
+        int count = 0;
+            
+        int lt = 0;
+        int rt = people.length - 1;
+        while(lt <= rt) {
+            if(count + 1 <= 2) {
+                count++;
+                if(sum + people[rt] <= limit) {
+                    sum += people[rt];
+                    rt--;
+                } else if(sum + people[lt] <= limit) {
+                    sum += people[lt];
+                    lt++;
+                } else {
+                    result++;
+                    sum = 0;
+                    count = 0;
+                }
+            } else {
+                result++;
+                sum = 0;
+                count =0;
             }
-            boat++;
         }
         
-        return boat;
+        return result;
     }
 }
