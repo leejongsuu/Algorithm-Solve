ab6fd9accfe7a39a53635dc0cd37a5798b3453bf
LeeJongsu <lik9819@naver.com>
2025-03-27 12:15:28 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,15 +2,16 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
+        // 1 2 3 4 5 6 7 8
+        
         int answer = 0;
-        a-=1;
-        b-=1;
-        while(a!=b) {
-            a /= 2;
-            b /= 2;
+        while(true) {
             answer++;
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            if(a == b) {
+                return answer;
+            }
         }
-            
-        return answer;
     }
 }
