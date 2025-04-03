b08b90eded174f9b06a68f4ddf7c317ff57eff8d
LeeJongsu <lik9819@naver.com>
2025-04-03 21:14:55 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.18 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -3,59 +3,45 @@ import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
         
-        final int INF = 65536;
+        final int MULTI_VALUE = 65536;
+        
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        
+        Map<String, Integer> map1 = getTwoWordsMap(str1);
+        Map<String, Integer> map2 = getTwoWordsMap(str2);
+        
+        int intersection = 0;
         int sum = 0;
-        int interSection = 0;
         
-        Map<String, Integer> map1 = solution(str1);
-        Map<String, Integer> map2 = solution(str2);
-        
-        for(String key : map1.keySet()) {
-            int value1 = map1.get(key);
-            int i = 0;
-            int s = value1;
-            if(map2.containsKey(key)) {
-                int value2 = map2.get(key);
-                i = Math.min(value1, value2);
-                s = Math.max(value1, value2);
-                map2.remove(key);
+        for(String word : map1.keySet()) {
+            int num1 = map1.get(word);
+            if(map2.containsKey(word)) {
+                int num2 = map2.get(word);
+                intersection += Math.min(num1, num2);
+                sum += Math.max(num1, num2);
+                map2.remove(word);
+            } else {
+                sum += num1;
             }
-            
-            interSection += i;
-            sum += s;
         }
         
-        for(String key : map2.keySet()) {
-            sum += map2.get(key);
+        for(String word : map2.keySet()) {
+            sum += map2.get(word);
         }
         
-        if(sum == 0) {
-            interSection = 1;
-            sum = 1;
-        }
-        
-        double similarity = (double) interSection / sum;
-        return (int) (similarity * INF);
+        if(sum == 0) return MULTI_VALUE;
+        else return (int) (((double) intersection / sum) * MULTI_VALUE); 
     }
     
-    private Map<String, Integer> solution(String str) {
-        
+    public Map<String, Integer> getTwoWordsMap(String str) {
         Map<String, Integer> map = new HashMap<>();
-        
-        str = str.toLowerCase();
-        int len = str.length();
-        
-        for(int i = 0; i < len - 1; i++) {
-            String temp = str.substring(i, i+2);
-            if(isAlphabet(temp)) {
-                map.put(temp, map.getOrDefault(temp, 0) + 1);
+        for(int i = 0; i < str.length() - 1; i++) {
+            if(Character.isAlphabetic(str.charAt(i)) && Character.isAlphabetic(str.charAt(i + 1))) {
+                String word = str.substring(i, i + 2);
+                map.put(word, map.getOrDefault(word, 0) + 1);
             }
         }
-        
         return map;
-    }
-    
-    private boolean isAlphabet(String str) {
-        return Character.isAlphabetic(str.charAt(0)) && Character.isAlphabetic(str.charAt(1));
     }
 }
