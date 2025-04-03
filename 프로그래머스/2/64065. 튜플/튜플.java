1f62931d4d33a84665184fec2cecf0e123e5bfca
LeeJongsu <lik9819@naver.com>
2025-04-03 20:20:15 +0900

    [level 2] Title: 튜플, Time: 95.19 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,27 +1,31 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String s) {
         
-        String[] strArr = s.replaceAll("[{}]", " ").trim().split(" , ");
+        s = s.substring(1, s.length());
+        StringTokenizer st = new StringTokenizer(s, "{}");
         
-        Set<Integer> set = new HashSet<>();
-        int size = strArr.length;
-        int[] result = new int[size];
+        List<String> tupleList = new ArrayList<>();
         
-        Arrays.sort(strArr, (o1, o2) -> o1.length() - o2.length());
+        while(st.hasMoreTokens()) {
+            String token = st.nextToken();
+            if(token.equals(",")) continue;
+            tupleList.add(token);
+        }
         
-        for(int i = 0; i < size; i++) {
-            String[] elements = strArr[i].split(",");
-            for(String element : elements) {
-                int iNum = Integer.parseInt(element);
-                if(set.add(iNum)) {
-                    result[i] = iNum;
-                    break;
+        Collections.sort(tupleList, (o1, o2) -> o1.length() - o2.length());
+        
+        List<Integer> resultList = new ArrayList<>();
+        for(String tuple : tupleList) {
+            String[] parts = tuple.split(",");
+            for(String part : parts) {
+                int num = Integer.parseInt(part);
+                if(!resultList.contains(num)) {
+                    resultList.add(num);    
                 }
             }
         }
-            
-        return result;
+        
+        return resultList.stream().mapToInt(Integer::intValue).toArray();
     }
 }
