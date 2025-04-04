5521d1aa899f149e0902e3b728c481418468b6ea
LeeJongsu <lik9819@naver.com>
2025-04-04 09:23:17 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.54 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -2,46 +2,44 @@ import java.util.*;
 
 class Solution {
     public int solution(String str1, String str2) {
+        Map<String, Integer> map1 = solution(str1);
+        Map<String, Integer> map2 = solution(str2);
         
         final int MULTI_VALUE = 65536;
-        
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
-        
-        Map<String, Integer> map1 = getTwoWordsMap(str1);
-        Map<String, Integer> map2 = getTwoWordsMap(str2);
-        
         int intersection = 0;
         int sum = 0;
         
-        for(String word : map1.keySet()) {
-            int num1 = map1.get(word);
-            if(map2.containsKey(word)) {
-                int num2 = map2.get(word);
+        for(String key : map1.keySet()) {
+            int num1 = map1.get(key);
+            if(map2.containsKey(key)) {
+                int num2 = map2.get(key);
                 intersection += Math.min(num1, num2);
                 sum += Math.max(num1, num2);
-                map2.remove(word);
+                map2.remove(key);
             } else {
                 sum += num1;
             }
         }
         
-        for(String word : map2.keySet()) {
-            sum += map2.get(word);
+        for(String key : map2.keySet()) {
+            sum += map2.get(key);
         }
-        
         if(sum == 0) return MULTI_VALUE;
-        else return (int) (((double) intersection / sum) * MULTI_VALUE); 
+        else return (int) (((double) intersection / sum) * MULTI_VALUE);
     }
     
-    public Map<String, Integer> getTwoWordsMap(String str) {
+    public Map<String, Integer> solution(String str) {
         Map<String, Integer> map = new HashMap<>();
+        
+        str = str.toLowerCase();
+        
         for(int i = 0; i < str.length() - 1; i++) {
-            if(Character.isAlphabetic(str.charAt(i)) && Character.isAlphabetic(str.charAt(i + 1))) {
-                String word = str.substring(i, i + 2);
-                map.put(word, map.getOrDefault(word, 0) + 1);
+            if(Character.isAlphabetic(str.charAt(i)) && Character.isAlphabetic(str.charAt(i+1)))             {
+                String key = str.substring(i, i+2);
+                map.put(key, map.getOrDefault(key, 0) + 1); 
             }
         }
+        
         return map;
     }
 }
