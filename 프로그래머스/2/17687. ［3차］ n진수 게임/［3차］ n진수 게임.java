dc98462a8cc473f062392b3fc10ed822f07e1007
LeeJongsu <lik9819@naver.com>
2025-04-08 22:16:14 +0900

    [level 2] Title: [3차] n진수 게임, Time: 24.72 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,40 +1,49 @@
 import java.util.*;
 
 class Solution {
+    
+    char[] ch = new char[16];
+    
     public String solution(int n, int t, int m, int p) {
-        
-        StringBuilder sb = new StringBuilder();
-        
-        char[] ch = new char[16];
         for(int i = 0; i < 10; i++) {
             ch[i] = (char) (i + '0');
         }
         for(int i = 0; i < 6; i++) {
-            ch[i + 10] = (char) (i + 'A');
+            ch[i + 10] = (char) ('A' + i);
         }
         
-        for(int i = 0, j = 0; t > 0; i++) {
-            Stack<Integer> stack = toScale(n, i);
-            while(!stack.isEmpty() && t > 0) {
-                int num = stack.pop();
-                if(j % m == (p - 1)) {
-                    sb.append(ch[num]);
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 1; i <= m; i++) {
+            queue.offer(i);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int num = 0; t > 0; num++) {
+            String sNum = toScale(num, n);
+            for(int i = 0; i < sNum.length() && t > 0; i++) {
+                int current = queue.poll();
+                if(current == p) {
                     t--;
+                    sb.append(sNum.charAt(i));
                 }
-                j++;
+                queue.offer(current);
             }
         }
         
         return sb.toString();
     }
     
-    private Stack<Integer> toScale(int n, int num) {
-        Stack<Integer> stack = new Stack<>();
-        while(num >= n) {
-            stack.push(num % n);
-            num /= n;
+    public String toScale(int num, int n) {
+        if(num == 0) return "0";
+        else {
+            StringBuilder sb = new StringBuilder();
+            while(num > 0) {
+                int value = num % n;
+                sb.insert(0, ch[value]);
+                num /= n;
+            }
+            return sb.toString();
         }
-        stack.push(num % n);
-        return stack;
     }
 }
