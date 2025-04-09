a2a08049582ea270bc3db7a1b99553ec3303e3a1
LeeJongsu <lik9819@naver.com>
2025-04-09 21:37:25 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 11.74 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,32 +1,28 @@
 class Solution {
     public int solution(int n, int k) {
-        String str = toChange(n, k);
-        String[] strArr = str.split("0+");
         
         int result = 0;
-        for(String temp : strArr) {
-            if(isPrime(Long.parseLong(temp))) {
-                result++;
-            }
-        }
         
+        String scaled = toScale(n, k);
+        for(String sNum : scaled.split("0{1,}")) {
+            if(isPrime(Long.parseLong(sNum))) result++;
+        }
         return result;
     }
     
-    private String toChange(int n, int k) {
+    public String toScale(int n, int k) {
         StringBuilder sb = new StringBuilder();
-        while(n > k) {
+        while(n > 0) {
             sb.insert(0, n % k);
             n /= k;
         }
-        sb.insert(0, n % k);
         
         return sb.toString();
     }
     
-    private boolean isPrime(long num) {
+    public boolean isPrime(long num) {
         if(num == 1) return false;
-        for(int i = 3; i <= Math.sqrt(num); i+=2) {
+        for(long i = 2; i <= (long) Math.sqrt(num); i++) {
             if(num % i == 0) return false;
         }
         return true;
