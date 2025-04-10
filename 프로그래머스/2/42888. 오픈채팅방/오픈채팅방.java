dc9eb61e1d7f31a3c4094cbebaf90f2501f21d00
LeeJongsu <lik9819@naver.com>
2025-04-10 18:00:22 +0900

    [level 2] Title: 오픈채팅방, Time: 162.13 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,43 +1,6 @@
-import java.util.*;
-
 class Solution {
-    class User {
-        String id;
-        boolean isEnter;
-        
-        public User(String id, boolean isEnter) {
-            this.id = id;
-            this.isEnter = isEnter;
-        }
-    }
     public String[] solution(String[] record) {
-        Queue<User> queue = new LinkedList<>();
-        Map<String, String> userMap = new HashMap<>();
-        
-        for(String info : record) {
-            String[] parts = info.split(" ");
-            String command = parts[0];
-            String id = parts[1];
-            
-            if(command.equals("Leave")) {
-                queue.offer(new User(id, false));
-                continue;
-            }
-            
-            String name = parts[2];
-            userMap.put(id, name);
-            if(command.equals("Enter")) {
-                queue.offer(new User(id, true));
-            }
-        }
-            
-        String[] result = new String[queue.size()];
-        for(int i = 0; !queue.isEmpty(); i++) {
-            User user = queue.poll();
-            String end = user.isEnter ? "들어왔습니다." : "나갔습니다.";
-            result[i] = userMap.get(user.id) + "님이 " + end;
-        }
-            
-        return result;
+        String[] answer = {};
+        return answer;
     }
 }
