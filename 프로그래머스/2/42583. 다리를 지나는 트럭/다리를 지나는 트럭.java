824ade3cf8a8b0a4b461c05f04d2dfa91ca15353
LeeJongsu <lik9819@naver.com>
2025-05-21 09:53:03 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 24.39 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,62 +1,32 @@
 import java.util.*;
 
 class Solution {
-    
-    class Truck {
-        int weight;
-        int time;
-        
-        public Truck(int weight, int time) {
-            this.weight = weight;
-            this.time = time;
-        }
-        
-        public void decreaseTime() {
-            this.time -= 1;
-        }
-    }
-    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         
-        int result = 0;
-        int curCount = 0;
-        int curWeight = 0;
-        int i = 0;
+        int time = 0;
+        int idx = 0;
+        long sumOnBridge = 0;
         
-        ArrayDeque<Truck> truckQ = new ArrayDeque<>();
-        truckQ.offer(new Truck(truck_weights[i], bridge_length));
+        Queue<Integer> bridge = new LinkedList<>();
         
-        curWeight += truck_weights[i];
-        curCount++;
-        i++;
-        result++;
-        
-        while(!truckQ.isEmpty()) {
-         
-            int size = truckQ.size();
-            for(int j = 0; j < size; j++) {
-                Truck truck = truckQ.pollFirst();
-           
-                truck.decreaseTime();
-                if(truck.time == 0) {
-                    curCount--;
-                    curWeight -= truck.weight;
-                    continue;
-                }
-    
-                truckQ.offerLast(truck);
-            }
-            
-            
-            if(i < truck_weights.length && curCount < bridge_length && curWeight + truck_weights[i] <= weight) {
-                curCount++;
-                curWeight += truck_weights[i];
-                truckQ.addLast(new Truck(truck_weights[i], bridge_length));
-                i++;
-            }
-            result++;
+        for(int i = 0; i < bridge_length; i++) {
+            bridge.offer(0);
         }
         
-        return result;
+        while(idx < truck_weights.length || sumOnBridge > 0) {
+            time++;
+            
+            sumOnBridge -= bridge.poll();
+            
+            if(idx < truck_weights.length && truck_weights[idx] + sumOnBridge <= weight) {
+                bridge.offer(truck_weights[idx]);
+                sumOnBridge += truck_weights[idx];
+                idx++;
+            } else {
+                bridge.offer(0);
+            }
+        }
+        
+        return time;
     }
 }
