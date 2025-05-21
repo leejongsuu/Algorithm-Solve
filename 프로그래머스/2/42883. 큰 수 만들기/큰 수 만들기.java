6acfef8c83cd4d3a6be7c6192def6f3d598370c1
LeeJongsu <lik9819@naver.com>
2025-05-21 09:58:21 +0900

    [level 2] Title: 큰 수 만들기, Time: 14.83 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,22 +1,19 @@
-import java.util.*;
-
 class Solution {
-    
     public String solution(String number, int k) {
         
         int len = number.length();
         int top = -1;
         int keep = len - k;
-        
         char[] stack = new char[len];
         
         for(int i = 0; i < len; i++) {
             char c = number.charAt(i);
             
             while(top >= 0 && k > 0 && stack[top] < c) {
-                top--;
                 k--;
+                top--;
             }
+            
             stack[++top] = c;
         }
         
