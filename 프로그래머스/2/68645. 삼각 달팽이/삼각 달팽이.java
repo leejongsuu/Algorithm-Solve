b4747c6ad1aa8fb208c56fc4bd0ebed58725de1b
LeeJongsu <lik9819@naver.com>
2025-05-22 10:38:22 +0900

    [level 2] Title: 삼각 달팽이, Time: 22.13 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,59 +1,39 @@
-import java.util.*;
-
 class Solution {
-    
-    static int[] dr = {1, 0, -1, 0};
-    static int[] dc = {0, 1, 0, -1};
-    static int[][] triangle;
     public int[] solution(int n) {
         
-        triangle = new int[n][n];
-    
-        dfs(0, 0, n, 1, n);
+        int size = n * (n + 1) / 2;
+        int[] result = new int[size];
         
-        List<Integer> list = new ArrayList<>();
+        int[][] triangle = new int[n][n];
+        
+        int[] dr = {1, 0, -1};
+        int[] dc = {0, 1, -1};
+        
+        int r = 0, c = 0, d = 0;
+        for(int num = 1; num <= size; num++) {
+            triangle[r][c] = num;
+            
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+            if(nr < 0 || nc < 0 || nr >= n || nc >= n || triangle[nr][nc] != 0) {
+                d = (d + 1) % 3;
+                nr = r + dr[d];
+                nc = c + dc[d];
+            }
+            
+            r = nr;
+            c = nc;
+        }
+        
+        int idx = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 if(triangle[i][j] != 0) {
-                    list.add(triangle[i][j]);
+                    result[idx++] = triangle[i][j];
                 }
             }
         }
         
-        return list.stream().mapToInt(Integer::intValue).toArray();
-    }
-    
-    void dfs(int r, int c, int level, int num, int n) {
-        
-        for(int i = 0; i < level - 1; i++) {
-            triangle[r][c] = num++;
-            r += 1;
-        }
-        triangle[r][c] = num++;
-        
-        level--;
-        if(level == 0) return;
-        c += 1;
-        
-        for(int i = 0; i < level - 1; i++) {
-            triangle[r][c] = num++;
-            c += 1;
-        }
-        triangle[r][c] = num++;
-        
-        level--;
-        if(level == 0) return;
-        r -= 1;
-        
-        for(int i = 0; i < level - 1; i++) {
-            triangle[r][c] = num++;
-            r -= 1;
-        }
-        triangle[r][c] = num++;
-        
-        level--;
-        if(level == 0) return;
-        
-        dfs(r + 1, n - c, level, num, n);
+        return result;
     }
 }
