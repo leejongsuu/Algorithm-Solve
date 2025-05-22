5e1a8af7a124501ae4aad33c0cf6db833a644964
LeeJongsu <lik9819@naver.com>
2025-05-22 10:50:11 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -3,24 +3,24 @@ class Solution {
         
         int ans = 0;
         while(storey > 0) {
-            int d = storey % 10; // 일의 자리
-            int q = (storey / 10) % 10; // 다음 자리(10의 자리)
-            
+            int d = storey % 10;
             if(d < 5) {
                 ans += d;
                 storey /= 10;
             } else if(d > 5) {
                 ans += 10 - d;
-                storey = storey / 10 + 1;
+                storey = (storey / 10) + 1;
             } else {
-                ans += 5;
+                ans += d;
+                int q = (storey / 10) % 10;
                 if(q >= 5) {
-                    storey = storey / 10 + 1;
+                    storey = (storey / 10) + 1;
                 } else {
                     storey /= 10;
                 }
             }
         }
+        
         return ans;
     }
 }
