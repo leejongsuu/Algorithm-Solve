5b819fae17085a34385984c571a241cb7c330314
LeeJongsu <lik9819@naver.com>
2025-05-23 21:21:55 +0900

    [level 2] Title: 시소 짝꿍, Time: 31.41 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -2,53 +2,50 @@ import java.util.*;
 
 class Solution {
     public long solution(int[] weights) {
-        // 1) 무게별 빈도 계산
-        Map<Integer, Long> freq = new HashMap<>();
-        for (int w : weights) {
-            freq.put(w, freq.getOrDefault(w, 0L) + 1);
+        long answer = 0;
+        
+        Map<Integer, Long> map = new HashMap<>();
+        for(int weight : weights) {
+            map.put(weight, map.getOrDefault(weight, 0L) + 1);
         }
-
-        long ans = 0;
-
-        // 2) 같은 무게끼리 조합: C(n,2)
-        for (long cnt : freq.values()) {
-            ans += cnt * (cnt - 1) / 2;
+        
+        for(long value : map.values()) {
+            answer += value * (value - 1) / 2;
         }
-
-        // 3) 다른 무게 조합: (d1,d2) 쌍 중 d1/d2>1인 경우만
-        //    (3/2), (4/2), (4/3)
-        for (Map.Entry<Integer, Long> e : freq.entrySet()) {
+        
+        for(Map.Entry<Integer, Long> e : map.entrySet()) {
             int w = e.getKey();
             long cntW = e.getValue();
-
-            // 3/2 비율: w2 = w * 3/2
-            int t1 = w * 3;
-            if (t1 % 2 == 0) {
-                int w2 = t1 / 2;
-                Long cntW2 = freq.get(w2);
-                if (cntW2 != null) {
-                    ans += cntW * cntW2;
+            
+            // 3 / 2
+            int w2 = w * 3;
+            if(w2 % 2 == 0) {
+                w2 /= 2;
+                Long cntW2 = map.get(w2);
+                if(cntW2 != null) {
+                    answer += cntW * cntW2;
                 }
             }
-
-            // 4/2 비율: w2 = w * 2
-            int w2 = w * 2;
-            Long cntW2 = freq.get(w2);
-            if (cntW2 != null) {
-                ans += cntW * cntW2;
+            
+            // 4 / 2
+            int w3 = w * 4;
+            w3 /= 2;
+            Long cntW3 = map.get(w3);
+            if(cntW3 != null) {
+                answer += cntW * cntW3;
             }
-
-            // 4/3 비율: w2 = w * 4/3
-            int t3 = w * 4;
-            if (t3 % 3 == 0) {
-                int w3 = t3 / 3;
-                Long cntW3 = freq.get(w3);
-                if (cntW3 != null) {
-                    ans += cntW * cntW3;
+            
+            // 4 / 3
+            int w4 = w * 4;
+            if(w4 % 3 == 0) {
+                w4 /= 3;
+                Long cntW4 = map.get(w4);
+                if(cntW4 != null) {
+                    answer += cntW * cntW4;
                 }
             }
         }
-
-        return ans;
+        
+        return answer;
     }
 }
