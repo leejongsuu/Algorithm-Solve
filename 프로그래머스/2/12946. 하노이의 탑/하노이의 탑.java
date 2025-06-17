11d77d29cb647851d1b16f1b652f2a64397c2590
LeeJongsu <lik9819@naver.com>
2025-06-17 18:25:07 +0900

    [level 2] Title: 하노이의 탑, Time: 2.16 ms, Memory: 95.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    
+    List<int[]> answerList = new ArrayList<>();
+    
+    public int[][] solution(int n) {
+        hanoi(n, 1, 3, 2);
+        return answerList.toArray(new int[answerList.size()][]);
+    }
+    
+    void hanoi(int n, int from, int to, int via) {
+        if(n == 1) {
+            answerList.add(new int[]{from, to});
+            return;
+        }
+        
+        hanoi(n - 1, from, via, to);
+        answerList.add(new int[]{from, to});
+        hanoi(n - 1, via, to, from);
+    }
+}
