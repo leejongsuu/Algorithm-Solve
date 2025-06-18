a2c84c30e1951c633f305d7b070345c106bde9d7
LeeJongsu <lik9819@naver.com>
2025-06-18 10:02:22 +0900

    [level 2] Title: 광물 캐기, Time: 0.90 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -2,15 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] picks, String[] minerals) {
+
         int totalPicks = picks[0] + picks[1] + picks[2];
         int len = Math.min((minerals.length + 4) / 5, totalPicks);
-        
-        int[][] fatigueTable = new int[][]{
-            {1, 1, 1},
-            {5, 1, 1},
-            {25, 5, 1}
-        };
-        
         List<int[]> blocks = new ArrayList<>();
         
         for(int i = 0; i < len; i++) {
@@ -23,18 +17,21 @@ class Solution {
             blocks.add(new int[]{dia, iron, stone});
         }
         
-        blocks.sort((a, b) ->          
-            (b[0] * 25 + b[1] * 5 + b[2]) - (a[0] * 25 + a[1] * 5 + a[2])
-        );
+        blocks.sort((a, b) -> (b[0] * 25 + b[1] * 5 + b[0]) - (a[0] * 25 + a[1] * 5 + a[0]));
         
         int fatigue = 0;
         int pickType = 0;
+        
+        int[][] fatigueTable = {
+            {1, 1, 1},
+            {5, 1, 1},
+            {25, 5, 1}
+        };
         
         for(int[] block : blocks) {
             while(pickType < 3 && picks[pickType] == 0) {
                 pickType++;
             }
-            
             if(pickType == 3) break;
             
             fatigue += block[0] * fatigueTable[pickType][0];
