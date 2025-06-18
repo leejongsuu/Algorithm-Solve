a0002a56c91573a8a18b9589188c6a0b4f484236
LeeJongsu <lik9819@naver.com>
2025-06-18 09:53:01 +0900

    [level 2] Title: 우박수열 정적분, Time: 6.55 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-
+    
     class Point {
         int x, y;
         public Point(int x, int y) {
@@ -9,43 +9,43 @@ class Solution {
             this.y = y;
         }
     }
-
-    List<Point> points = new ArrayList<>();
-
+    
     public double[] solution(int k, int[][] ranges) {
+        
+        List<Point> points = new ArrayList<>();
+        
         int n = 0;
-
-        while (k > 1) {
-            points.add(new Point(n++, k));
-            k = (k % 2 == 0) ? k / 2 : k * 3 + 1;
+        while(k > 1) {
+            points.add(new Point(n, k));
+            k = (k % 2 == 0 ? k / 2 : k * 3 + 1);
+            n++;
         }
-        points.add(new Point(n, 1)); // 마지막 1 추가
-
+        points.add(new Point(n, 1));
+        
         double[] dis = new double[n];
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             Point a = points.get(i);
             Point b = points.get(i + 1);
-            dis[i] = (a.y + b.y) / 2.0; // 사다리꼴 면적
+            dis[i] = (a.y + b.y) / 2.0;
         }
-
+        
         int size = ranges.length;
         double[] result = new double[size];
-
-        for (int i = 0; i < size; i++) {
+        
+        for(int i = 0; i < size; i++) {
             int start = ranges[i][0];
             int end = n + ranges[i][1];
-
-            if (start > end) {
+            if(start > end) {
                 result[i] = -1.0;
             } else {
                 double sum = 0;
-                for (int j = start; j < end; j++) {
+                for(int j = start; j < end; j++) {
                     sum += dis[j];
                 }
                 result[i] = sum;
             }
         }
-
+        
         return result;
     }
 }
