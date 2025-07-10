2cbf2cca37e9095f5c80102f74bc9d85229b8266
LeeJongsu <lik9819@naver.com>
2025-07-10 13:17:35 +0900

    [level 2] Title: 전화번호 목록, Time: 274.82 ms, Memory: 212 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        
+
         Set<String> phoneSet = new HashSet<>();
+        
         for(String phone : phone_book) {
             phoneSet.add(phone);
         }
         
         for(String phone : phone_book) {
-            int len = phone.length();
-            for(int i = 1; i < len; i++) {
-                if(phoneSet.contains(phone.substring(0, i))) {
-                    return false;
-                }
+            for(int i = 1; i < phone.length(); i++) {
+                String prefix = phone.substring(0, i);
+                if(phoneSet.contains(prefix)) return false;
             }
         }
+        
         return true;
     }
 }
