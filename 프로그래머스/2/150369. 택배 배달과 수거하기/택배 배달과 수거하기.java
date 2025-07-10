a696456c588fa3d4495fe90aeff43bda04ac826b
LeeJongsu <lik9819@naver.com>
2025-07-10 11:24:10 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 25.95 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,13 +1,9 @@
-import java.util.*;
-
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        
         int dPtr = n - 1, pPtr = n - 1;
         
         while(dPtr >= 0 || pPtr >= 0) {
-            // 가장 멀리까지 가야 할 집 인덱스
             while(dPtr >= 0 && deliveries[dPtr] == 0) dPtr--;
             while(pPtr >= 0 && pickups[pPtr] == 0) pPtr--;
             
@@ -15,21 +11,18 @@ class Solution {
             
             int distance = Math.max(dPtr, pPtr);
             
-            // 왕복 거리 누적
             answer += (distance + 1) * 2;
             
-            // 배달
             int capRemain = cap;
             while(capRemain > 0 && dPtr >= 0) {
-                int delivery = Math.min(capRemain, deliveries[dPtr]);
-                deliveries[dPtr] -= delivery;
-                capRemain -= delivery;
+                int deliver = Math.min(capRemain, deliveries[dPtr]);
+                deliveries[dPtr] -= deliver;
+                capRemain -= deliver;
                 if(deliveries[dPtr] == 0) {
                     dPtr--;
                 }
             }
             
-            // 수거
             capRemain = cap;
             while(capRemain > 0 && pPtr >= 0) {
                 int pickup = Math.min(capRemain, pickups[pPtr]);
