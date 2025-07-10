d967ca26ac7f70441a45db550084f9c3e7571271
LeeJongsu <lik9819@naver.com>
2025-07-10 11:58:19 +0900

    [level 2] Title: 교점에 별 만들기, Time: 33.94 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -2,36 +2,35 @@ import java.util.*;
 
 class Solution {
     public String[] solution(int[][] line) {
-        List<long[]> points = new ArrayList<>();
+        
+        List<long[]> pointList = new ArrayList<>();
+        
         long minX = Long.MAX_VALUE, maxX = Long.MIN_VALUE;
         long minY = Long.MAX_VALUE, maxY = Long.MIN_VALUE;
-
-        int n = line.length;
-        // 1) 모든 쌍의 교점 계산
-        for (int i = 0; i < n - 1; i++) {
+        
+        for(int i = 0; i < line.length - 1; i++) {
             long A = line[i][0], B = line[i][1], E = line[i][2];
-            for (int j = i + 1; j < n; j++) {
+            for(int j = i + 1; j < line.length; j++) {
                 long C = line[j][0], D = line[j][1], F = line[j][2];
-
-                long div = A * D - B * C;
-                if (div == 0) continue;
-
-                // 교점 (x, y) = ((B*F - E*D)/div, (E*C - A*F)/div)
-                long numX = B * F - E * D;
-                long numY = E * C - A * F;
-                if (numX % div != 0 || numY % div != 0) continue;
-
-                long x = numX / div;
-                long y = numY / div;
-
-                points.add(new long[]{x, y});
+                
+                long div = A*D - B*C;
+                
+                if(div == 0) continue;
+                
+                long x = (B*F - E*D) / div;
+                long y = (E*C - A*F) / div;
+                
+                if(A*x + B*y + E != 0 || C*x + D*y + F != 0) continue;
+                
                 minX = Math.min(minX, x);
                 maxX = Math.max(maxX, x);
                 minY = Math.min(minY, y);
                 maxY = Math.max(maxY, y);
+                
+                pointList.add(new long[]{x, y});
             }
         }
-
+        
         int width  = (int)(maxX - minX + 1);
         int height = (int)(maxY - minY + 1);
         char[][] grid = new char[height][width];
@@ -39,18 +38,19 @@ class Solution {
         for (int r = 0; r < height; r++) {
             Arrays.fill(grid[r], '.');
         }
-
-        for (long[] p : points) {
-            int gx = (int)(p[0] - minX);
-            int gy = (int)(maxY - p[1]);
-            grid[gy][gx] = '*';
-        }
-
-        String[] answer = new String[height];
-        for (int r = 0; r < height; r++) {
-            answer[r] = new String(grid[r]);
+        
+        for(long[] point : pointList) {
+            int x = (int) (point[0] - minX);
+            int y = (int) (maxY - point[1]);
+            
+            grid[y][x] = '*';
         }
         
-        return answer;
+        String[] result = new String[height];
+        for(int r = 0; r < height; r++) {
+            result[r] = new String(grid[r]);
+        }
+        
+        return result;
     }
 }
