e4ca7d8462cdedd18d1d83750118d2a9ca6ba2db
LeeJongsu <lik9819@naver.com>
2025-07-14 09:59:41 +0900

    [level 2] Title: [3차] 압축, Time: 3.71 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -7,33 +7,30 @@ class Solution {
         
         Map<String, Integer> map = new HashMap<>();
         
-        // A부터 Z까지 사전에 초기화
-        for (int i = 0; i < 26; i++) {
+        for(int i = 0; i < 26; i++) {
             map.put(String.valueOf((char) ('A' + i)), i + 1);
         }
-
-        int index = 27; // 사전의 색인 번호 27부터 시작
+        
+        int index = 27;
         int len = msg.length();
         String before = "";
         
-        for (int i = 0; i < len;) {
+        for(int i = 0; i < len;) {
             int j = i + 1;
-            for (; j <= len; j++) {
+            for(; j <= len; j++) {
                 String temp = msg.substring(i, j);
                 
-                // 사전에 없는 경우
-                if (!map.containsKey(temp)) {
-                    result.add(map.get(before)); // 직전 일치하는 문자열의 색인 번호 저장
-                    map.put(temp, index++); // 새로운 문자열을 사전에 추가
+                if(!map.containsKey(temp)) {
+                    result.add(map.get(before));
+                    map.put(temp, index++);
                     break;
                 }
                 
-                before = temp; // 현재 일치하는 문자열 저장
+                before = temp;
             }
-            i = j - 1; // 현재 위치를 다음 위치로 업데이트
+            
+            i = j - 1;
         }
-        
-        // 마지막 남은 문자열의 색인 번호 추가
         result.add(map.get(before));
         
         return result.stream().mapToInt(Integer::intValue).toArray();
