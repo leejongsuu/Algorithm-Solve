4e3fa270b8879bf9c8dc0c8fc679247acca0e7e6
LeeJongsu <lik9819@naver.com>
2025-07-15 15:52:22 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 11.17 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,21 +1,26 @@
 class Solution {
     public long[] solution(long[] numbers) {
+        
         int len = numbers.length;
-        long[] result = new long[len];
+        
+        long[] answer = new long[len];
         
         for(int i = 0; i < len; i++) {
-            long x = numbers[i];
-            if(x % 2 == 0) {
-                result[i] = x + 1;
-            } else {
-                long bit = 1;
-                while((x & bit) != 0) {
-                    bit <<= 1;
-                }
-                result[i] = x + bit - (bit >> 1);
+            long number = numbers[i];
+            
+            if(number % 2 == 0) {
+                answer[i] = number + 1;
+                continue;
             }
+            
+            long bit = 1;
+            while((bit & number) != 0) {
+                bit <<= 1;
+            }
+             
+            answer[i] = (number + bit) - (bit >> 1);
         }
         
-        return result;
+        return answer;
     }
 }
