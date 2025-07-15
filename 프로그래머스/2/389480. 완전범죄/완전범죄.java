fb8d85fb92caa08659893e9ef0546127f2457f38
LeeJongsu <lik9819@naver.com>
2025-07-15 10:23:18 +0900

    [level 2] Title: 완전범죄, Time: 5.53 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,42 +1,40 @@
 class Solution {
-     public int solution(int[][] info, int n, int m) {
+    public int solution(int[][] info, int n, int m) {
+        
         boolean[][] dp = new boolean[n][m];
         dp[0][0] = true;
-
-        for (int[] item : info) {
+        
+        for(int[] item : info) {
             int aCost = item[0];
             int bCost = item[1];
             boolean[][] next = new boolean[n][m];
-
-            for (int a = 0; a < n; a++) {
-                for (int b = 0; b < m; b++) {
-                    if (!dp[a][b]) continue;
-                    // 1) A가 훔치기
+            
+            for(int a = 0; a < n; a++) {
+                for(int b = 0; b < m; b++) {
+                    if(!dp[a][b]) continue;
+                    
                     int na = a + aCost;
-                    int nb = b;
-                    if (na < n) {
-                        next[na][nb] = true;
+                    int nb = b + bCost;
+                    
+                    if(na < n) {
+                        next[na][b] = true;
                     }
-                    // 2) B가 훔치기
-                    na = a;
-                    nb = b + bCost;
-                    if (nb < m) {
-                        next[na][nb] = true;
+                    
+                    if(nb < m) {
+                        next[a][nb] = true;
                     }
                 }
             }
+            
             dp = next;
         }
-
-        // 가능한 상태 중에서 A 흔적 최소값 찾기
-        for (int a = 0; a < n; a++) {
-            for (int b = 0; b < m; b++) {
-                if (dp[a][b]) {
-                    return a;
-                }
+        
+        for(int a = 0; a < n; a++) {
+            for(int b = 0; b < m; b++) {
+                if(dp[a][b]) return a;
             }
         }
-        // 불가능하면 -1
+        
         return -1;
     }
 }
