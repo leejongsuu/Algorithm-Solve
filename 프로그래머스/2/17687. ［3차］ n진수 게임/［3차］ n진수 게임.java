e0ce9fb78c249045dfd132cd6b470c6f7a65d0ef
LeeJongsu <lik9819@naver.com>
2025-07-15 10:09:46 +0900

    [level 2] Title: [3차] n진수 게임, Time: 16.08 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,21 +1,19 @@
 class Solution {
-    char[] ch;
+    
     public String solution(int n, int t, int m, int p) {
-        ch = new char[16];
-        for(int i = 0; i < 10; i++) ch[i] = (char) (i + '0');
-        for(int i = 0; i < 6; i++) ch[i + 10] = (char) (i + 'A');
+        char[] ch = new char[16];
+        for(int i = 0; i < 10; i++) ch[i] = (char) ('0' + i);
+        for(int i = 0; i < 6; i++) ch[i + 10] = (char) ('A' + i);
         
         StringBuilder sb = new StringBuilder();
-        int num = 0, sequence = 0;
         
+        int num = 0, sequence = 0;
         while(sb.length() < t) {
-            String scale = toScale(n, num);
+            String scale = toScale(n, num, ch);
             for(char s : scale.toCharArray()) {
                 if(sequence % m == p - 1) {
                     sb.append(s);
-                }
-                if(sb.length() == t) {
-                    break;
+                    if(sb.length() == t) break;
                 }
                 sequence++;
             }
@@ -25,7 +23,7 @@ class Solution {
         return sb.toString();
     }
     
-    public String toScale(int n, int num) {
+    String toScale(int n, int num, char[] ch) {
         if(num == 0) return "0";
         
         StringBuilder sb = new StringBuilder();
@@ -33,6 +31,7 @@ class Solution {
             sb.insert(0, ch[num % n]);
             num /= n;
         }
+        
         return sb.toString();
     }
 }
