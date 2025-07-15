bc4521843a771a555f096e3c78c0d09044a678e5
LeeJongsu <lik9819@naver.com>
2025-07-15 10:14:37 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.39 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -2,20 +2,16 @@ class Solution {
     public int solution(int n) {
         final int MOD = 1_000_000_007;
         
-        if ((n & 1) == 1) return 0;
-
+        if(n % 2 == 1) return 0;
+        
         int[] dp = new int[n + 1];
         dp[0] = 1;
+        if(n > 2) dp[2] = 3;
         
-        if (n >= 2) dp[2] = 3;
-
-        
-        for (int i = 4; i <= n; i += 2) {
-            // 4*dp[i-2] - dp[i-4], 음수 방지를 위해 +MOD
-            long v = (4L * dp[i - 2] % MOD - dp[i - 4] + MOD) % MOD;
-            dp[i] = (int) v;
+        for(int i = 4; i <= n; i++) {
+            dp[i] = (int) (4L * dp[i - 2] % MOD - dp[i - 4] + MOD) % MOD;
         }
-
+        
         return dp[n];
     }
 }
