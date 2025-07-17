316147dc2e596ab590423916ef05bc557ed9dc78
LeeJongsu <lik9819@naver.com>
2025-07-17 19:26:11 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 18.45 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,32 +1,46 @@
 import java.util.*;
 
 class Solution {
+    
+    class Truck {
+        int time;
+        int weight;
+        
+        public Truck(int time, int weight) {
+            this.time = time;
+            this.weight = weight;
+        }
+    }
+    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         
-        int time = 0;
-        int idx = 0;
-        long sumOnBridge = 0;
+        int len = truck_weights.length;
+        int current_weights = 0;
         
-        Queue<Integer> bridge = new LinkedList<>();
+        Queue<Truck> queue = new LinkedList<>();
         
-        for(int i = 0; i < bridge_length; i++) {
-            bridge.offer(0);
-        }
-        
-        while(idx < truck_weights.length || sumOnBridge > 0) {
-            time++;
+        int t = 0;
+        for(int i  = 0; i < len; t++) {
+            if(!queue.isEmpty()) {
+                if(t - queue.peek().time >= bridge_length) {
+                    current_weights -= queue.poll().weight;
+                }
+            }
             
-            sumOnBridge -= bridge.poll();
-            
-            if(idx < truck_weights.length && truck_weights[idx] + sumOnBridge <= weight) {
-                bridge.offer(truck_weights[idx]);
-                sumOnBridge += truck_weights[idx];
-                idx++;
-            } else {
-                bridge.offer(0);
+            if(current_weights + truck_weights[i] <= weight && queue.size() + 1 <= bridge_length ) {
+                current_weights += truck_weights[i];
+                queue.offer(new Truck(t, truck_weights[i]));
+                i++;
             }
         }
         
-        return time;
+        while(!queue.isEmpty()) {
+            if(t - queue.peek().time >= bridge_length) {
+                    current_weights -= queue.poll().weight;
+            }
+            t++;
+        }
+        
+        return t;
     }
 }
