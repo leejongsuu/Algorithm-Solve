62bed011e420cc103c7944c44bfe8b32d5d9ca21
LeeJongsu <lik9819@naver.com>
2025-07-17 23:03:36 +0900

    [level 2] Title: 삼각 달팽이, Time: 19.18 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -3,7 +3,6 @@ class Solution {
         
         int size = n * (n + 1) / 2;
         int[] result = new int[size];
-        
         int[][] triangle = new int[n][n];
         
         int[] dr = {1, 0, -1};
@@ -15,6 +14,7 @@ class Solution {
             
             int nr = r + dr[d];
             int nc = c + dc[d];
+            
             if(nr < 0 || nc < 0 || nr >= n || nc >= n || triangle[nr][nc] != 0) {
                 d = (d + 1) % 3;
                 nr = r + dr[d];
@@ -28,7 +28,7 @@ class Solution {
         int idx = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(triangle[i][j] != 0) {
+                if(triangle[i][j] > 0) {
                     result[idx++] = triangle[i][j];
                 }
             }
