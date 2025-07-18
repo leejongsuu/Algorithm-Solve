ee8eddf1d5dab0ef7dbc75f650c3a5e7cb80912f
LeeJongsu <lik9819@naver.com>
2025-07-18 11:15:27 +0900

    [level 2] Title: 삼각 달팽이, Time: 18.24 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[] solution(int n) {
-        
+         
         int size = n * (n + 1) / 2;
         int[] result = new int[size];
         int[][] triangle = new int[n][n];
         
-        int[] dr = {1, 0, -1};
-        int[] dc = {0, 1, -1};
-        
         int r = 0, c = 0, d = 0;
+        int[][] dir = new int[][]{{1, 0}, {0, 1}, {-1, -1}};
+        
         for(int num = 1; num <= size; num++) {
             triangle[r][c] = num;
             
-            int nr = r + dr[d];
-            int nc = c + dc[d];
+            int nr = r + dir[d][0];
+            int nc = c + dir[d][1];
             
             if(nr < 0 || nc < 0 || nr >= n || nc >= n || triangle[nr][nc] != 0) {
                 d = (d + 1) % 3;
-                nr = r + dr[d];
-                nc = c + dc[d];
+                nr = r + dir[d][0];
+                nc = c + dir[d][1];
             }
             
             r = nr;
@@ -28,7 +27,7 @@ class Solution {
         int idx = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(triangle[i][j] > 0) {
+                if(triangle[i][j] != 0) {
                     result[idx++] = triangle[i][j];
                 }
             }
