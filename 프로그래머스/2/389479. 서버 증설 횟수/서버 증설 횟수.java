deebd954e75f2ca712ebe1f66349f8d04f166265
LeeJongsu <lik9819@naver.com>
2025-07-21 14:53:37 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,21 +1,21 @@
 class Solution {
     public int solution(int[] players, int m, int k) {
-        
         int result = 0;
         int active = 0;
         
         int[] server = new int[24];
-        for(int t = 0; t < 24; t++) {
-            if(t >= k) {
-                active -= server[t - k];
+        
+        for(int i = 0; i < 24; i++) {
+            if(i >= k) {
+                active -= server[i - k];
             }
             
-            int req = players[t] / m;
+            int req = players[i] / m;
             if(active < req) {
                 int add = req - active;
                 result += add;
                 active += add;
-                server[t] = add;
+                server[i] = add;
             }
         }
         
