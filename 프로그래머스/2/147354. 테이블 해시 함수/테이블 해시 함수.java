a131a78e63f02082fd6c1ccc08e9d6c2cb08467a
LeeJongsu <lik9819@naver.com>
2025-07-23 19:59:08 +0900

    [level 2] Title: 테이블 해시 함수, Time: 14.34 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -2,26 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
+        
         Comparator<int[]> comp = new Comparator<>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                int dif = o1[col - 1] - o2[col - 1];
-                if(dif == 0) return o2[0] - o1[0];
-                return dif;                
+                if(o1[col - 1] != o2[col -1]) {
+                    return o1[col - 1] - o2[col -1];
+                }
+                return o2[0] - o1[0];
             }
         };
         
         Arrays.sort(data, comp);
         
-        int answer = 0;
-        for(int row = row_begin; row <= row_end; row++) {
+        int result = 0;
+        
+        for(int i = row_begin; i <= row_end; i++) {
             int sum = 0;
-            for(int num : data[row - 1]) {
-                sum += num % row;
+            for(int d : data[i - 1]) {
+                sum += d % i;
             }
-            answer ^= sum;
+            result ^= sum;
         }
         
-        return answer;
+        return result;
     }
 }
