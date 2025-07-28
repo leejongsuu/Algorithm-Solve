6347b85b908dcc1614906397db4c255da4aec318
LeeJongsu <lik9819@naver.com>
2025-07-28 20:09:54 +0900

    [level 2] Title: 우박수열 정적분, Time: 8.51 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -4,6 +4,7 @@ class Solution {
     
     class Point {
         int x, y;
+        
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
@@ -11,39 +12,41 @@ class Solution {
     }
     
     public double[] solution(int k, int[][] ranges) {
+        List<Point> pointList = new ArrayList<>();
         
-        List<Point> points = new ArrayList<>();
-        
-        int n = 0;
-        while(k > 1) {
-            points.add(new Point(n, k));
+        for(int x = 0; k >= 1; x++) {
+            pointList.add(new Point(x, k));
+            if(k == 1) {
+                break;
+            }
             k = (k % 2 == 0 ? k / 2 : k * 3 + 1);
-            n++;
         }
-        points.add(new Point(n, 1));
         
+        int n = pointList.size() - 1;
         double[] dis = new double[n];
         for(int i = 0; i < n; i++) {
-            Point a = points.get(i);
-            Point b = points.get(i + 1);
+            Point a = pointList.get(i);
+            Point b = pointList.get(i + 1);
+            
             dis[i] = (a.y + b.y) / 2.0;
         }
         
-        int size = ranges.length;
-        double[] result = new double[size];
-        
-        for(int i = 0; i < size; i++) {
+        int len = ranges.length;
+        double[] result = new double[len];
+        for(int i = 0; i < len; i++) {
             int start = ranges[i][0];
             int end = n + ranges[i][1];
             if(start > end) {
-                result[i] = -1.0;
-            } else {
-                double sum = 0;
-                for(int j = start; j < end; j++) {
-                    sum += dis[j];
-                }
-                result[i] = sum;
+                result[i] = -1;
+                continue;
             }
+            
+            double sum = 0;
+            for(int j = start; j < end; j++) {
+                sum += dis[j];
+            }
+            
+            result[i] = sum;
         }
         
         return result;
