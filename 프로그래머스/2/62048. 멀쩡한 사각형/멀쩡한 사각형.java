6ba68aac5bf5e61aff2e2bdfe7c05c92b2515994
LeeJongsu <lik9819@naver.com>
2025-07-29 20:05:44 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.09 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,11 +1,10 @@
 class Solution {
     public long solution(int w, int h) {
         int g = gcd(w, h);
-        int unusable = w + h - g;
-        return (long) w * h - unusable;
+        return (long) w * h - (w + h - g);
     }
     
-    int gcd(int a, int b) {
+    public int gcd(int a, int b) {
         while(b != 0) {
             int temp = a % b;
             a = b;
