ca619535f7d28c4a84d36820980272d611e3a446
LeeJongsu <lik9819@naver.com>
2025-07-29 19:48:20 +0900

    [level 2] Title: 우박수열 정적분, Time: 5.66 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -1,7 +1,5 @@
 import java.util.*;
-
 class Solution {
-    
     class Point {
         int x, y;
         
@@ -12,37 +10,39 @@ class Solution {
     }
     
     public double[] solution(int k, int[][] ranges) {
-        List<Point> pointList = new ArrayList<>();
-        
+        List<Point> points = new ArrayList<>();
         for(int x = 0; k >= 1; x++) {
-            pointList.add(new Point(x, k));
+            points.add(new Point(x, k));
             if(k == 1) {
                 break;
             }
-            k = (k % 2 == 0 ? k / 2 : k * 3 + 1);
+            k = k % 2 == 0 ? k / 2 : k * 3 + 1;
         }
         
-        int n = pointList.size() - 1;
+        int n = points.size() - 1;
         double[] dis = new double[n];
+        
         for(int i = 0; i < n; i++) {
-            Point a = pointList.get(i);
-            Point b = pointList.get(i + 1);
+            Point a = points.get(i);
+            Point b = points.get(i + 1);
             
             dis[i] = (a.y + b.y) / 2.0;
         }
         
         int len = ranges.length;
         double[] result = new double[len];
+        
         for(int i = 0; i < len; i++) {
-            int start = ranges[i][0];
-            int end = n + ranges[i][1];
-            if(start > end) {
+            int s = ranges[i][0];
+            int e = n + ranges[i][1];
+            
+            if(s > e) {
                 result[i] = -1;
                 continue;
             }
             
             double sum = 0;
-            for(int j = start; j < end; j++) {
+            for(int j = s; j < e; j++) {
                 sum += dis[j];
             }
             
