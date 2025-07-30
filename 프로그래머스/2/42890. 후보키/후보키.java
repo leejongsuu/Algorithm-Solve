9da6d17dbc202f526b475eb1a3321c05e91893eb
LeeJongsu <lik9819@naver.com>
2025-07-30 20:03:40 +0900

    [level 2] Title: 후보키, Time: 1.23 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] relation) {
+
         int n = relation.length;
         int m = relation[0].length;
         
         List<Integer> candidates = new ArrayList<>();
         
         for(int subset = 1; subset < (1 << m); subset++) {
-            
             if(!isMinimal(subset, candidates)) {
                 continue;
             }
             
-            if(isUnique(subset, relation, n, m)) {
+            if(isUnique(subset, n, m, relation)) {
                 candidates.add(subset);
             }
         }
@@ -21,23 +21,23 @@ class Solution {
         return candidates.size();
     }
     
-    boolean isMinimal(int subset, List<Integer> candidates) {
+    private boolean isMinimal(int subset, List<Integer> candidates) {
         for(int candidate : candidates) {
-            if((subset & candidate) == candidate) {
+            if((candidate & subset) == candidate) {
                 return false;
             }
         }
         return true;
     }
     
-    boolean isUnique(int subset, String[][] relation, int n, int m) {
+    private boolean isUnique(int subset, int n, int m, String[][] relation) {
         Set<String> seen = new HashSet<>();
         
         for(int i = 0; i < n; i++) {
             StringBuilder sb = new StringBuilder();
             for(int j = 0; j < m; j++) {
-                if((subset & 1 << j) != 0) {
-                    sb.append(relation[i][j]).append('|');
+                if((subset & (1 << j)) != 0) {
+                    sb.append(relation[i][j]);
                 }
             }
             String key = sb.toString();
