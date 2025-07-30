2ff0eb6142e44833bd8af4bd06f211d36a8add66
LeeJongsu <lik9819@naver.com>
2025-07-30 19:16:17 +0900

    [level 2] Title: 점 찍기, Time: 9.60 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,21 +1,15 @@
 class Solution {
     public long solution(int k, int d) {
-        long result = 0;
-        long D = d;
-        long K = k;
-        long maxX = D / K;
-        long d2 = D * D;
+        long answer = 0;
         
-        for(long x = 0; x <= maxX; x++) {
-            long xk = x * K;
-            long remain = d2 - xk * xk;
-            
-            if(remain < 0) break;
-            
-            long maxY = (long) (Math.sqrt(remain) / K);
-            result += maxY + 1;
+        long D2 = 1L * d * d;
+        int maxX = d / k;
+        
+        for(int x = 0; x <= maxX; x++) {
+            long XK2 = 1L * x * x * k * k;
+            answer += (int) Math.sqrt(D2 - XK2) / k + 1;
         }
         
-        return result;
+        return answer;
     }
 }
