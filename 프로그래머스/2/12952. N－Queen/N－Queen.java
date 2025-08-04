d3837d53273c3c802b42bda040da1d58d7682b77
LeeJongsu <lik9819@naver.com>
2025-08-04 20:13:13 +0900

    [level 2] Title: N-Queen, Time: 75.84 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,21 +1,18 @@
 class Solution {
     int result = 0;
-    
-    boolean[] colUsed;
-    boolean[] diag1Used;
-    boolean[] diag2Used;
+    boolean[] colUsed, diagUsed, diag2Used;
     
     public int solution(int n) {
         colUsed = new boolean[n];
-        diag1Used = new boolean[2 * n - 1];
+        diagUsed = new boolean[2 * n - 1];
         diag2Used = new boolean[2 * n - 1];
         
-        dfs(0, n);
+        canPlace(0, n);
+        
         return result;
     }
     
-    void dfs(int row, int n) {
-        
+    private void canPlace(int row, int n) {
         if(row == n) {
             result++;
             return;
@@ -24,15 +21,15 @@ class Solution {
         for(int col = 0; col < n; col++) {
             int d1 = row - col + (n - 1);
             int d2 = row + col;
-            if(!colUsed[col] && !diag1Used[d1] && !diag2Used[d2]) {
+            if(!colUsed[col] && !diagUsed[d1] && !diag2Used[d2]) {
                 colUsed[col] = true;
-                diag1Used[d1] = true;
+                diagUsed[d1] = true;
                 diag2Used[d2] = true;
                 
-                dfs(row + 1, n );
+                canPlace(row + 1, n);
                 
                 colUsed[col] = false;
-                diag1Used[d1] = false;
+                diagUsed[d1] = false;
                 diag2Used[d2] = false;
             }
         }
