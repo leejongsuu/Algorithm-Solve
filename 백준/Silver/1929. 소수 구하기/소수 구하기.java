58712fc2c723e2c0c9eb07b7223f1009166a2059
LeeJongsu <lik9819@naver.com>
2025-09-29 20:54:10 +0900

    [Silver III] Title: 소수 구하기, Time: 216 ms, Memory: 20056 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,37 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static boolean[] prime;
-
-    public static void get_prime(int n) {
-        //true : 소수X , false = 소수
-        prime[0] = prime[1] = true;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (prime[i]) continue;
-            for (int j = i; i * j <= n; j++) {
-                prime[i * j] = true;
-            }
-        }
-    }
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] isNotPrime = new boolean[N + 1];
+        isNotPrime[0] = isNotPrime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            for (int j = i; i * j <= N; j++) {
+                isNotPrime[i * j] = true;
+            }
+        }
+
         StringBuilder sb = new StringBuilder();
-
-        String[] input = br.readLine().split(" ");
-        int M = Integer.parseInt(input[0]);
-        int N = Integer.parseInt(input[1]);
-
-        prime = new boolean[N + 1];
-
-        get_prime(N);
-
         for (int i = M; i <= N; i++) {
-            if (!prime[i]) {
+            if (!isNotPrime[i]) {
                 sb.append(i).append('\n');
             }
         }
