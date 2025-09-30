7a7e712bc2b5a32bac727e79affbe0f18ed4aef9
LeeJongsu <lik9819@naver.com>
2025-09-30 22:22:18 +0900

    [Gold V] Title: 탑, Time: 700 ms, Memory: 87684 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,58 +1,51 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static class TOP {
+        int index, height;
 
-        int N = Integer.parseInt(br.readLine());
-        int[] topHeights = new int[N];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            topHeights[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int[] result = solution(N, topHeights);
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i : result) {
-            sb.append(i).append(' ');
-        }
-
-        System.out.println(sb);
-    }
-
-    static class Top {
-        int index;
-        int height;
-
-        public Top(int index, int height) {
+        TOP(int index, int height) {
             this.index = index;
             this.height = height;
         }
     }
 
-    public static int[] solution(int n, int[] topHeights) {
-        int[] arr = new int[n];
+    public static void main(String[] args) throws IOException {
 
-        Stack<Top> stack = new Stack<>();
-        stack.push(new Top(1, topHeights[0]));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 1; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] topHeights = new int[N];
+        for (int i = 0; i < N; i++) {
+            topHeights[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<TOP> stack = new Stack<>();
+
+        int[] result = new int[N];
+
+        for (int i = 0; i < N; i++) {
             while (!stack.isEmpty() && stack.peek().height < topHeights[i]) {
                 stack.pop();
             }
             if (!stack.isEmpty()) {
-                arr[i] = stack.peek().index;
+                result[i] = stack.peek().index;
             }
-            stack.push(new Top(i + 1, topHeights[i]));
+            stack.push(new TOP(i + 1, topHeights[i]));
         }
 
-        return arr;
+        StringBuilder sb = new StringBuilder();
+        for (int i : result) {
+            sb.append(i).append(' ');
+        }
+
+        System.out.println(sb);
     }
 }
