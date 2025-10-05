997757ade5e5d0d5c1ef5ac78669f3bdfb153325
LeeJongsu <lik9819@naver.com>
2025-10-05 16:55:42 +0900

    [level 2] Title: 괄호 변환, Time: 9.94 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -1,57 +1,62 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
-    
     public String solution(String p) {
-        return makeParentheses(p);
+        
+        return alter(p);
     }
     
-    public String makeParentheses(String str) {
-        int lParentheses = 0;
-        int rParentheses = 0;
-        int length = str.length();
+    public String alter(String w) {
         
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == '(') lParentheses++;
-            else if(str.charAt(i) == ')') rParentheses++;
+        if(w.equals("")) {
+            return "";
+        }
+        
+        int left = 0;
+        int right = 0;
+        
+        String u ="";
+        String v = "";
+        
+        for(int i = 0; i < w.length(); i++) {
+            if(w.charAt(i) == '(') left++;
+            else right++;
             
-            if(lParentheses == rParentheses) {
-                String u = str.substring(0, i + 1);
-                String v = str.substring(i + 1, length);
-                
-                if(isAlrightParentheses(u)) {
-                    return u + makeParentheses(v);
-                }
-                
-                StringBuffer sb = new StringBuffer();
-                sb.append('(').append(makeParentheses(v)).append(')');
-                sb.append(reverseParentheses(u.substring(1, u.length() - 1)));
-                
-                return sb.toString();
-            }
-        }
-        return "";
-    }
-    
-    public boolean isAlrightParentheses(String str) {
-        Stack<Character> stack = new Stack<>();
-        for(char c: str.toCharArray()) {
-            if(c == '(') stack.push(c);
-            else {
-                if(stack.isEmpty()) return false;
-                stack.pop();
+            if(left == right) {
+                u = w.substring(0, i + 1);
+                v = w.substring(i + 1);
+                break;
             }
         }
         
-        return stack.isEmpty();
+        if(checkAlright(u)) {
+            return u + alter(v);
+        } else {
+            String s = "(" + alter(v) + ")";
+            u = u.substring(1, u.length() - 1);
+            char[] ch = u.toCharArray();
+            for(int i = 0; i < ch.length; i++) {
+                if(ch[i] == '(') ch[i] = ')';
+                else ch[i] = '(';
+            }
+            return s + String.valueOf(ch);
+        }
     }
     
-    public String reverseParentheses(String str) {
-        char[] ch = str.toCharArray();
-        for(int i = 0; i < str.length(); i++) {
-            if(ch[i] == '(') ch[i] = ')';
-            else ch[i] = '(';
+    public boolean checkAlright(String str) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : str.toCharArray()) {
+            if(c == '(') {
+                stack.push(c);
+            } else {
+                if(stack.isEmpty()) return false;
+                else stack.pop();
+            }
         }
-        return String.valueOf(ch);
+        
+        if(!stack.isEmpty()) return false;
+        
+        return true;
     }
 }
