cb6ba30ad86e68df01753e0d642f97a270c56eab
LeeJongsu <lik9819@naver.com>
2025-10-06 18:20:33 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.60 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -2,64 +2,86 @@ import java.util.*;
 
 class Solution {
     
-    long answer = 0;
-    char[] operators = {'+', '*', '-'};
-    boolean[] visited = new boolean[3];
-    List<String> expressionList = new ArrayList<>();
+    long result = 0;
+    boolean[] visited;
+    char[] operands;
+    List<String> expList = new ArrayList<>();
     
     public long solution(String expression) {
         
-        StringTokenizer st = new StringTokenizer(expression, "*+-", true);
+        Set<Character> operandSet = new HashSet<>();
+        
+        StringTokenizer st = new StringTokenizer(expression, "[-*+]", true);
         while(st.hasMoreTokens()) {
-            expressionList.add(st.nextToken());
+            String part = st.nextToken();
+            expList.add(part);
+            
+            if(!Character.isDigit(part.charAt(0))) {
+                operandSet.add(part.charAt(0));
+            }
         }
         
-        makePriorityOperator(0, new char[3]);
+        int size = operandSet.size();
+        operands = new char[size];
+        visited = new boolean[size];
         
-        return answer;
+        int i = 0;
+        for(char c : operandSet) {
+            operands[i++] = c;
+        }
+        
+        makeCombination(0, size, new char[size]);
+        
+        return result;
     }
     
-    public void makePriorityOperator(int L, char[] prOperators) {
-        if(L == 3) {
-            answer = Math.max(answer, calculatePrize(prOperators, new ArrayList<>(expressionList)));
+    private void solution(char[] combi) {
+        List<String> tempList = new ArrayList<>(expList);
+
+        for(char c : combi) {
+            String operand = String.valueOf(c);
+            int index = tempList.indexOf(operand);
+            
+            while(index > - 1) {
+                long a = Long.parseLong(tempList.get(index - 1));
+                long b = Long.parseLong(tempList.get(index + 1));
+                long answer = calculate(a, b, c);
+                
+                tempList.add(index - 1, String.valueOf(answer));
+                
+                tempList.remove(index);
+                tempList.remove(index);
+                tempList.remove(index);
+                
+                index = tempList.indexOf(operand);
+            }
+        }
+        
+        result = Math.max(result, Math.abs(Long.parseLong(tempList.get(0))));
+    }
+    
+    private void makeCombination(int L, int size, char[] combi) {
+        if(L == size) {
+            solution(combi);
             return;
         }
         
-        for(int i = 0; i < 3; i++) {
+        for(int i = 0; i < size; i++) {
             if(!visited[i]) {
+                combi[L] = operands[i];
                 visited[i] = true;
-                prOperators[L] = operators[i];
-                makePriorityOperator(L + 1, prOperators);
+                makeCombination(L + 1, size, combi);
                 visited[i] = false;
             }
         }
     }
     
-    public long calculatePrize(char[] prOperators, List<String> list) {
-        for(int i = 0; i < 3; i++) {
-            while(true) {
-                String operator = String.valueOf(prOperators[i]);
-                int index = list.indexOf(operator);
-                
-                if(index < 0) break;
-                else {
-                    long a = Long.parseLong(list.get(index - 1));
-                    long b = Long.parseLong(list.get(index + 1));
-                    String result = String.valueOf(calculateOperator(a, b, prOperators[i]));
-                    list.add(index - 1, result);
-                    list.subList(index, index + 3).clear();
-                }
-            }
-        }
-        return Math.abs(Long.parseLong(list.get(0)));
-    }
-    
-    public long calculateOperator(long a, long b, char c) {
+    private long calculate(long a, long b, char c) {
         return switch(c) {
-            case '*' -> a * b;
-            case '+' -> a + b;
-            case '-' -> a - b;
-            default -> -1;
+                case '+' -> a + b;
+                case '-' -> a - b;
+                case '*' -> a * b;
+                default -> Long.MIN_VALUE;
         };
-    } 
+    }
 }
