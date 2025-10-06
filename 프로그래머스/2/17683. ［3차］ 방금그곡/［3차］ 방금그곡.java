cb4ca29a851c0a4f82b64f24f433ff75a50549df
LeeJongsu <lik9819@naver.com>
2025-10-06 16:18:02 +0900

    [level 2] Title: [3차] 방금그곡, Time: 13.98 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -2,57 +2,80 @@ import java.util.*;
 
 class Solution {
     
-    public String solution(String m, String[] musicinfos) {
+    class Music implements Comparable<Music> {
+        int index;
+        int play;
+        String title;
         
-        String answer = "(None)";
-        int max = Integer.MIN_VALUE;
-        
-        m = convertMelody(m);
-        
-        for(String musicinfo : musicinfos) {
-            String[] parts = musicinfo.split(",");
-            String startTime = parts[0];
-            String endTime = parts[1];
-            String title = parts[2];
-            String music = parts[3];
-            
-            StringBuffer sb = new StringBuffer();
-            int timeLength = calculateTimeLength(parts[0], parts[1]);
-            music = convertMelody(music);
-            int len = music.length();
-            for(int i = 0; i < timeLength; i++) {
-                sb.append(music.charAt(i % len));
-            }
-            
-            String str = sb.toString();
-            if(str.contains(m) && timeLength > max) {
-                    max = timeLength;
-                    answer = title;
-            }
+        Music(int index, int play, String title) {
+            this.index = index;
+            this.play = play;
+            this.title = title;
         }
         
-        return answer;
+        public String getTitle() {
+            return this.title;
+        }
+        
+        @Override
+        public int compareTo(Music o) {
+            if(this.play == o.play) {
+                return this.index - o.index;
+            }
+            return o.play - this.play;
+        }
+    }
+    public String solution(String m, String[] musicinfos) {
+        
+        String cm = convert(m);
+        
+        List<Music> musicList = new ArrayList<>();
+        
+        for(int i = 0; i < musicinfos.length; i++) {
+            String[] infos = musicinfos[i].split(",");
+            
+            String startTime = infos[0];
+            String endTime = infos[1];
+            String title = infos[2];
+            String melody = infos[3];
+            
+            int play = getPlayTime(startTime, endTime);
+            String cmelody = convert(melody);
+            
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < play; j++) {
+                char c = cmelody.charAt(j % cmelody.length());
+                sb.append(c);
+            }
+            
+            if(sb.indexOf(cm) > -1) {
+                musicList.add(new Music(i, play, title));
+            }
+        }
+                
+        Collections.sort(musicList);
+        
+        return musicList.isEmpty() ? "(None)" : musicList.get(0).getTitle();
     }
     
-    private String convertMelody(String melody) {
-        return melody
+    public String convert(String str) {
+        return str
             .replace("C#", "Q")
             .replace("D#", "W")
             .replace("F#", "R")
             .replace("G#", "T")
-            .replace("A#", "Y")
-            .replace("B#", "U");
+            .replace("A#", "U")
+            .replace("E#", "I")
+            .replace("B#", "O");
     }
     
-    private int calculateTimeLength(String start, String end) {
-        int endTime = 0;
-        endTime += 60 * Integer.parseInt(end.substring(0, 2));
-        endTime += Integer.parseInt(end.substring(3));
+    public int getPlayTime(String startTime, String endTime) {
+        int start = 60 * Integer.parseInt(startTime.substring(0, 2))
+            + Integer.parseInt(startTime.substring(3));
         
-        int startTime = 0;
-        startTime += 60 * Integer.parseInt(start.substring(0, 2));
-        startTime += Integer.parseInt(start.substring(3));
+        int end = 60 * Integer.parseInt(endTime.substring(0, 2))
+            + Integer.parseInt(endTime.substring(3));
         
-        return endTime - startTime;
+        return end - start;
     }
 }
