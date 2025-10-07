b55b736d0b0e5d01baf607804f060d292df831c9
LeeJongsu <lik9819@naver.com>
2025-10-07 15:54:05 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.39 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,37 +1,64 @@
 import java.util.*;
 
 class Solution {
+    
     public int solution(int[][] jobs) {
-        int answer = 0;
         
-        // 작업이 요청되는 시점 기준으로 오름차순 정렬
-        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
-        
-        // 작업의 소요시간 기준으로 오름차순 정렬
-        PriorityQueue<int[]> PQ = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        
-        int jobs_index = 0; // 작업 배열 인덱스
-        int finish_job = 0; // 처리 완료된 작업 개수
-        int end_time = 0; // 작업 처리 완료 시간
-        
-        while(true) {
-            
-            if(finish_job == jobs.length) break;
-            
-            while(jobs_index < jobs.length && jobs[jobs_index][0] <= end_time) {
-                PQ.offer(jobs[jobs_index++]);
+        int len = jobs.length;
+        int totalResponseTime = 0;
+        int processedJobs = 0; 
+        int jobIndex = 0;      
+        int currentTime = 0;   
+
+        Work[] works = new Work[len];
+        for (int i = 0; i < len; i++) {
+            works[i] = new Work(i, jobs[i][0], jobs[i][1]);
+        }
+
+        Arrays.sort(works, (a, b) -> a.startTime - b.startTime);
+
+        PriorityQueue<Work> pq = new PriorityQueue<>();
+
+        while (processedJobs < len) {
+            while (jobIndex < len && works[jobIndex].startTime <= currentTime) {
+                pq.offer(works[jobIndex++]);
             }
-            
-            if(!PQ.isEmpty()) {
-                int[] job = PQ.poll();
-                answer += end_time - job[0] + job[1];
-                end_time += job[1];
-                finish_job++;
-            } else {
-                end_time = jobs[jobs_index][0];
+
+            if (pq.isEmpty()) {
+                currentTime = works[jobIndex].startTime;
+            } 
+            else {
+                Work current = pq.poll();
+
+                currentTime += current.workTime;
+                totalResponseTime += currentTime - current.startTime;
+                processedJobs++;
             }
         }
+
+        return totalResponseTime / len;
+    }
+    
+    class Work implements Comparable<Work> {
+        int index;
+        int startTime;
+        int workTime;
         
-        return answer / jobs.length;
+        Work(int index, int startTime, int workTime) {
+            this.index = index;
+            this.startTime = startTime;
+            this.workTime = workTime;
+        }
+        
+        @Override
+        public int compareTo(Work o) {
+            if(this.workTime == o.workTime) {
+                if(this.startTime == o.startTime) {
+                    return this.index - o.index;
+                }
+                return this.startTime - o.startTime;
+            }
+            return this.workTime - o.workTime;
+        }
     }
 }
