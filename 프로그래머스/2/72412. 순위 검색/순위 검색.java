ede7b44fee675df15eb3ea94fcd7e00463d3d642
LeeJongsu <lik9819@naver.com>
2025-10-07 13:55:49 +0900

    [level 2] Title: 순위 검색, Time: 918.21 ms, Memory: 199 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -2,57 +2,107 @@ import java.util.*;
 
 class Solution {
     
-    Map<String, List<Integer>> infoMap = new HashMap<>();
-    final int INF = 4;
+    final String[] languages = {"cpp", "java", "python", "-"};
+    final String[] work = {"backend", "frontend", "-"};
+    final String[] experience = {"junior", "senior", "-"};
+    final String[] food = {"chicken", "pizza", "-"};
+    
+    Map<String, List<Integer>> map = new HashMap<>();
+    
     public int[] solution(String[] info, String[] query) {
         
         int len = query.length;
         int[] result = new int[len];
         
-        for(String applicant : info) {
-            String[] parts = applicant.split(" ");
-            makeInfoMap(0, "", parts, Integer.parseInt(parts[INF]));
+        makeCombination(0, new int[4]);
+        
+        for(String str : info) {
+            String[] parts = str.split(" ");
+            inputInfo(0, new String[4], parts);
         }
         
-        for(String key : infoMap.keySet()) {
-            Collections.sort(infoMap.get(key));
+        for(String key : map.keySet()) {
+            List<Integer> scores = map.get(key);
+            Collections.sort(scores);
         }
         
         for(int i = 0; i < len; i++) {
-            String[] parts = query[i].replaceAll(" and ", "").split(" ");
+            String[] parts = query[i].replace(" and ", "").split(" ");
+            
             String key = parts[0];
             int score = Integer.parseInt(parts[1]);
             
-            if(infoMap.containsKey(key)) {
-                int index = binarySearch(score, infoMap.get(key));
-                result[i] = infoMap.get(key).size() - index;
-            }
+            List<Integer> scoreList = map.get(key);
+            
+            int index = binarySearch(score, scoreList);
+            
+            result[i] = scoreList.size() - index;
         }
         
         return result;
     }
     
-    public void makeInfoMap(int L, String str, String[] parts, int score) {
-        if(L == INF) {
-            infoMap.computeIfAbsent(str, k -> new ArrayList<>()).add(score);
+    private void makeCombination(int L, int[] combi) {
+        if(L == 4) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(languages[combi[0]]);
+            sb.append(work[combi[1]]);
+            sb.append(experience[combi[2]]);
+            sb.append(food[combi[3]]);
+            
+            map.put(sb.toString(), new ArrayList<>());
+            
             return;
         }
         
-        makeInfoMap(L + 1, str + parts[L], parts, score);
-        makeInfoMap(L + 1, str + "-", parts, score);
-    }
-    
-    public int binarySearch(int score, List<Integer> scoreList) {
-        int lo = 0;
-        int hi = scoreList.size();
-        while(lo < hi) {
-            int mid = (lo + hi) / 2;
-            if(scoreList.get(mid) < score) {
-                lo = mid + 1;
-            } else {
-                hi = mid;
+        if(L == 0) {
+            for(int i = 0; i < 4; i++) {
+                combi[L] = i;
+                makeCombination(L + 1, combi);
+            }
+        } else {
+            for(int i = 0; i < 3; i++) {
+                combi[L] = i;
+                makeCombination(L + 1, combi);
             }
         }
-        return lo;
+    }
+    
+    private void inputInfo(int L, String[] combi, String[] parts) {
+        if(L == 4) {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < 4; i++) {
+                sb.append(combi[i]);
+            }
+            
+            String key = sb.toString();
+            int score = Integer.parseInt(parts[4]);
+            
+            map.get(key).add(score);
+            
+            return;
+        }
+        
+        combi[L] = parts[L];
+        inputInfo(L + 1, combi, parts);
+        
+        combi[L] = "-";
+        inputInfo(L + 1, combi, parts);
+    }
+    
+    private int binarySearch(int score, List<Integer> list) {
+        int lt = 0, rt = list.size();
+        
+        while(lt < rt) {
+            int mid = (lt + rt) / 2;
+            
+            if(list.get(mid) < score) {
+                lt = mid + 1;
+            } else {
+                rt = mid;
+            }
+        }
+        
+        return lt;
     }
 }
