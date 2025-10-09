60461b14454583fde271a48d6413bd1487f9a4e3
LeeJongsu <lik9819@naver.com>
2025-10-09 15:51:44 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.88 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,63 +2,65 @@ import java.util.*;
 
 class Solution {
     
-    public int solution(int[][] jobs) {
-        
-        int len = jobs.length;
-        int totalResponseTime = 0;
-        int processedJobs = 0; 
-        int jobIndex = 0;      
-        int currentTime = 0;   
-
-        Work[] works = new Work[len];
-        for (int i = 0; i < len; i++) {
-            works[i] = new Work(i, jobs[i][0], jobs[i][1]);
-        }
-
-        Arrays.sort(works, (a, b) -> a.startTime - b.startTime);
-
-        PriorityQueue<Work> pq = new PriorityQueue<>();
-
-        while (processedJobs < len) {
-            while (jobIndex < len && works[jobIndex].startTime <= currentTime) {
-                pq.offer(works[jobIndex++]);
-            }
-
-            if (pq.isEmpty()) {
-                currentTime = works[jobIndex].startTime;
-            } 
-            else {
-                Work current = pq.poll();
-
-                currentTime += current.workTime;
-                totalResponseTime += currentTime - current.startTime;
-                processedJobs++;
-            }
-        }
-
-        return totalResponseTime / len;
-    }
-    
     class Work implements Comparable<Work> {
         int index;
-        int startTime;
+        int requestTime;
         int workTime;
         
-        Work(int index, int startTime, int workTime) {
+        Work(int index, int requestTime, int workTime) {
             this.index = index;
-            this.startTime = startTime;
+            this.requestTime = requestTime;
             this.workTime = workTime;
         }
         
         @Override
         public int compareTo(Work o) {
             if(this.workTime == o.workTime) {
-                if(this.startTime == o.startTime) {
+                if(this.requestTime == o.requestTime) {
                     return this.index - o.index;
                 }
-                return this.startTime - o.startTime;
+                return this.requestTime - o.requestTime;
             }
+            
             return this.workTime - o.workTime;
         }
+    }
+    
+    public int solution(int[][] jobs) {
+        
+        int len = jobs.length;
+        Work[] works = new Work[len];
+        
+        for(int i = 0; i < len; i++) {
+            works[i] = new Work(i, jobs[i][0], jobs[i][1]);
+        }
+        
+        Arrays.sort(works, (a, b) -> a.requestTime - b.requestTime);
+        
+        PriorityQueue<Work> pq = new PriorityQueue<>();
+        
+        int totalResponse = 0;
+        int currentTime = 0;
+        int index = 0;
+        int sum = 0;
+        
+        while(totalResponse < len) {
+            while(index < len && works[index].requestTime <= currentTime) {
+                pq.offer(works[index++]);
+            }
+            
+            if(pq.isEmpty()) {
+                currentTime = works[index].requestTime;
+                continue;
+            }
+            
+            Work current = pq.poll();
+            
+            currentTime += current.workTime;
+            sum += currentTime - current.requestTime;
+            totalResponse++;
+        }
+        
+        return sum / len;
     }
 }
