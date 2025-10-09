a83bef1996b7c691de431d13d0f436b40c4cbcb9
LeeJongsu <lik9819@naver.com>
2025-10-09 17:25:41 +0900

    [level 1] Title: 모의고사, Time: 4.11 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,24 +1,28 @@
-import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
+
     public int[] solution(int[] answers) {
         
-        int[] person1 = new int[]{1,2,3,4,5};
-        int[] person2 = new int[]{2,1,2,3,2,4,2,5};
-        int[] person3 = new int[]{3,3,1,1,2,2,4,4,5,5};
+        final int[] one = {1, 2, 3, 4, 5};
+        final int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+        final int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] count = new int[3];
-        for(int i = 0; i < answers.length; i++) {
-            if(person1[i % person1.length] == answers[i]) count[0]++;
-            if(person2[i % person2.length] == answers[i]) count[1]++;
-            if(person3[i % person3.length] == answers[i]) count[2]++;
+        int[] counts = new int[3];
+        
+        int index = 0;
+        for(int answer : answers) {
+            if(one[index % one.length] == answer) counts[0]++;
+            if(two[index % two.length] == answer) counts[1]++;
+            if(three[index % three.length] == answer) counts[2]++;
+            index++;
         }
         
-        int max = Arrays.stream(count).max().getAsInt();
-        ArrayList<Integer> result = new ArrayList<>();
+        int max = Math.max(counts[0], Math.max(counts[1], counts[2]));
+        
+        List<Integer> result = new ArrayList<>();
         for(int i = 0; i < 3; i++) {
-            if(count[i] == max) {
+            if(counts[i] == max) {
                 result.add(i + 1);
             }
         }
