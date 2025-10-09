d58e56100b560536a7bda29c299f4d6f4a4b3d02
LeeJongsu <lik9819@naver.com>
2025-10-09 22:14:58 +0900

    [level 2] Title: 구명보트, Time: 25.54 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
 
 ### 성능 요약
 
-메모리: 57.6 MB, 시간: 14.42 ms
+메모리: 58.3 MB, 시간: 25.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 27일 11:05:51
+2025년 10월 09일 22:14:54
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,33 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         
+        int result = 0;
+        
         Arrays.sort(people);
         
-        int result = 1;
-        int sum = 0;
-        int count = 0;
-            
-        int lt = 0;
-        int rt = people.length - 1;
-        while(lt <= rt) {
-            if(count + 1 <= 2) {
-                count++;
-                if(sum + people[rt] <= limit) {
-                    sum += people[rt];
-                    rt--;
-                } else if(sum + people[lt] <= limit) {
-                    sum += people[lt];
-                    lt++;
-                } else {
-                    result++;
-                    sum = 0;
-                    count = 0;
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        
+        for(int p : people) {
+            deque.add(p);
+        }
+        
+        while(!deque.isEmpty()) {
+            int first = deque.pollLast();
+            if(!deque.isEmpty()) {
+                int second = deque.peekFirst();
+                if(first + second <= limit) {
+                    deque.pollFirst();
                 }
-            } else {
-                result++;
-                sum = 0;
-                count =0;
             }
+            result++;
         }
         
         return result;
