a8ee3d40529257140f89ac44f0e79963c403c1fd
LeeJongsu <lik9819@naver.com>
2025-10-17 12:10:42 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,66 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static class Counsel {
-        int index;
-        int t, p;
-
-        Counsel(int index, int t, int p) {
-            this.index = index;
-            this.t = t;
-            this.p = p;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        Counsel[] counsels = new Counsel[N + 1];
+
+        int[] times = new int[N + 1];
+        int[] points = new int[N + 1];
 
         for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int t = Integer.parseInt(st.nextToken());
-            int p = Integer.parseInt(st.nextToken());
-            counsels[i] = new Counsel(i, t, p);
+            times[i] = Integer.parseInt(st.nextToken());
+            points[i] = Integer.parseInt(st.nextToken());
         }
 
-        Queue<Counsel> queue = new LinkedList<>();
-        int[] dp = new int[N + 1];
+        int[] dp = new int[N + 2];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = N; i > 0; i--) {
+            int nextDay = i + times[i];
 
-            if (dp[i] >= counsels[i].p || i + counsels[i].t > N + 1) {
-                continue;
-            }
-
-            dp[i] = counsels[i].p;
-            queue.offer(counsels[i]);
-
-            while (!queue.isEmpty()) {
-                Counsel current = queue.poll();
-
-                for (int j = current.index + current.t; j <= N; j++) {
-                    Counsel next = counsels[j];
-                    if (next.index + next.t <= N + 1) {
-                        if (dp[j] < dp[current.index] + next.p) {
-                            dp[j] = dp[current.index] + next.p;
-                            queue.offer(next);
-                        }
-                    }
-                }
+            if (nextDay > N + 1) {
+                dp[i] = dp[i + 1];
+            } else {
+                dp[i] = Math.max(dp[i + 1], points[i] + dp[nextDay]);
             }
         }
 
-        int max = Arrays.stream(dp).max().getAsInt();
-        System.out.println(max);
+        System.out.println(dp[1]);
     }
 }
