5f9dad5dec345979451632066da26af6f065631c
LeeJongsu <lik9819@naver.com>
2025-10-17 14:01:21 +0900

    [Gold IV] Title: 플로이드, Time: 360 ms, Memory: 42380 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,54 +1,61 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        final int INF = 100000 * N;
 
-        int[][] dist = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (i == j) {
-                    dist[i][j] = 0;
-                    continue;
-                }
-                dist[i][j] = INF;
-            }
+        final int INF = N * 100_000 + 1;
+
+        int[][] city = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(city[i], INF);
         }
 
         for (int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()) - 1;
+            int e = Integer.parseInt(st.nextToken()) - 1;
             int w = Integer.parseInt(st.nextToken());
-            dist[s][e] = Math.min(dist[s][e], w);
+
+            city[s][e] = Math.min(city[s][e], w);
         }
 
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j) {
+                        city[i][j] = 0;
+                        continue;
+                    }
+
+                    city[i][j] = Math.min(city[i][j], city[i][k] + city[k][j]);
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (dist[i][j] == INF) {
-                    sb.append(0).append(' ');
+        for (int[] start : city) {
+            for (int end : start) {
+                if (end >= INF) {
+                    sb.append(0);
                 } else {
-                    sb.append(dist[i][j]).append(' ');
+                    sb.append(end);
                 }
+                sb.append(' ');
             }
             sb.append('\n');
         }
 
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
