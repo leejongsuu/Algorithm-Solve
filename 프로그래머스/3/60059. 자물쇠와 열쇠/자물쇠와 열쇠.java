502153aba2feb7e867da5129f5f1f6efffb7fe62
LeeJongsu <lik9819@naver.com>
2025-10-24 23:29:26 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 32.56 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,72 +1,74 @@
-import java.util.*;
-
 class Solution {
     
-    int M, N;
+    int N, M;
     
     public boolean solution(int[][] key, int[][] lock) {
         
-        M = key.length;
         N = lock.length;
-
-        int paddedSize = N + 2 * (M - 1);
-        int[][] paddedLock = new int[paddedSize][paddedSize];
+        M = key.length;
+        
+        int P = M - 1;
+        int newSize = N + 2 * P;
+        int[][] expLock = new int[newSize][newSize];
         
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                paddedLock[i + M - 1][j + M - 1] = lock[i][j];
+                expLock[i + P][j + P] = lock[i][j];
             }
         }
         
-        for(int k = 0; k < 4; k++) {
-            key = rotate(key);
+        for(int d = 0 ; d < 4; d++) {
+            key = rotateKey(key);
             
-            for(int r = 0; r <= paddedSize - M; r++) {
-                for(int c = 0; c <= paddedSize - M; c++) {
-                    if(check(r, c, paddedLock, key)) {
+            for(int i = 0; i < N + P; i++) {
+                for(int j = 0; j < N + P; j++) {
+                    if(check(i, j, P, key, expLock)) {
                         return true;
                     }
                 }
             }
         }
+
         return false;
     }
     
-    public int[][] rotate(int[][] key) {
+    public boolean check(int r, int c, int size, int[][] key, int[][] expLock) {
+        
+        for(int i = 0; i < M; i++) {
+            for(int j = 0; j < M; j++) {
+                expLock[r + i][c + j] += key[i][j];
+            }
+        }
+        
+        boolean isLocked = true;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(expLock[i + size][j + size] != 1) {
+                    isLocked = false;
+                    break;
+                }
+            }
+            if(!isLocked) break;
+        }
+        
+        for(int i = 0; i < M; i++) {
+            for(int j = 0; j < M; j++) {
+                expLock[r + i][c + j] -= key[i][j];
+            }
+        }
+        
+        return isLocked;
+    }
+    
+    public int[][] rotateKey(int[][] key) {
         int[][] newKey = new int[M][M];
+
         for(int i = 0; i < M; i++) {
             for(int j = 0; j < M; j++) {
                 newKey[i][j] = key[j][M - (i + 1)];
             }
         }
+        
         return newKey;
-    }
-    
-    public boolean check(int r, int c, int[][] paddedLock, int[][] key) {
-        
-        for(int i = 0; i < M; i++) {
-            for(int j = 0; j < M; j++) {
-                paddedLock[r + i][c + j] += key[i][j];
-            }
-        }
-        
-        boolean isUnlocked = true;
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(paddedLock[i + M - 1][j + M - 1] != 1) {
-                    isUnlocked = false; 
-                    break;
-                }
-            }
-            if(!isUnlocked) break;
-        }
-        
-        for(int i = 0; i < M; i++) {
-            for(int j = 0; j < M; j++) {
-                paddedLock[r + i][c + j] -= key[i][j];
-            }
-        }
-        
-        return isUnlocked;
     }
 }
