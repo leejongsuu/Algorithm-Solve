885b934500e7e56f5c5a84931eb0b93cd6001ca7
LeeJongsu <lik9819@naver.com>
2025-10-24 23:58:23 +0900

    [Gold IV] Title: 인구 이동, Time: 552 ms, Memory: 297188 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -9,7 +9,6 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, L, R;
-    static int[][] land;
     final static int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public static void main(String[] args) throws IOException {
@@ -20,84 +19,80 @@ public class Main {
         L = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
 
-        land = new int[N][N];
+        int[][] country = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                land[i][j] = Integer.parseInt(st.nextToken());
+                country[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         int day = 0;
-        while (day <= 2000) {
-
-            boolean[][] check = new boolean[N][N];
-
+        while (day < 2000) {
+            boolean[][] visited = new boolean[N][N];
             boolean isMove = false;
 
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (!check[i][j]) {
-                        check[i][j] = true;
-                        if (move(i, j, check) != -1) {
+                    if (!visited[i][j]) {
+                        visited[i][j] = true;
+                        if (move(i, j, country, visited)) {
                             isMove = true;
                         }
                     }
                 }
             }
-
             if (!isMove) {
                 break;
             }
-
             day++;
         }
 
         System.out.println(day);
     }
 
-    private static int move(int r, int c, boolean[][] check) {
+    private static boolean move(int r, int c, int[][] country, boolean[][] visited) {
 
-        List<int[]> list = new LinkedList<>();
-        list.add(new int[]{r, c});
+        int population = country[r][c];
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{r, c});
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{r, c});
 
-        int population = land[r][c];
+        List<int[]> posStoreList = new LinkedList<>();
+        posStoreList.add(new int[]{r, c});
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
 
             for (int[] dir : dirs) {
                 int nr = current[0] + dir[0];
                 int nc = current[1] + dir[1];
 
-                if (isIn(nr, nc) && !check[nr][nc]) {
-                    int sub = Math.abs(land[nr][nc] - land[current[0]][current[1]]);
+                if (isIn(nr, nc) && !visited[nr][nc]) {
+                    int sub = Math.abs(country[current[0]][current[1]] - country[nr][nc]);
                     if (sub >= L && sub <= R) {
-                        population += land[nr][nc];
-                        queue.offer(new int[]{nr, nc});
-                        list.add(new int[]{nr, nc});
-                        check[nr][nc] = true;
+                        visited[nr][nc] = true;
+                        population += country[nr][nc];
+                        q.offer(new int[]{nr, nc});
+                        posStoreList.add(new int[]{nr, nc});
                     }
                 }
             }
         }
 
-        int size = list.size();
-        if (size == 1) {
-            return -1;
-        } else {
+        int size = posStoreList.size();
+        if (size > 1) {
             int newPopulation = population / size;
-            for (int[] pos : list) {
-                land[pos[0]][pos[1]] = newPopulation;
+            for (int[] pos : posStoreList) {
+                country[pos[0]][pos[1]] = newPopulation;
             }
-            return newPopulation;
+            return true;
         }
+
+        return false;
     }
 
     private static boolean isIn(int r, int c) {
-        return r >= 0 && c >= 0 && r < N && c < N;
+        return r >= 0 && r < N && c >= 0 && c < N;
     }
 }
