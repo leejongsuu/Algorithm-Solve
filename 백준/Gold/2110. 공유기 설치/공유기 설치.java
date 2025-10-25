efd2828da31c6b78ee4d88e79de1d261799d414b
LeeJongsu <lik9819@naver.com>
2025-10-25 09:51:39 +0900

    [Gold IV] Title: 공유기 설치, Time: 248 ms, Memory: 28976 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -20,38 +20,40 @@ public class Main {
 
         Arrays.sort(houses);
 
-        System.out.println(binarySearch(houses, C));
+        int result = binarySearch(C, houses);
+        System.out.println(result);
     }
 
-    public static int binarySearch(int[] houses, int C) {
-        // hi를 '불가능한 최소 거리'의 시작점으로 보기 위해 +1
-        int lo = 1; 
-        int hi = houses[houses.length - 1] - houses[0] + 1; 
+    private static int binarySearch(int target, int[] houses) {
+        int max = 0;
+        int lo = 0, hi = houses[houses.length - 1] - houses[0] + 1;
 
-        // while (lo < hi)
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
 
-            int count = 1;
-            int lastInstalled = houses[0];
-            for (int i = 1; i < houses.length; i++) {
-                if (houses[i] - lastInstalled >= mid) {
-                    count++;
-                    lastInstalled = houses[i];
-                }
-            }
-
-            if (count >= C) {
-                // mid 거리가 가능하므로, 더 큰 거리를 찾아 오른쪽으로
+            int count = getCount(mid, houses);
+            if (count >= target) {
                 lo = mid + 1;
+                max = Math.max(max, mid);
             } else {
-                // mid 거리가 불가능하므로, hi를 mid로 좁힘
                 hi = mid;
             }
         }
 
-        // 루프가 끝나면 lo는 '설치 불가능한 최소 거리'
-        // 따라서 가능한 최대 거리는 lo - 1
-        return lo - 1;
+        return max;
+    }
+
+    private static int getCount(int distance, int[] houses) {
+        int count = 1;
+        int before = houses[0];
+
+        for (int i = 1; i < houses.length; i++) {
+            if (houses[i] - before >= distance) {
+                count++;
+                before = houses[i];
+            }
+        }
+
+        return count;
     }
 }
