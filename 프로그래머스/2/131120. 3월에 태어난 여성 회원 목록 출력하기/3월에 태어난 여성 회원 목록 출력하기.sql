612dafea0f224dd7b817d80f5cd35ea048140fce
LeeJongsu <lik9819@naver.com>
2025-10-30 19:00:14 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT
+member_id,
+member_name,
+gender,
+date_format(date_of_birth, '%Y-%m-%d') as date_of_birth
+from member_profile
+where date_of_birth like '%-03-%' and gender = 'W' and tlno is not null
+order by member_id
