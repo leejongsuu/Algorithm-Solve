ea28959b08b033a4a35e42e966074df4ea410678
LeeJongsu <lik9819@naver.com>
2025-11-10 20:11:45 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -2,44 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Data {
-    int zero_cnt, one_cnt;
-
-    public Data(int zero_cnt, int one_cnt) {
-        this.zero_cnt = zero_cnt;
-        this.one_cnt = one_cnt;
-    }
-}
-
 public class Main {
 
-    static Data[] dy = new Data[41];
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
-        dy[0] = new Data(1, 0);
-        dy[1] = new Data(0, 1);
+        final int INF = 41;
+
+        int[][] fibo = new int[INF][2];
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < T; i++) {
-            int num = Integer.parseInt(br.readLine());
-            Data data = dfs(num);
-            sb.append(data.zero_cnt).append(' ').append(data.one_cnt).append('\n');
+            int n = Integer.parseInt(br.readLine());
+            int[] result = getfibo(n, fibo);
+            sb.append(result[0]).append(" ").append(result[1]).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    static Data dfs(int n) {
-        if (dy[n] != null) return dy[n];
-        else {
-            Data previous = dfs(n - 1);
-            Data twoPrevious = dfs(n - 2);
-            return dy[n] = new Data(previous.zero_cnt + twoPrevious.zero_cnt, previous.one_cnt + twoPrevious.one_cnt);
+    public static int[] getfibo(int n, int[][] fibo) {
+        if (n == 0) {
+            return new int[]{1, 0};
+        } else if (n == 1) {
+            return new int[]{0, 1};
+        } else if (fibo[n][0] > 0 || fibo[n][1] > 0) {
+            return fibo[n];
+        } else {
+            int[] before = getfibo(n - 1, fibo);
+            int[] before2 = getfibo(n - 2, fibo);
+            return fibo[n] = new int[]{before[0] + before2[0], before[1] + before2[1]};
         }
     }
 }
