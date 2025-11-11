dfff5e9f71b1750fb150d1914f86f17a1ddf8a23
LeeJongsu <lik9819@naver.com>
2025-11-11 13:34:25 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -4,30 +4,27 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int[] stair = new int[N + 1];
-        int[] dy = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            stair[i] = Integer.parseInt(br.readLine());
+        int[] scores = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            scores[i] = Integer.parseInt(br.readLine());
         }
 
-        dy[1] = stair[1];
-        if (N >= 2) {
-            dy[2] = stair[1] + stair[2];
+        int[] dp = new int[n + 1];
+
+        dp[1] = scores[1];
+        if (n >= 2) {
+            dp[2] = scores[1] + scores[2];
         }
 
-        for (int i = 3; i <= N; i++) {
-            dy[i] = Math.max(dy[i - 2], dy[i - 3] + stair[i - 1]) + stair[i];
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2], dp[i - 3] + scores[i - 1]) + scores[i];
         }
 
-        System.out.println(dy[N]);
+        System.out.println(dp[n]);
     }
 }
