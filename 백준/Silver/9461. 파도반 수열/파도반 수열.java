56cd21e1759c39c42b82b90525726987c331438a
LeeJongsu <lik9819@naver.com>
2025-11-11 13:59:35 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -4,26 +4,32 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static long[] arr = new long[101];
-
-    static long solution(int n) {
-        if (n <= 3) return 1;
-        if (arr[n] != 0) return arr[n];
-        else {
-            return arr[n] = solution(n - 3) + solution(n - 2);
-        }
-    }
+    static final int INF = 100;
+    static long[] dp = new long[INF + 1];
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
-            sb.append(solution(Integer.parseInt(br.readLine()))).append('\n');
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dfs(n)).append('\n');
         }
 
         System.out.println(sb);
+    }
+
+    public static long dfs(int n) {
+        if (n <= 3) {
+            return 1;
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        return dp[n] = dfs(n - 3) + dfs(n - 2);
     }
 }
