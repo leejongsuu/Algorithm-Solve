7a245510744072de5c6cb56d6af46d5dd4a52484
LeeJongsu <lik9819@naver.com>
2025-11-13 13:49:54 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 17464 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,57 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static long solution(int k, int n, long max, int[] arr) {
-
-        //반드시 max에서 +1 값이어야 함.
-        max++;
-
-        long min = 1;
-        long mid;
-
-        while (min < max) {
-
-            mid = (max + min) / 2;
-
-            long count = 0;
-
-            for (int i = 0; i < k; i++) {
-                count += (arr[i] / mid);
-            }
-
-            if (count < n) {
-                max = mid;
-            } else {
-                min = mid + 1;
-            }
-        }
-
-        return min - 1;
-    }
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] input = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int K = Integer.parseInt(input[0]);
-        int N = Integer.parseInt(input[1]);
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[K];
-
-        long max = 0;
-
         for (int i = 0; i < K; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-            if (max < arr[i]) {
-                max = arr[i];
+        }
+
+        long lt = 1;
+        long rt = (long) Arrays.stream(arr).max().getAsInt() + 1;
+
+        while (lt < rt) {
+            long mid = lt + (rt - lt) / 2;
+
+            int count = getCount(mid, arr);
+            if (count >= N) {
+                lt = mid + 1;
+            } else {
+                rt = mid;
             }
         }
 
-        System.out.println(solution(K, N, max, arr));
+        System.out.println(lt - 1);
+    }
+
+    public static int getCount(long mid, int[] arr) {
+        int count = 0;
+        for (int a : arr) {
+            count += (int) (a / mid);
+        }
+        return count;
     }
 }
