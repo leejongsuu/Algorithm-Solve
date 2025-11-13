392f1f4903c3d4454fee844bd68e158d02e6eba5
LeeJongsu <lik9819@naver.com>
2025-11-13 11:10:50 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 100 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -5,17 +5,22 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        final int MOD = 10_007;
 
-        int[] dp = new int[N + 1];
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i <= N; i++) {
-            dp[i] = (2 * dp[i - 2] + dp[i - 1]) % 10007;
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        if (n > 1) {
+            dp[2] = 3;
         }
 
-        System.out.println(dp[N]);
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[n]);
     }
 }
