199739174046d272fd640059efb970dafedf5a3b
LeeJongsu <lik9819@naver.com>
2025-11-14 18:26:45 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.42 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,18 +1,15 @@
 class Solution {
     public int solution(int sticker[]) {
-
+        
         int n = sticker.length;
         
         if(n == 1) {
             return sticker[0];
         }
         
-        
-        // case 1 : sticker[0] 뜯는 경우
         int[] dp1 = new int[n];
-        
         dp1[0] = sticker[0];
-        dp1[1] = dp1[0]; // 첫 스티커 뜯었으므로, 두 번째는 못 뜯음.
+        dp1[1] = sticker[0];
         
         for(int i = 2; i < n - 1; i++) {
             dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
@@ -20,7 +17,7 @@ class Solution {
         
         int answer1 = dp1[n - 2];
         
-        // case 2 : sticker[0] 뜯지 않는 경우
+        
         int[] dp2 = new int[n];
         dp2[0] = 0;
         dp2[1] = sticker[1];
