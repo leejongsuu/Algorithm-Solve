2d51f04760c63f4db823112e202dfba44135b88d
LeeJongsu <lik9819@naver.com>
2025-11-14 17:14:50 +0900

    [Silver II] Title: 마인크래프트, Time: 592 ms, Memory: 36112 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,55 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-        int b = Integer.parseInt(input[2]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n][m];
-        int max = Integer.MIN_VALUE;
+        int[][] plain = new int[N][M];
+
         int min = Integer.MAX_VALUE;
+        int max = 0;
 
-        for (int i = 0; i < n; i++) {
-            input = br.readLine().split(" ");
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(input[j]);
-                if (arr[i][j] > max) max = arr[i][j];
-                if (arr[i][j] < min) min = arr[i][j];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                plain[i][j] = Integer.parseInt(st.nextToken());
+                if (plain[i][j] < min) {
+                    min = plain[i][j];
+                } else if (plain[i][j] > max) {
+                    max = plain[i][j];
+                }
             }
         }
 
-        int time = Integer.MAX_VALUE;
-        int height = 0;
-        for (int i = min; i <= max; i++) {
-            int block = b;
-            int temporal = 0;
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < m; k++) {
-                    if (arr[j][k] > i) {
-                        temporal += ((arr[j][k] - i) * 2);
-                        block += (arr[j][k] - i);
-                    } else if (arr[j][k] < i) {
-                        temporal += (i - arr[j][k]);
-                        block -= (i - arr[j][k]);
+        int maxHeight = 0;
+        int minTime = Integer.MAX_VALUE;
+
+        for (int h = min; h <= max; h++) {
+            int toUp = 0;
+            int toDown = 0;
+
+            for (int[] arr : plain) {
+                for (int num : arr) {
+                    if (h > num) {
+                        toUp += h - num;
+                    } else if (h < num) {
+                        toDown += num - h;
                     }
                 }
             }
 
-            if (block < 0) continue;
+            int b = B + toDown;
 
-            if (temporal <= time) {
-                time = temporal;
-                height = i;
+            if (toUp > b) {
+                continue;
+            }
+
+            int time = toDown * 2 + toUp;
+
+            if (time <= minTime) {
+                minTime = time;
+                maxHeight = h;
             }
         }
-        System.out.println(time + " " + height);
+
+        System.out.println(minTime + " " + maxHeight);
     }
 }
